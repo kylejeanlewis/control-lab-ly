4c40415d4ac998ea43c2bbccc46f7758bf8d0e27
Chang Jie Leong <changjie9@gmail.com>
2022-10-20 17:11:48 +0800

    Update biologic_utils.py
    - changed the program from mpp to GEIS

--- src/utils/characterisation/electrical/biologic_utils.py
+++ src/utils/characterisation/electrical/biologic_utils.py
@@ -18,20 +18,61 @@ print(f"Import: OK <{__name__}>")
 
 # %%
 # create device
-bl = ebl.BiologicDevice( '192.168.1.2' )
+bl = ebl.BiologicDevice('192.168.1.2', populate_info=False)
 
-# create mpp program
+# create GEIS program
+'''
+current: Initial current in Ampere.
+amplitude_current: Sinus amplitude in Ampere.
+initial_frequency: Initial frequency in Hertz.
+final_frequency: Final frequency in Hertz.
+frequency_number: Number of frequencies.
+duration: Overall duration in seconds. # Comment: Isn't this really a step duration?
+vs_initial: If step is vs. initial or previous.
+    [Default: False]
+time_interval: Maximum time interval between points in seconds.
+    [Default: 1]
+potential_interval: Maximum interval between points in Volts.
+    [Default: 0.001]
+sweep: Defines whether the spacing between frequencies is logarithmic
+    ('log') or linear ('lin'). [Default: 'log']
+repeat: Number of times to repeat the measurement and average the values
+    for each frequency. [Default: 1]
+correction: Drift correction. [Default: False]
+wait: Adds a delay before the measurement at each frequency. The delay
+    is expressed as a fraction of the period. [Default: 0]
+'''
 params = {
-	'run_time': 10* 60		
+	'run_time': 10* 60,
+    'current': 0,
+    'amplitude_current': 5E-3,
+    'initial_frequency': 5,
+    'final_frequency': 5E4,
+    'frequency_number': 100,
+    'duration': 1
 }
 
-mpp = blp.MPP(
+geis = blp.GEIS(
     bl,
     params, 	
-    channels = [ 0, 1, 2, 3, 4, 5, 6 ]        
+    channels = [0]        
 )
 
 # run program
-mpp.run( 'data' )
+geis.run( 'data' )
 
 # %%
+"""
+First-party recommended way
+"""
+
+
+class BioLogic(object):
+    def __init__(self, address, timeout=5, populate_info=True):
+        super().__init__(address, timeout, populate_info)
+        
+    def connect(self, bin_file=None, xlx_file=None):
+        return super().connect(bin_file, xlx_file)
+    
+    def measure(self, chs=None):
+        return super().start_channels(chs)
