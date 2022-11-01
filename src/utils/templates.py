151b2dbfc8bf06a0bf3f4d184815e5bd8267e446
Chang Jie Leong <changjie9@gmail.com>
2022-11-01 16:04:14 +0800

    Create templates.py

---
+++ src/utils/templates.py
@@ -0,0 +1,39 @@
+# %% -*- coding: utf-8 -*-
+"""
+Created on Fri 2022/06/18 09:00:00
+
+@author: Chang Jie
+
+Easy BioLogic package documentation can be found at:
+https://github.com/bicarlsen/easy-biologic
+
+Notes:
+- (actionables)
+"""
+# Standard library imports
+import cmath
+import math
+import nest_asyncio
+import numpy as np
+import os
+import pandas as pd
+import sys
+import time
+
+# Third party imports
+import easy_biologic as biologic_api # pip install easy-biologic
+import plotly.express as px # pip install plotly-express
+import PySimpleGUI as sg # pip install PySimpleGUI
+
+# %%
+# Local application imports
+# REPO_NAME = 'control-lab-le'
+# HERE = '/src/utils'
+# cwd = os.getcwd()
+# repo = cwd.split(REPO_NAME)[0] + REPO_NAME
+# here = repo + HERE.replace('/', '\\')
+# print(f"Import: OK <{__name__}>")
+from . import Tool, Helper
+# %%
+import gui
+# %%
