55dadded2f8dc1e3a546f86269054726c2b94a9c
kylejeanlewis <changjie9@gmail.com>
2022-11-03 00:49:52 +0800

    Update __init__.py

---
+++ controllable/Measure/Electrical/__init__.py
@@ -0,0 +1,9 @@
+class Measurer(object):
+    def __init__(self) -> None:
+        pass
+    
+    def _connect(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method '_connect'")
+        return
+    
