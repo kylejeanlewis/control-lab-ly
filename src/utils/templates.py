e649e2fcbda2f37a45be845a71e3bfeb3aa69c18
Chang Jie Leong <changjie9@gmail.com>
2022-11-02 13:15:18 +0800

    Update templates.py

--- src/utils/templates.py
+++ src/utils/templates.py
@@ -1,39 +1,33 @@
 # %% -*- coding: utf-8 -*-
 """
-Created on Fri 2022/06/18 09:00:00
-
+Created: Tue 2022/11/01 17:13:35
 @author: Chang Jie
 
-Easy BioLogic package documentation can be found at:
-https://github.com/bicarlsen/easy-biologic
-
-Notes:
-- (actionables)
+Notes / actionables:
+-
 """
 # Standard library imports
-import cmath
-import math
-import nest_asyncio
-import numpy as np
-import os
-import pandas as pd
-import sys
-import time
+# import numpy as np
+# import os
+# import pandas as pd
+# import sys
+# import time
 
 # Third party imports
-import easy_biologic as biologic_api # pip install easy-biologic
-import plotly.express as px # pip install plotly-express
-import PySimpleGUI as sg # pip install PySimpleGUI
+# import easy_biologic as biologic_api # pip install easy-biologic
+# import plotly.express as px # pip install plotly-express
+# import PySimpleGUI as sg # pip install PySimpleGUI
 
 # %%
 # Local application imports
-# REPO_NAME = 'control-lab-le'
-# HERE = '/src/utils'
-# cwd = os.getcwd()
-# repo = cwd.split(REPO_NAME)[0] + REPO_NAME
-# here = repo + HERE.replace('/', '\\')
-# print(f"Import: OK <{__name__}>")
-from . import Tool, Helper
-# %%
-import gui
+print(f"Import: OK <{__name__}>")
+
+class myClass (object):
+    def __init__(self) -> None:
+        pass
+    
+    def myMethod(self, *args, **kwargs):
+        print(f"'{self.__class__.__name__}' class has no method 'myMethod'")
+        return
+
 # %%
