2c215df881f91604a537f58fcd701d98b188aeb2
Chang Jie Leong <changjie9@gmail.com>
2022-11-02 16:42:24 +0800

    Create testing_ground.py

---
+++ testing_ground.py
@@ -0,0 +1,17 @@
+# %% -*- coding: utf-8 -*-
+"""
+Created: Tue 2022/11/01 17:13:35
+@author: Chang Jie
+
+Notes / actionables:
+-
+"""
+# Standard library imports
+
+# Third party imports
+
+# Local application imports
+from controllable.Move import Cartesian, Jointed
+print(f"Import: OK <{__name__}>")
+
+# %%
