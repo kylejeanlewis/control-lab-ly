95bb431dacd2b97a604843b9bb6f1aaddc96bf1f
Chang Jie Leong <changjie9@gmail.com>
2022-11-02 16:41:23 +0800

    Update __init__.py
    - create base class

---
+++ controllable/Move/__init__.py
@@ -0,0 +1,176 @@
+class Mover(object):
+    def __init__(self) -> None:
+        pass
+    
+    def _connect(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method '_connect'")
+        return
+    
+    def _freeze(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method '_freeze'")
+        return
+    
+    def _shutdown(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method '_shutdown'")
+        return
+    
+    def _transform_vector_in(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method '_transform_vector_in'")
+        return
+
+    def _transform_vector_out(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method '_transform_vector_out'")
+        return
+    
+    def blow(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'blow'")
+        return
+    
+    def calibrate(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'calibrate'")
+        return
+    
+    def calibrationMode(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'calibrationMode'")
+        return
+    
+    def connect(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'connect'")
+        return
+    
+    def drop(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'drop'")
+        return
+    
+    def getOrientation(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'getOrientation'")
+        return
+
+    def getPosition(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'getPosition'")
+        return
+    
+    def getWorkspacePosition(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'getWorkspacePosition'")
+        return
+    
+    def getSettings(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'getSettings'")
+        return
+    
+    def grab(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'grab'")
+        return
+    
+    def heat(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'heat'")
+        return
+    
+    def home(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'home'")
+        return
+
+    def isFeasible(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'isFeasible'")
+        return
+
+    def move(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'move'")
+        return
+    
+    def moveBy(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'moveBy'")
+        return
+
+    def moveTo(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'moveTo'")
+        return
+
+    def moveJointBy(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'moveJointBy'")
+        return
+    
+    def moveJointTo(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'moveJointTo'")
+        return
+
+    def moveCoordBy(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'moveCoordBy'")
+        return
+    
+    def moveCoordTo(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'moveCoordTo'")
+        return
+    
+    def reset(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'reset'")
+        return
+    
+    def rotateBy(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'rotateBy'")
+        return
+
+    def rotateTo(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'rotateTo'")
+        return
+    
+    def setImplementOffset(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'setImplementOffset'")
+        return
+
+    def setPosition(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'setPosition'")
+        return
+    
+    def setSpeed(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'setSpeed'")
+        return
+    
+    def stop(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'stop'")
+        return
+    
+    def suck(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method 'suck'")
+        return
+    
+    def tuck(self, *args, **kwargs):
+        """EMPTY METHOD"""
+        print(f"'{self.__class__.__name__}' class has no method '_tuck'")
+        return
+
+from . import Cartesian
+from . import Jointed
