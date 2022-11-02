311c28633f980d71a6628786c7bb899a89e3598a
kylejeanlewis <changjie9@gmail.com>
2022-11-03 00:48:56 +0800

    Update testing_ground.py
    - to test non-code importing in eis_datatype.py

--- testing_ground.py
+++ testing_ground.py
@@ -7,13 +7,29 @@ Notes / actionables:
 -
 """
 # Standard library imports
-
+import pandas as pd
 # Third party imports
 
 # Local application imports
 # from controllable.Move import Cartesian, Jointed
 # from controllable.builds import mySetup
-from controllable.Measure.Electrical.Biologic import biologic_utils
+# from controllable.Measure.Electrical.Biologic import biologic_utils
+from controllable.Analyse.Data.Types.eis_datatype import ImpedanceSpectrum
 print(f"Import: OK <{__name__}>")
 
+
+if __name__ == "__main__":
+    df = pd.read_csv('src/utils/characterisation/electrical/examples/biologic_test3.csv', header=1)    
+    name_map = {
+            "Impendance phase": "Impedance phase [rad]",
+            "Impendance_ce phase": "Impedance_ce phase [rad]"
+        }
+    df.rename(columns=name_map, inplace=True)
+    
+    spectrum = ImpedanceSpectrum(df, instrument='biologic_')
+    spectrum.plotNyquist()
+    spectrum.fit()
+    spectrum.getCircuitDiagram()
+    spectrum.plotNyquist()
+    pass
 # %%
