32f17b766396b21fde8fb3496d89bfaa203c6485
Chang Jie Leong <changjie9@gmail.com>
2022-11-03 18:00:34 +0800

    Update testing_ground.py

--- testing_ground.py
+++ testing_ground.py
@@ -14,22 +14,11 @@ import pandas as pd
 # from controllable.Move import Cartesian, Jointed
 # from controllable.builds import mySetup
 # from controllable.Measure.Electrical.Biologic import biologic_utils
-from controllable.Analyse.Data.Types.eis_datatype import ImpedanceSpectrum
+# from controllable.Analyse.Data.Types.eis_datatype import ImpedanceSpectrum
+from controllable.Measure.Electrical import Biologic, Keithley
 print(f"Import: OK <{__name__}>")
 
-
 if __name__ == "__main__":
-    df = pd.read_csv('src/utils/characterisation/electrical/examples/biologic_test3.csv', header=1)    
-    name_map = {
-            "Impendance phase": "Impedance phase [rad]",
-            "Impendance_ce phase": "Impedance_ce phase [rad]"
-        }
-    df.rename(columns=name_map, inplace=True)
-    
-    spectrum = ImpedanceSpectrum(df, instrument='biologic_')
-    spectrum.plotNyquist()
-    spectrum.fit()
-    spectrum.getCircuitDiagram()
-    spectrum.plotNyquist()
+    keith = Keithley.Keithley(name='keith')
     pass
 # %%
