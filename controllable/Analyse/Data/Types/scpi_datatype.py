9dcda6868111afc304c7b5e0bf1198f33fa3699b
Chang Jie Leong <changjie9@gmail.com>
2022-11-03 15:07:58 +0800

    Update scpi_datatype.py
    - changed input handling
    - rename parse to getPrompts
    - changed output of getPrompts from list to dict

--- controllable/Analyse/Data/Types/scpi_datatype.py
+++ controllable/Analyse/Data/Types/scpi_datatype.py
@@ -6,27 +6,49 @@ Created: Tue 2022/11/02 17:13:35
 Notes / actionables:
 -
 """
-
 class SCPI(object):
     """
     SCPI input class for Keithley.
     
     Args:
         string (str): text string of SCPI commands or filename of txt file where SCPI commands are saved
-        scpi_list (list): list of SCPI commands line-by-line
+        scpi_list (list): list of SCPI prompts for settings, inputs, and outputs
     """
-    def __init__(self, string='', scpi_list=[]):
-        if len(string) == 0 and len(scpi_list):
-            scpi_join = ['\n'.join(s) for s in scpi_list]
-            string = '\n###\n'.join(scpi_join)
-        elif string.endswith('.txt'):
-            with open(string) as file:
-                string = file.read()
-        if len(string) == 0:
+    def __init__(self, data):
+        self.string = ''
+        self._readData(data)
+        return
+    
+    def _readData(self, data):
+        string = ''
+        if type(data) == dict:
+            for k,v in data.items():
+                data[k] = '\n'.join(v)
+            string = '\n###\n'.join([data['settings'], data['inputs'], data['outputs']])
+        elif type(data) == str:
+            if 'data'.endswith('.txt'):
+                with open(data) as file:
+                    data = file.read()
+            string = data
+        else:
             raise Exception('Please input either filename or SCPI instruction string/list!')
         self.string = string
         return
-
+    
+    def getPrompts(self):
+        """
+        Parse SCPI command into blocks corresponding to settings prompt, input prompt, and output prompt.
+        
+        Returns:
+            dict: SCPI prompts for settings, inputs, and outputs
+        """
+        sections = ['settings', 'inputs', 'outputs']
+        scpi_dict = {}
+        scpi_split = self.string.split('###')
+        for i,s in enumerate(scpi_split):
+            scpi_dict[sections[i]] = [l.strip() for l in s.split('\n') if len(l)]
+        return scpi_dict
+    
     def replace(self, inplace=False, **kwargs):
         """
         Replace placeholder text in SCPI commands with desired values.
@@ -38,25 +60,12 @@ class SCPI(object):
             str: SCPI commands with desired values
         """
         string = self.string
-        for k,v in kwargs.items():
-            if type(v) == bool:
-                v = 'ON' if v else 'OFF'
-            string = string.replace(f'<{k}>', str(v))
+        string = string.format(**kwargs)
+        string = string.replace('True', 'ON')
+        string = string.replace('False', 'OFF')
         if inplace:
             self.string = string
             return
         return string
 
-    def parse(self):
-        """
-        Parse SCPI command into blocks corresponding to settings prompt, input prompt, and output prompt.
-        
-        Returns:
-            list: SCPI prompts for settings, inputs, and outputs
-        """
-        scpi_split = self.string.split('###')
-        for i,s in enumerate(scpi_split):
-            scpi_split[i] = [l.strip() for l in s.split('\n') if len(l)]
-        if len(scpi_split) == 1:
-            scpi_split = scpi_split[0]
-        return scpi_split
+# %%
