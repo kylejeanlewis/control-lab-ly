f5e707c09d2e6a75828b113694d99721f07c4fda
Chang Jie Leong <changjie9@gmail.com>
2022-11-03 17:35:36 +0800

    Create base_programs.py

---
+++ controllable/Measure/Electrical/Keithley/programs/base_programs.py
@@ -0,0 +1,18 @@
+# %% -*- coding: utf-8 -*-
+"""
+Created: Tue 2022/11/02 17:13:35
+@author: Chang Jie
+
+Notes / actionables:
+-
+"""
+# Local application imports
+from .....Analyse.Data.Types.scpi_datatype import SCPI
+print(f"Import: OK <{__name__}>")
+
+class Program(object):
+    def __init__(self) -> None:
+        pass
+    
+    def run(self, inst):
+        return
