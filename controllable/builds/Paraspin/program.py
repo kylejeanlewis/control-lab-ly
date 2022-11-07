c8e8ae211e9a33fdd635a6688b1eb35d0e1c2b22
Chang Jie Leong <changjie9@gmail.com>
2022-11-07 18:11:19 +0800

    Create program.py

---
+++ controllable/builds/Paraspin/program.py
@@ -0,0 +1,52 @@
+# %% -*- coding: utf-8 -*-
+"""
+Adapted from @jaycecheng spinutils
+
+Created: Tue 2022/11/01 17:13:35
+@author: Chang Jie
+
+Notes / actionables:
+-
+"""
+# Standard library imports
+import numpy as np
+import pandas as pd
+import threading
+import time
+
+# Third party imports
+
+# Local application imports
+from .routines import Setup
+print(f"Import: OK <{__name__}>")
+
+class Program(object):
+    def __init__(self) -> None:
+        return
+    
+    # GUI methods
+    def _build_window(self):
+        return
+    def _disable_interface(self):
+        return
+    def _loop_gui(self):
+        return
+    
+    # Main methods
+    def assignPlans(self):
+        return
+    def checkInputs(self):
+        return
+    def loadScheduler(self):
+        return
+    def queue(self): #give instructions
+        return
+    def readPlans(self):
+        return
+    def runExperiment(self):
+        return
+    def saveState(self):
+        return
+    def start(self):
+        return
+    
