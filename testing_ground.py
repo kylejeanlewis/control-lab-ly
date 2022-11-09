b4458df66cfa67e7c85afb3bd53c2e21bd9b689f
Chang Jie Leong <changjie9@gmail.com>
2022-11-09 22:43:54 +0800

    Update testing_ground.py
    example for loading Paraspin/program

--- testing_ground.py
+++ testing_ground.py
@@ -15,10 +15,21 @@ import pandas as pd
 # from controllable.builds import mySetup
 # from controllable.Measure.Electrical.Biologic import biologic_utils
 # from controllable.Analyse.Data.Types.eis_datatype import ImpedanceSpectrum
-from controllable.Measure.Electrical import Biologic, Keithley
+# from controllable.Measure.Electrical import Biologic, Keithley
+from controllable.builds.Paraspin import Setup, Program
 print(f"Import: OK <{__name__}>")
 
+REAGENTS_FILE = r'C:\Users\leongcj\Desktop\Astar_git\control-lab-le\controllable\builds\Paraspin\reagents.csv'
+RECIPE_FILE = r'C:\Users\leongcj\Desktop\Astar_git\control-lab-le\controllable\builds\Paraspin\recipe.csv'
+STATE_FILE = 'Paraspin/program/state.yaml'
+
 if __name__ == "__main__":
-    keith = Keithley.Keithley(name='keith')
+    # spinbot = Setup('', ignore_connections=True)
+    spin_program = Program(ignore_connections=False, recover_state_from_file=STATE_FILE)
+    spin_program.loadRecipe(REAGENTS_FILE, RECIPE_FILE)
+    spin_program.setup.labelPosition('fill', (-120,0,0))
+    # spin_program.saveState()
+    # spin_program.prepareSetup()
+    # spin_program.saveState()
     pass
 # %%
