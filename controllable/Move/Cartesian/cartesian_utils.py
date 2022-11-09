cd6bd4ac9a7bd88e5b392cda0f5ef6e59d760028
Chang Jie Leong <changjie9@gmail.com>
2022-11-10 01:35:56 +0800

    Update cartesian_utils.py

--- controllable/Move/Cartesian/cartesian_utils.py
+++ controllable/Move/Cartesian/cartesian_utils.py
@@ -21,7 +21,7 @@ class CNC(Mover):
     Controller for cnc xyz-movements.
     - port: serial port of cnc Arduino
     """
-    def __init__(self, port, xyz_bounds=[(0,0,0), (0,0,0)], Z_safe=np.nan, move_speed=0, verbose=False, **kwargs):
+    def __init__(self, xyz_bounds=[(0,0,0), (0,0,0)], Z_safe=np.nan, move_speed=0, verbose=False, **kwargs):
         self.xyz_bounds = [tuple(xyz_bounds[0]), tuple(xyz_bounds[1])]
         self.mcu = None
         self.heights = {
@@ -31,7 +31,7 @@ class CNC(Mover):
         self.coordinates = (0,0,0)
         
         self.verbose = verbose
-        self._port = port
+        self._port = ''
         self._baudrate = None
         self._timeout = None
         # self._movement_speed = move_speed
