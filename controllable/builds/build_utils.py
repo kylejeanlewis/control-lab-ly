4eee37dc9279069abf62399aaf694f62df00231a
Chang Jie Leong <changjie9@gmail.com>
2022-11-10 18:00:45 +0800

    Create build_utils.py

---
+++ controllable/builds/build_utils.py
@@ -0,0 +1,63 @@
+# %% -*- coding: utf-8 -*-
+"""
+Created: Tue 2022/11/01 17:13:35
+@author: Chang Jie
+
+Notes / actionables:
+-
+"""
+# Standard library imports
+import pkgutil
+import time
+import yaml
+
+# Third party imports
+
+# Local application imports
+print(f"Import: OK <{__name__}>")
+
+class BaseSetup(object):
+    def __init__(self, *args, **kwargs):
+        self.flags = {}
+        self.positions = {}
+        self._config = {}
+        pass
+    
+    def _checkInputs(self, **kwargs):
+        keys = list(kwargs.keys())
+        if any(len(kwargs[key]) != len(kwargs[keys[0]]) for key in keys):
+            raise Exception(f"Ensure the lengths of these inputs are the same: {', '.join(keys)}")
+        return
+    
+    def _connect(self, *args, **kwargs):
+        return
+    
+    def _getClass(self, module, dot_notation):
+        _class = module
+        for child in dot_notation.split('.'):
+            _class = getattr(_class, child)
+        return _class
+    
+class BaseProgram(object):
+    def __init__(self, *args, **kwargs):
+        pass
+    
+    def _isOverrun(self, start_time, timeout):
+        if timeout!=None and time.time() - start_time > timeout:
+            return True
+        return False
+    
+    def _readPlans(self, config_file, config_option):
+        yml = pkgutil.get_data(__name__, config_file).decode('utf-8')
+        configs = yaml.safe_load(yml)
+        config = configs[config_option]
+        return config
+    
+    # GUI methods
+    def _gui_build_window(self, *args, **kwargs):
+        return
+    def _gui_disable_interface(self, *args, **kwargs):
+        return
+    def _gui_loop(self, *args, **kwargs):
+        return
+    
