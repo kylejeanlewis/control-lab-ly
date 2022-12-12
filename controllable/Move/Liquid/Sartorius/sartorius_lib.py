90a031c76dee6b6720a9b7c556ee9e40c893dde3
Chang Jie Leong <changjie9@gmail.com>
2022-12-12 15:20:19 +0800

    Update sartorius_lib.py

--- controllable/Move/Liquid/Sartorius/sartorius_lib.py
+++ controllable/Move/Liquid/Sartorius/sartorius_lib.py
@@ -15,33 +15,35 @@ from enum import Enum
 print(f"Import: OK <{__name__}>")
 
 class ModelInfo(Enum):
-    rLine_0     = dict(
+    BRL0        = dict(
                     resolution=0.5, home_position=30, max_position=443, tip_eject_position=-40,
-                    speed_codes = [0,60,106,164,260,378,448]
+                    capacity=0, speed_codes = [0,60,106,164,260,378,448]
                 )
-    rLine_200   = dict(
+    BRL200      = dict(
                     resolution=0.5, home_position=30, max_position=443, tip_eject_position=-40,
-                    speed_codes = [0,31,52,80,115,150,190]
+                    capacity=200, speed_codes = [0,31,52,80,115,150,190]
                 )
-    rLine_1000  = dict(
+    BRL1000     = dict(
                     resolution=2.5, home_position=30, max_position=443, tip_eject_position=-40,
-                    speed_codes = [0,150,265,410,650,945,1120]
+                    capacity=1000, speed_codes = [0,150,265,410,650,945,1120]
                 )
-    rLine_5000  = dict(
+    BRL5000     = dict(
                     resolution=10, home_position=30, max_position=580, tip_eject_position=-55,
-                    speed_codes = [0,550,1000,1500,2500,3650,4350]
+                    capacity=5000, speed_codes = [0,550,1000,1500,2500,3650,4350]
                 )
 
-class ErrorCodes(Enum):
+class ErrorCode(Enum):
     er1 = 'The command has not been understood by the module'
     er2 = 'The command has been understood but would result in out-of-bounds state'
     er3 = 'LRC is configured to be used and the checksum does not match'
     er4 = 'The drive is on and the command or query cannot be answered'
 
-class SpeedCodes(Enum):
-    rLine_0     = [0,60,106,164,260,378,448]
-    rLine_200   = [0,31,52,80,115,150,190]
-    rLine_1000  = [0,150,265,410,650,945,1120]
-    rLine_5000  = [0,550,1000,1500,2500,3650,4350]
+class StatusCode(Enum):
+    Normal          = 0
+    Braking         = 1
+    Running         = 2
+    Drive_Busy      = 4
+    Running_Busy    = 6
+    General_Error   = 8
 
 # %%
