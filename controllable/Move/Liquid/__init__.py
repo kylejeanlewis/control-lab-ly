bd8236a4c1ea6e28427595966c72dafb02e11373
Chang Jie Leong <changjie9@gmail.com>
2022-12-13 14:28:07 +0800

    Update __init__.py

--- controllable/Move/Liquid/__init__.py
+++ controllable/Move/Liquid/__init__.py
@@ -1,3 +1,3 @@
 from .liquid_utils import LiquidHandler
 from .syringe_utils import SyringeAssembly
-from .Sartorius.sartorius_utils import Sartorius
+from .Sartorius import Sartorius, SartoriusDevice
