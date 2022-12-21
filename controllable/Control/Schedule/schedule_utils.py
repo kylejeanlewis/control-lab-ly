ee730fb65b5e5c7d49aab503908b993e017f5547
Chang Jie Leong <changjie9@gmail.com>
2022-12-21 16:48:58 +0800

    Update schedule_utils.py

--- controllable/Control/Schedule/schedule_utils.py
+++ controllable/Control/Schedule/schedule_utils.py
@@ -13,3 +13,28 @@ import time
 
 # Local application imports
 print(f"Import: OK <{__name__}>")
+
+class Scheduler(object):
+    def __init__(self):
+        return
+    
+    def decideNext(self, statuses, all_steps):
+        for key, steps in all_steps.items():
+            if statuses[key]['busy']:
+                return None
+            if len(steps):
+                return key
+        return None
+    
+    
+class ScanningScheduler(Scheduler):
+    def __init__(self):
+        return
+        
+    def decideNext(self, statuses, all_steps):
+        for key, steps in all_steps.items():
+            if statuses[key]['busy']:
+                continue
+            if len(steps):
+                return key
+        return None
