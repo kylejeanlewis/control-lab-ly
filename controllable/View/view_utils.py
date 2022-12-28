360d2c246d98a49e3c803a6eb59f3fd605959699
kylejeanlewis <changjie9@gmail.com>
2022-12-28 16:01:54 +0800

    Separate Optical and Thermal utils

--- controllable/View/view_utils.py
+++ controllable/View/view_utils.py
@@ -9,15 +9,13 @@ Notes / actionables:
 # Standard library imports
 import numpy as np
 import pandas as pd
-import pkgutil
 
 # Third party imports
 import cv2 # pip install opencv-python
 
 # Local application imports
-from . import Image
+from .image_utils import Image
 from .Classifiers import Classifier
-from .Thermal.Flir.ax8.ax8 import Ax8ThermalCamera
 print(f"Import: OK <{__name__}>")
 
 class Camera(object):
@@ -291,79 +289,3 @@ class Camera(object):
         except SystemError:
             print('Please select a classifier.')
         return
-
-
-class Optical(Camera):
-    """
-    Optical camera object
-
-    Args:
-        calibration_unit (int, optional): calibration of pixels to mm. Defaults to 1.
-        cam_size (tuple, optional): width and height of image. Defaults to (640,480).
-        rotation (int, optional): rotation of camera feed. Defaults to 0.
-    """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._connect()
-        
-        img_bytes = pkgutil.get_data(__name__, 'placeholders/optical_camera.png')
-        self._set_placeholder(img_bytes=img_bytes)
-        return
-    
-    def _connect(self):
-        """
-        Connect to the imaging device
-        """
-        self.feed = cv2.VideoCapture(0)
-        self._flags['isConnected'] = True
-        return
-
-
-class Thermal(Camera):
-    """
-    Thermal camera object
-
-    Args:
-        ip_address (str): IP address of thermal camera
-        calibration_unit (int, optional): calibration of pixels to mm. Defaults to 1.
-        cam_size (tuple, optional): width and height of image. Defaults to (640,480).
-        rotation (int, optional): rotation of camera feed. Defaults to 0.
-    """
-    def __init__(self, ip_address:str, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.ip_address = ip_address
-        self.rotation = 180
-        self._connect()
-        
-        img_bytes = pkgutil.get_data(__name__, 'placeholders/infrared_camera.png')
-        self._set_placeholder(img_bytes=img_bytes)
-        return
-    
-    def _connect(self):
-        """
-        Connect to the imaging device
-        """
-        self.device = Ax8ThermalCamera(self.ip_address, verbose=False)
-        if self.device.modbus.is_open:
-            self.feed = self.device.video.stream
-            self._flags['isConnected'] = True
-        return
-    
-    def _read(self):
-        """
-        Read camera feed
-
-        Returns:
-            bool, array: True if frame is obtained; array of frame
-        """
-        return self.device.modbus.is_open, self.feed.read()
-    
-    def _release(self):
-        """
-        Release the camera feed
-        """
-        if self.device.modbus.is_open:
-            self.feed.stop()
-            self.feed.stream.release()
-        return
-    
