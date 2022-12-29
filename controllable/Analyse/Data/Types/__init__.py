05f2ca6ec62cef7b63e90b20a394d806e7cd861c
kylejeanlewis <changjie9@gmail.com>
2022-12-29 16:51:51 +0800

    Update __init__.py
    - added TYPES_LIST

--- controllable/Analyse/Data/Types/__init__.py
+++ controllable/Analyse/Data/Types/__init__.py
@@ -1,3 +1,5 @@
 from .circuit_datatype import CircuitDiagram
 from .eis_datatype import ImpedanceSpectrum
 from ._scpi_datatype import SCPI
+
+TYPES_LIST = ['CircuitDiagram','ImpedanceSpectrum','SCPI']
