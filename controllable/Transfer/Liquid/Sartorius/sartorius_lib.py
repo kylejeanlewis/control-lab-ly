9b0dd69f2a11ec7fc370e381d119416807708a96
Chang Jie Leong <changjie9@gmail.com>
2023-01-04 15:12:50 +0800

    Update sartorius_lib.py

--- controllable/Transfer/Liquid/Sartorius/sartorius_lib.py
+++ controllable/Transfer/Liquid/Sartorius/sartorius_lib.py
@@ -12,11 +12,12 @@ from enum import Enum
 # Local application imports
 print(f"Import: OK <{__name__}>")
 
-ERRS = ['er1','er2','er3','er4']
-STATUS_QUERIES = ['DS','DE','DP','DN']
-STATIC_QUERIES = ['Dv','DM','DX','DI','DO','DR']
-QUERIES = STATUS_QUERIES + STATIC_QUERIES
-
+class ErrorCode(Enum):
+    er1 = 'The command has not been understood by the module'
+    er2 = 'The command has been understood but would result in out-of-bounds state'
+    er3 = 'LRC is configured to be used and the checksum does not match'
+    er4 = 'The drive is on and the command or query cannot be answered'
+    
 class ModelInfo(Enum):
     BRL0        = dict(
                     resolution=0.5, home_position=30, max_position=443, tip_eject_position=-40,
@@ -35,16 +36,31 @@ class ModelInfo(Enum):
                     capacity=5000, speed_codes=[0,550,1000,1500,2500,3650,4350]
                 )
 
-class ErrorCode(Enum):
-    er1 = 'The command has not been understood by the module'
-    er2 = 'The command has been understood but would result in out-of-bounds state'
-    er3 = 'LRC is configured to be used and the checksum does not match'
-    er4 = 'The drive is on and the command or query cannot be answered'
+class StaticQueryCode(Enum):
+    Version         = 'DV'
+    Model           = 'DM'
+    Cycles          = 'DX'
+    Speed_In        = 'DI'
+    Speed_Out       = 'DO'
+    Resolution      = 'DR'
+    
+class StatusQueryCode(Enum):
+    Status          = 'DS'
+    Errors          = 'DE'
+    Position        = 'DP'
+    Liquid_Sensor   = 'DN'
 
 class StatusCode(Enum):
-    Normal          = '0'
-    Braking         = '1'
-    Running         = '2'
-    Drive_Busy      = '4'
-    Running_Busy    = '6'
-    General_Error   = '8'
+    Normal          = 0
+    Braking         = 1
+    Running         = 2
+    Drive_Busy      = 4
+    Running_Busy    = 6
+    General_Error   = 8
+
+ERRORS          = [error.name for error in ErrorCode]
+MODELS          = [model.name for model in ModelInfo]
+STATIC_QUERIES  = [static_query.value for static_query in StaticQueryCode]
+STATUS_QUERIES  = [status_query.value for status_query in StatusQueryCode]
+STATUSES        = [status.value for status in StatusCode]
+QUERIES         = STATUS_QUERIES + STATIC_QUERIES
