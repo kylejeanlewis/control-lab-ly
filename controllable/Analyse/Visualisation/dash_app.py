b26306bb3cb501c7f9f4e7b3438ebe1533870e1b
Chang Jie Leong <changjie9@gmail.com>
2023-01-06 21:33:36 +0800

    Create dash_app.py

---
+++ controllable/Analyse/Visualisation/dash_app.py
@@ -0,0 +1,10 @@
+import dash
+from dash import dcc
+from dash import html
+
+app = dash.Dash()
+app.layout = html.Div([
+    dcc.Graph(figure=fig)
+])
+
+app.run_server(debug=True, use_reloader=False)
