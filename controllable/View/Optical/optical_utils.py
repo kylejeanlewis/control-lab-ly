5cca0918bdbd0e3d9d9a34d1979e244bca9d9452
Chang Jie Leong <changjie9@gmail.com>
2023-01-27 21:37:27 +0800

    Update optical_utils.py
    - add setResolution method
    - add cam_index input for _connect

--- controllable/View/Optical/optical_utils.py
+++ controllable/View/Optical/optical_utils.py
@@ -21,6 +21,7 @@ class Optical(Camera):
     Optical camera object
 
     Args:
+        cam_index (int, optional): address of camera. Defaults to 0.
         calibration_unit (int, optional): calibration of pixels to mm. Defaults to 1.
         cam_size (tuple, optional): width and height of image. Defaults to (640,480).
         rotation (int, optional): rotation of camera feed. Defaults to 0.
@@ -36,7 +37,22 @@ class Optical(Camera):
     def _connect(self, cam_index=0):
         """
         Connect to the imaging device
+        
+        Args:
+            cam_index (int, optional): address of camera. Defaults to 0.
         """
-        self.feed = cv2.VideoCapture(cam_index)
+        self.feed = cv2.VideoCapture(cam_index, cv2.CAP_DSHOW)
         self._flags['isConnected'] = True
+        self.setResolution()
+        return
+    
+    def setResolution(self, size=(10000,10000)):
+        """
+        Set resolution of camera feed
+
+        Args:
+            size (tuple, optional): width and height of feed in pixels. Defaults to (10000,10000).
+        """
+        self.feed.set(cv2.CAP_PROP_FRAME_WIDTH, size[0])
+        self.feed.set(cv2.CAP_PROP_FRAME_HEIGHT, size[1])
         return
