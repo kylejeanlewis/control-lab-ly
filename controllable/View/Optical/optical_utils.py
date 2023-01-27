7b321538d14fced355f1f94561bc03307664b8a7
Chang Jie Leong <changjie9@gmail.com>
2023-01-27 16:42:19 +0800

    Update optical_utils.py

--- controllable/View/Optical/optical_utils.py
+++ controllable/View/Optical/optical_utils.py
@@ -25,18 +25,18 @@ class Optical(Camera):
         cam_size (tuple, optional): width and height of image. Defaults to (640,480).
         rotation (int, optional): rotation of camera feed. Defaults to 0.
     """
-    def __init__(self, *args, **kwargs):
+    def __init__(self, cam_index=0, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._connect()
+        self._connect(cam_index)
         
         img_bytes = pkgutil.get_data(__name__, 'placeholders/optical_camera.png')
         self._set_placeholder(img_bytes=img_bytes)
         return
     
-    def _connect(self):
+    def _connect(self, cam_index=0):
         """
         Connect to the imaging device
         """
-        self.feed = cv2.VideoCapture(0)
+        self.feed = cv2.VideoCapture(cam_index)
         self._flags['isConnected'] = True
         return
