24971d86f87f9633f47c4a071aa394c2bbe1c82c
Chang Jie Leong <changjie9@gmail.com>
2023-01-31 00:27:50 +0800

    Update program.py
    - create setup from config and layout files

--- controllable/builds/SynthesisB1/program.py
+++ controllable/builds/SynthesisB1/program.py
@@ -7,6 +7,8 @@ Notes / actionables:
 -
 """
 # Standard library imports
+import json
+from collections import namedtuple
 
 # Third party imports
 
@@ -16,24 +18,24 @@ from ...Measure.Physical import MassBalance
 from ...View.Optical import Optical
 print(f"Import: OK <{__name__}>")
 
-here = '\\'.join(__file__.split('\\')[:-1])
-config_file = f"{here}\\config.yaml"
-layout_file = f"{here}\\layout.json"
+REPO = 'control-lab-le'
+here = '/'.join(__file__.split('\\')[:-1])
+root = here.split(REPO)[0]
+config_file = f"{here}/config.yaml"
+layout_file = f"{here}/layout.json"
 
-def run():
-    spinbot = LiquidMoverSetup(config=config_file, config_option=0, layout=layout_file)
+with open(layout_file) as file:
+    layout_dict = json.load(file)
+for slot in layout_dict['slots'].values():
+    slot['filepath'] = f"{root}{slot['filepath']}"
+
+def create_setup():
+    spinbot = LiquidMoverSetup(config=config_file, config_option=0, layout_dict=layout_dict)
     balance = MassBalance('COM8')
     camera = Optical(1)
+    
+    Setup = namedtuple(
+        'Setup', ['setup', 'mover', 'liquid', 'balance', 'camera']
+    )
 
-    setup =spinbot
-    mover = setup.mover
-    liquid = setup.liquid
-
-    # setup.align(setup.deck.get_slot(name='jars').get_well('A1').middle)
-    # balance.toggleRecord(True)
-    # camera.toggleRecord(True, folder='C:/Users/leongcj/Desktop/machine vision', timeout=60)
-    # liquid.aspirate(volume=200, speed=None)
-    # time.sleep(60)
-    # balance.toggleRecord(False)
-    # camera.toggleRecord(False)
-# %%
+    return Setup(spinbot, spinbot.mover, spinbot.liquid, balance, camera)
