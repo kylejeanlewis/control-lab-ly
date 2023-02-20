6bf1f31f6ad26b2428d4cce8308cb21d5bf75162
Chang Jie Leong <changjie9@gmail.com>
2023-02-20 22:05:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-VERSION = '0.1.3.8'
+VERSION = '0.1.3.9'
 DESCRIPTION = 'Lab Equipment Automation Package'
 
 # Setting up
