1c875b54d23cd1cb6c1db59cf1876c57143cb610
Chang Jie Leong <changjie9@gmail.com>
2023-03-09 14:27:07 +0800

    Update tricontinent_utils.py
    - added `volume` property
    - added documentation
    - added message output for compound actions

--- controllably/Transfer/Liquid/Pumps/tricontinent_utils.py
+++ controllably/Transfer/Liquid/Pumps/tricontinent_utils.py
@@ -36,7 +36,7 @@ class TriContinent(Pump):
         self.channel = channel
         self.model = model
         self.name = name
-        self.output_direction = ''
+        self.output_direction = 'right'
         self.step_limit = int(''.join(filter(str.isdigit, self.model)))
         self.syringe_volume = syringe_volume
         self.resolution = self.syringe_volume / self.step_limit
@@ -79,7 +79,21 @@ class TriContinent(Pump):
         error = ErrorCode[code].value if code in ERRORS else 'Unknown'
         return busy, error
     
+    @property
+    def volume(self):
+        return self.position/self.step_limit * self.syringe_volume
+    
     def _is_expected_reply(self, message:str, response:str):
+        """
+        Check whether the response is an expected reply
+
+        Args:
+            message_code (str): two-character message code
+            response (str): response string from device
+
+        Returns:
+            bool: whether the response is an expected reply
+        """
         if len(response) == 0:
             return False
         if response[0] != '/':
@@ -152,6 +166,15 @@ class TriContinent(Pump):
     
     # Decorators
     def _single_action(func):
+        """
+        Turns a method into a single action that runs only if it is not contained in a compound action
+
+        Args:
+            func (Callable): action method
+        
+        Returns:
+            Callable: wrapped method
+        """
         def wrapper(self, *args, **kwargs):
             message = func(self, *args, **kwargs)
             if self._flags.get('execute_now', False):
@@ -160,6 +183,15 @@ class TriContinent(Pump):
         return wrapper
     
     def _compound_action(func):
+        """
+        Turns a method into a compound action that suppresses single actions within from running
+
+        Args:
+            func (Callable): action method
+        
+        Returns:
+            Callable: wrapped method
+        """
         def wrapper(self, *args, **kwargs):
             self.setFlag('execute_now', False)
             message = func(self, *args, **kwargs)
@@ -168,22 +200,59 @@ class TriContinent(Pump):
         return wrapper
     
     @staticmethod
-    def loop(cycles, *args):
+    def loop(cycles:int, *args):
+        """
+        Specify how many times to loop the following actions
+
+        Args:
+            cycles (int): number of times to cycle
+
+        Returns:
+            str: message string
+        """
         return f"g{''.join(args)}G{cycles}"
     
     # Single actions
     @_single_action
     def empty(self, channel:int = None):
+        """
+        Empty the syringe pump
+
+        Args:
+            channel (int, optional): channel address. Defaults to None.
+
+        Returns:
+            str: message string
+        """
         message = "OA0"
         return message
     
     @_single_action
     def fill(self, channel:int = None):
+        """
+        Fill the syringe pump
+
+        Args:
+            channel (int, optional): channel address. Defaults to None.
+
+        Returns:
+            str: message string
+        """
         message = f"IA{self.step_limit}"
         return message
     
     @_single_action
     def initialise(self, output_direction:str = None, channel:int = None):
+        """
+        Empty the syringe pump
+
+        Args:
+            output_direction (str, optional): liquid output direction ('left' or 'right'). Defaults to None.
+            channel (int, optional): channel address. Defaults to None.
+
+        Returns:
+            str: message string
+        """
         if output_direction is None:
             output_direction = self.output_direction
         else:
@@ -195,27 +264,101 @@ class TriContinent(Pump):
         return message
     
     @_single_action
+    def move(self, direction:str, steps:int, channel=None):
+        """
+        Move plunger either up or down
+
+        Args:
+            axis (str): desired direction of plunger (up / down)
+            value (int): number of steps to move plunger by
+            channel (int, optional): channel to move. Defaults to None.
+        Raises:
+            Exception: Axis direction either 'up' or 'down'
+
+        Returns:
+            str: message string
+        """
+        message = ''
+        if direction.lower() in ['up','u']:
+            message =  f"D{abs(steps)}"
+        elif direction.lower() in ['down','d']:
+            message =  f"P{abs(steps)}"
+        else:
+            raise Exception("Please select either 'up' or 'down'")
+        return message
+        
+    @_single_action
     def moveBy(self, steps:int, channel:int = None):
+        """
+        Move plunger by specified number of steps
+
+        Args:
+            steps (int): number of steps to move plunger by >0: aspirate/move down; <0 dispense/move up)
+            channel (int, optional): channel to move by. Defaults to None.
+
+        Returns:
+            str: message string
+        """
         message = f"P{abs(steps)}" if steps >0 else f"D{abs(steps)}"
         return message
     
     @_single_action
     def moveTo(self, position:int, channel:int = None):
+        """
+        Move plunger to specified position
+
+        Args:
+            position (int): desired plunger position
+            channel (int, optional): channel to move to. Defaults to None.
+
+        Returns:
+            str: message string
+        """
         message = f"A{position}"
         return message
     
     @_single_action
-    def setSpeedRamp(self, ramp:int, channel:int = None):
+    def setSpeedRamp(self, ramp:int = 1, channel:int = None):
+        """
+        Set the ramp up between start speed and top speed
+
+        Args:
+            ramp (int): ramp speed
+            channel (int, optional): channel to set. Defaults to None.
+
+        Returns:
+            str: message string
+        """
         message = f"L{ramp}"
         return message
     
     @_single_action
     def setStartSpeed(self, speed:int, channel:int = None):
+        """
+        Set the starting speed of the plunger
+
+        Args:
+            speed (int): starting speed of plunger
+            channel (int, optional): channel to set. Defaults to None.
+
+        Returns:
+            str: message string
+        """
         message = f"v{speed}"
         return message
     
     @_single_action
     def setTopSpeed(self, speed:int, channel:int = None):
+        """
+        Set the top speed of the plunger
+
+        Args:
+            speed (int): top speed of plunger
+            channel (int, optional): channel to set. Defaults to None.
+
+        Returns:
+            str: message string
+        """
         message = f"V{speed}"
         return message
     
@@ -227,12 +370,13 @@ class TriContinent(Pump):
         Args:
             position (str): one of the above positions
             value (int, optional): only for 6-way distribution. Defaults to None.
+            channel (int, optional): channel to set. Defaults to None.
 
         Raises:
             Exception: Please select a valid position
 
         Returns:
-            str: machine message
+            str: message string
         """
         positions = ['I','O','B','E']
         position = position.upper()[0]
@@ -245,26 +389,80 @@ class TriContinent(Pump):
     
     @_single_action
     def wait(self, time_ms:int, channel:int = None):
+        """
+        Wait for a specified amount of time
+
+        Args:
+            time_ms (int): duration in milliseconds
+            channel (int, optional): channel to wait. Defaults to None.
+
+        Returns:
+            str: message string
+        """
         message = f"M{time_ms}"
         return message
     
     # Standard actions
     def isBusy(self):
+        """
+        Check whether the device is busy
+
+        Raises:
+            Exception: Unable to determine whether the device is busy
+
+        Returns:
+            bool: whether the device is busy
+        """
         busy,_ = self.status
         if busy is None and self.device is not None:
             raise Exception('Unable to determine whether the device is busy')
         return busy
+    
+    def isConnected(self):
+        """
+        Check whether the device is connected
+
+        Returns:
+            bool: whether the device is busy
+        """
+        return (self.device is not None)
 
     def queue(self, actions:list = [], channel:int = None):
+        """
+        Queue several commands together before sending to the device
+
+        Args:
+            actions (list, optional): list of actions. Defaults to [].
+            channel (int, optional): channel to set. Defaults to None.
+
+        Returns:
+            str: message string
+        """
         message = ''.join(actions)
         self.action_message = self.action_message + message
         return message
     
     def reset(self, channel:int = None):
+        """
+        Reset and initialise the device
+
+        Args:
+            channel (int, optional): channel to reset. Defaults to None.
+        """
         self.initialise()
         return
     
     def run(self, message:str = None, channel:int = None):
+        """
+        Send the message to the device and run the action
+
+        Args:
+            message (str, optional): message string of commands. Defaults to None.
+            channel (int, optional): channel to run. Defaults to None.
+
+        Returns:
+            str: message string
+        """
         if message is None:
             message = self.action_message
             self.action_message = ''
@@ -277,12 +475,33 @@ class TriContinent(Pump):
         return message
     
     def stop(self, channel:int = None):
+        """
+        Stops the device immediately, terminating any actions in progress or in queue
+
+        Args:
+            channel (int, optional): channel to stop. Defaults to None.
+
+        Returns:
+            str: message string
+        """
         response = self._query('T')
         return response
     
     # Compound actions
     @_compound_action
     def dose(self, volume:int, start_speed:int = 50, top_speed:int = 200, channel:int = None):
+        """
+        Supply a dose of liquid
+
+        Args:
+            volume (int): desired volume of liquid
+            start_speed (int, optional): starting speed of the dosing. Defaults to 50.
+            top_speed (int, optional): top speed of the dosing. Defaults to 200.
+            channel (int, optional): channel to does from. Defaults to None.
+            
+        Returns:
+            str: message string
+        """
         steps = int(volume/self.resolution)
         volume = steps * self.resolution
         self.queue([
@@ -299,12 +518,23 @@ class TriContinent(Pump):
             self.setValve('O'),
             self.moveBy(-abs(steps))
         ])
+        message = self.action_message
         self.run()
         print(f"Dispensing {volume}uL {self.name} is complete")
-        return
+        return message
     
     @_compound_action
     def prime(self, cycles:int, channel:int = None):
+        """
+        Prime the pump by cycling the plunger through its max and min positions
+
+        Args:
+            cycles (int): number of times to cycle
+            channel (int, optional): channel to prime. Defaults to None.
+            
+        Returns:
+            str: message string
+        """
         self.queue([
             self.initialise(),
             self.setStartSpeed(50),
@@ -317,9 +547,10 @@ class TriContinent(Pump):
                 self.moveTo(0)
             )
         ])
+        message = self.action_message
         self.run()
         print(f"Priming of pump {self.channel} complete")
-        return
+        return message
 
 
 class TriContinentEnsemble(Pump):
@@ -371,19 +602,6 @@ class TriContinentEnsemble(Pump):
     def setTopSpeed(self, speed:int, channel:int = None):
         return self.channels.get(channel, self.current_channel).setTopSpeed(speed=speed)
     def setValve(self, position:str, value:int = None, channel:int = None):
-        """
-        Set valve position to one of [I]nput, [O]utput, [B]ypass, [E]xtra
-
-        Args:
-            position (str): one of the above positions
-            value (int, optional): only for 6-way distribution. Defaults to None.
-
-        Raises:
-            Exception: Please select a valid position
-
-        Returns:
-            str: machine message
-        """
         return self.channels.get(channel, self.current_channel).setValve(position=position, value=value)
     def wait(self, time_ms:int, channel:int = None):
         return self.channels.get(channel, self.current_channel).wait(time_ms=time_ms)
