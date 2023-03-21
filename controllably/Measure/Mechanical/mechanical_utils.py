ec05b42ad483e6d70b07a00ca92b71b634839e0f
Chang Jie Leong <changjie9@gmail.com>
2023-03-21 10:23:04 +0800

    Add files for base classes

---
+++ controllably/Measure/Mechanical/mechanical_utils.py
@@ -0,0 +1,16 @@
+# %% -*- coding: utf-8 -*-
+"""
+Created: Tue 2022/11/02 17:13:35
+@author: Chang Jie
+
+Notes / actionables:
+- add multi channel support
+"""
+# Standard library imports
+
+# Local application imports
+from ..measure_utils import Measurer
+print(f"Import: OK <{__name__}>")
+
+class Mechanical(Measurer):
+    ...
---
+++ controllably/Measure/measure_utils.py
@@ -0,0 +1,15 @@
+# %% -*- coding: utf-8 -*-
+"""
+Created: Tue 2022/11/02 17:13:35
+@author: Chang Jie
+
+Notes / actionables:
+- add multi channel support
+"""
+# Standard library imports
+from abc import ABC, abstractmethod
+# Local application imports
+print(f"Import: OK <{__name__}>")
+
+class Measurer(ABC):
+    ...
