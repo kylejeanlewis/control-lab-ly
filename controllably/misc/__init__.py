0d943376d27cd5a7b90de547a7c8922c54edf081
Chang Jie Leong <changjie9@gmail.com>
2023-03-27 15:58:20 +0800

    Update misc\__init__,py

--- controllably/misc/__init__.py
+++ controllably/misc/__init__.py
@@ -1,5 +1,6 @@
 from . import factory as Factory
 from . import helper as Helper
 from . import layout as Layout
+from .factory import include_this_module, modules
 from .logger import Logger, LOGGER
-from .misc_utils import create_configs, create_setup, load_deck, load_setup, set_safety
+from .misc_utils import *
