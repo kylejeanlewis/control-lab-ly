9b95ce66bd9d64f3a6f5bcf4dfe454a9ecdb93bc
Chang Jie Leong <changjie9@gmail.com>
2023-03-30 21:36:31 +0800

    Update __init__.py

--- controllably/Move/Jointed/Dobot/__init__.py
+++ controllably/Move/Jointed/Dobot/__init__.py
@@ -1,1 +1,5 @@
-from .dobot_utils import MG400, M1Pro
+from .m1pro_utils import M1Pro
+from .mg400_utils import MG400
+
+from controllably import include_this_module
+include_this_module(get_local_only=False)
