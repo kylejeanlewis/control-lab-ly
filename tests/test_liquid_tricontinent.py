cea492e040ea5e36eba65f62dd681978aa76c381
Chang Jie Leong <changjie9@gmail.com>
2023-04-05 18:02:03 +0800

    Update test_liquid_tricontinent.py

--- tests/test_liquid_tricontinent.py
+++ tests/test_liquid_tricontinent.py
@@ -1,11 +1,24 @@
 # %%
 import init
 from controllably.Transfer.Liquid.Pumps.TriContinent import TriContinent
-me = TriContinent('COM6', channel=[1,2], model='C3000',capacity=1000, output_right=True, name=['first', 'second'])
+me = TriContinent('COM23', channel=[1,2], model='C3000',capacity=1000, output_right=True, name=['first', 'second'])
 # %%
-me.cycle(2)
-me.move('up', 40)
-me.moveBy(-50)
-me.moveTo(300)
+me.setCurrentChannel(2)
+# %%
+me.prime(1)
+# %%
+me.move(40, up=False)
+# %%
+me.moveBy(150,1) # FIXME: channel parameter does not work
+# %%
+me.moveTo(1500)
+# %%
+me.aspirate(200)
+# %%
+me.dispense(500)
+# %%
 me.getPosition()
+# %%
 me.getStatus()
+
+# %%
