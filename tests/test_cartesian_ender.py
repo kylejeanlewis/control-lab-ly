f8c937036bedc3945893c4f31695517a356d8f45
Chang Jie Leong <changjie9@gmail.com>
2023-04-05 17:59:37 +0800

    Update test_cartesian_ender.py

--- tests/test_cartesian_ender.py
+++ tests/test_cartesian_ender.py
@@ -4,19 +4,25 @@ from controllably.Move.Cartesian import Ender
 from controllably.Control.GUI import MoverPanel
 
 gui = MoverPanel(Ender('COM4'))
-gui.runGUI()
+# gui.runGUI()
 me = gui.tool
+me.__dict__
 # %%
 me.home()
+# %%
 me.moveTo((50,50,50))
+# %%
 me.move('z',-30)
+# %%
 me.moveBy((10,10,5))
+# %%
 me.safeMoveTo((20,40,20))
+# %%
 me.home()
 # %%
-me.heat(30)
+me.setTemperature(30)
+# %%
+me.setTemperature(25)
 # %%
 me.setSpeed()
-# %%
-me.__dict__
 # %%
