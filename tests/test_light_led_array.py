6a21c10815644ac1661b171546ca494b6415927c
Chang Jie Leong <changjie9@gmail.com>
2023-04-05 18:00:58 +0800

    Update test_light_led_array.py

--- tests/test_light_led_array.py
+++ tests/test_light_led_array.py
@@ -1,7 +1,20 @@
 # %% 
 import init
 from controllably.Make.Light import LEDArray
-me = LEDArray('COM18')
-# %%
+me = LEDArray('COM4', channels=[0,1,2,3])
 me.__dict__
+# %%
+me.setPower(255,3, channel=0)
+# %%
+me.setPower(128,4, channel=1)
+# %%
+me.setPower(64,5, channel=2)
+# %%
+me.setPower(32,6, channel=3)
+# %%
+me.getTimedChannels()
+# %%
+me.setPower(200) # LED will turn off if a new power is set before its time is up
+# %%
+me.turnOff()
 # %%
