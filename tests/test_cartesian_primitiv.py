7f62fcb84eaba553132c68197f9f8ddc9219d7c3
Chang Jie Leong <changjie9@gmail.com>
2023-04-05 17:59:41 +0800

    Update test_cartesian_primitiv.py

--- tests/test_cartesian_primitiv.py
+++ tests/test_cartesian_primitiv.py
@@ -4,16 +4,21 @@ from controllably.Move.Cartesian import Primitiv
 from controllably.Control.GUI import MoverPanel
 
 gui = MoverPanel(Primitiv('COM5'))
-gui.runGUI()
+# gui.runGUI()
 me = gui.tool
+me.__dict__
 # %%
 me.home()
+# %%
 me.moveTo((-50,-50,-50))
+# %%
 me.move('z',20)
+# %%
 me.moveBy((-10,-10,-50))
+# %%
 me.safeMoveTo((-20,-40,-90))
+# %%
 me.home()
 # %%
 me.setSpeed()
 # %%
-me.__dict__
