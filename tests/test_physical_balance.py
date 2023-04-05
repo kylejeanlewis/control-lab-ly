eb9a2a6ac4296a579ea98fbdfca41ed2a8eee75a
Chang Jie Leong <changjie9@gmail.com>
2023-04-05 18:01:05 +0800

    Update test_physical_balance.py

--- tests/test_physical_balance.py
+++ tests/test_physical_balance.py
@@ -2,9 +2,13 @@
 import init
 import time
 from controllably.Measure.Physical import MassBalance
-me = MassBalance('COM22')
+import plotly.express as px
+me = MassBalance('COM7')
 # %%
 me.zero()
 me.toggleRecord(True)
 time.sleep(10)
 me.toggleRecord(False)
+# %%
+px.line(me.buffer_df, 'Time', 'Mass')
+# %%
