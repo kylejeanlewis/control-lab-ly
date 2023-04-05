729e4c2d44821c49a5bd258149f759ca9e134030
Chang Jie Leong <changjie9@gmail.com>
2023-04-05 17:59:51 +0800

    Update test_dobot_mg400.py

--- tests/test_dobot_mg400.py
+++ tests/test_dobot_mg400.py
@@ -2,20 +2,34 @@
 import init
 from controllably.Move.Jointed.Dobot import MG400
 from controllably.Control.GUI import MoverPanel
+from controllably.Transfer.Substrate.Dobot import TwoJawGrip
 
-gui = MoverPanel(MG400('192.168.2.8'))
-gui.runGUI()
+gui = MoverPanel(MG400('192.168.2.6'), axes='XYZa')
+# gui.runGUI()
 me = gui.tool
-# %%
-me.home()
-me.moveTo((50,50,50))
-me.move('z',-30)
-me.moveBy((10,10,5))
-me.safeMoveTo((20,40,20))
-me.home()
-# %%
-me.heat(30)
-# %%
-me.setSpeed()
-# %%
 me.__dict__
+# %%
+me.home()
+# %%
+me.moveTo((200,200,0))
+# %%
+me.moveBy((50,50,50))
+# %%
+me.move('z',-80)
+# %%
+me.safeMoveTo((200,200,-20), ascent_speed=20, descent_speed=50)
+# %%
+me.home()
+# %%
+me.rotateBy((50,0,0))
+# %%
+me.rotateTo((-50,0,0))
+# %%
+me.home()
+# %%
+me.toggleAttachment(True, TwoJawGrip)
+# %%
+me.attachment.drop()
+# %%
+me.attachment.grab()
+# %%
