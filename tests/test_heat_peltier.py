63f6938596e31f762a834093bc30e291b54e3c3b
Chang Jie Leong <changjie9@gmail.com>
2023-04-06 16:35:24 +0800

    Update test_heat_peltier.py

--- tests/test_heat_peltier.py
+++ tests/test_heat_peltier.py
@@ -4,6 +4,17 @@ from controllably.Make.Heat import Peltier
 me = Peltier('COM26')
 # %%
 me.toggleRecord(True)
-me.holdTemperature(30, 30)
+# %%
+me.holdTemperature(30, 90)
+# %%
 me.setTemperature(35, blocking=False)
+# %%
 me.toggleRecord(False)
+# %%
+me.getTemperatures()
+# %%
+me.setTemperature(25, blocking=False)
+# %%
+import plotly.express as px
+px.line(me.buffer_df, 'Time', ['Set','Hot','Cold','Power'])
+# %%
