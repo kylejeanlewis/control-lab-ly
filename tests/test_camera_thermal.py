37002153af968afc50e059a092348831547d3796
Chang Jie Leong <changjie9@gmail.com>
2023-04-10 23:32:51 +0800

    Update test_camera_thermal.py

--- tests/test_camera_thermal.py
+++ tests/test_camera_thermal.py
@@ -3,8 +3,7 @@ import init
 from controllably.Control.GUI import ViewerPanel
 from controllably.View.Thermal import Thermal
 
-gui = ViewerPanel(Thermal('192.168.1.111'))
+gui = ViewerPanel(Thermal('192.168.1.111')) # FIXME: unable to connect
 gui.runGUI()
 me = gui.tool
-# %%
 me.__dict__
