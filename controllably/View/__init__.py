350da1fc5d036947e199eaa0c1bafe8743ca6911
Chang Jie Leong <changjie9@gmail.com>
2023-04-21 22:49:06 +0800

    Update __init__.py

--- controllably/View/__init__.py
+++ controllably/View/__init__.py
@@ -2,9 +2,11 @@
 This sub-package imports the base class for camera tools 
 and the class for image data.
 
+Modules:
+    Image
+
 Classes:
     Camera (ABC)
-    Image
 """
 from .view_utils import Camera
 from . import image_utils as Image
