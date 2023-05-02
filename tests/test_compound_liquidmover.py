91275da80bea2aab90fc2783c7c24b0ce8638c9f
Chang Jie Leong <changjie9@gmail.com>
2023-05-02 17:06:45 +0800

    Update test_compound_liquidmover.py
    - changed `attachTip()` test case

--- tests/test_compound_liquidmover.py
+++ tests/test_compound_liquidmover.py
@@ -10,7 +10,7 @@ me.__dict__
 # %%
 me.loadDeck(r"C:\Users\leongcj\Desktop\Astar_git\control-lab-le\library\deck\layoutB1.json")
 # %%
-me.attachTip()
+me.attachTip(start_tip='D4')
 # %%
 me.mover.home()
 # %%
