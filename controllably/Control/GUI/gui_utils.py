f140520ec7d13372d3bee7bc3abf82a872ac431a
Chang Jie Leong <changjie9@gmail.com>
2023-05-04 21:21:56 +0800

    Update gui_utils.py
    - added MultiChannelPanel class

--- controllably/Control/GUI/gui_utils.py
+++ controllably/Control/GUI/gui_utils.py
@@ -1,10 +1,10 @@
 # %% -*- coding: utf-8 -*-
 """
-This module holds the base class for control panels as well as compound panels.
+This module holds the base class for control panels as well as multichannel panels.
 
 Classes:
     Panel (ABC)
-    CompoundPanel (Panel)
+    MultiChannelPanel (Panel)
     
 Other constants and variables:
     HEIGHT (int): height of screen size in number of pixels
@@ -426,121 +426,80 @@ class Panel(ABC):
         return f'-{self.name}{text}'
     
 
-class CompoundPanel(Panel):
+class MultiChannelPanel(Panel):
     """
-    CompoundPanel provides methods to form a multi-tool control panel
+    Abstract Base Class (ABC) for Multi-Channel Panel objects (i.e. GUI panels for tools with multiple channels).
+    ABC cannot be instantiated, and must be subclassed with abstract methods implemented before use.
 
     ### Constructor
     Args:
-        `ensemble` (dict[str, Panel]): dictionary of individual sub-panels
+        `name` (str, optional): name of panel. Defaults to ''.
         `group` (Optional[str], optional): name of group. Defaults to None.
-        
-    ### Attributes
-    - `panels` (dict[str, Panel]): dictionary of individual sub-panels
     
     ### Methods
-    - `close`: exit the application
-    - `getLayout`: build `sg.Column` object
+    #### Abstract
+    - `getChannelPanel`: get the panel layout for a single channel
     - `listenEvents`: listen to events and act on values
+    #### Public
+    - `getLayout`: build `sg.Column` object
     """
     
-    def __init__(self, 
-        ensemble: dict[str, Panel],
-        group: Optional[str] = None,
-        **kwargs
-    ):
-        """
-        Instantiate the class
-
-        Args:
-            ensemble (dict[str, Panel]): dictionary of individual sub-panels
-            group (Optional[str], optional): name of group. Defaults to None.
-        """
-        super().__init__(group=group, **kwargs)
-        self.panels = {key: value for key,value in ensemble.items()}
+    def __init__(self, name:str = '', group:Optional[str] = None):
+        super().__init__(name=name, group=group)
         return
     
-    def close(self):
-        """Exit the application"""
-        for panel in self.panels.values():
-            panel.close()
-        return super().close()
-    
-    def getLayout(self, title:str = 'Control Panel', title_font_level:int = 0, **kwargs) -> sg.Column:
+    @abstractmethod
+    def getChannelPanel(self, channel_id:int, tool:object, **kwargs) -> sg.Column:
         """
-        Build `sg.Column` object
+        Get the panel layout for a single channel
 
         Args:
-            title (str, optional): title of layout. Defaults to 'Control Panel'.
-            title_font_level (int, optional): index of font size from levels in `font_sizes`. Defaults to 0.
+            channel_id (int): channel index
+            tool (object): tool object
 
         Returns:
             sg.Column: Column object
         """
-        font = (self.typeface, self.font_sizes[title_font_level], 'bold')
+        
+    def getLayout(self, title:str = 'Tool Control', title_font_level:int = 1, **kwargs) -> sg.Column:
+        """
+        Build `sg.Column` object
+
+        Args:
+            title (str, optional): title of layout. Defaults to 'Tool Control'.
+            title_font_level (int, optional): index of font size from levels in `font_sizes`. Defaults to 1.
+
+        Returns:
+            sg.Column: Column object
+        """
+        font = (self.typeface, self.font_sizes[title_font_level])
         layout = super().getLayout(title, justification='center', font=font)
+        channels = {}
+        if 'channels' not in dir(self.tool):
+            channels = {self.tool.channel: self.tool}
+        else:
+            channels = self.tool.channels
+        if len(channels) == 0:
+            return layout
         
-        tab_groups = {'main': []}
-        for key, panel in self.panels.items():
-            group = panel.group
-            _layout = panel.getLayout(title_font_level=title_font_level+1)
-            if not group:
-                group = 'main'
-            if group not in tab_groups.keys():
-                tab_groups[group] = []
-            tab_groups[group].append((key, _layout))
-            
-        tab_group_order = ['main', 'viewer', 'mover', 'measurer'] 
-        tab_group_order = tab_group_order + [grp for grp in list(tab_groups.keys()) if grp not in tab_group_order]
-        ordered_tab_groups = OrderedDict()
-        for key in tab_group_order:
-            if key not in tab_groups:
-                continue
-            ordered_tab_groups[key] = tab_groups.get(key)
-        tab_groups = ordered_tab_groups
+        channel_panels = []
+        for channel_id, tool in channels.items():
+            _layout = self.getChannelPanel(channel_id, tool)
+            channel_panels.append((channel_id, _layout))
+        if len(channel_panels) == 1:
+            panel = channel_panels[0][1]
+        else:
+            tabs = [sg.Tab(key, [[_layout]], expand_x=True) for key,_layout in channel_panels]
+            panel = sg.TabGroup(
+                [tabs], tab_location='topleft', key=f'-{self.name}-TABS-', 
+                expand_x=True, expand_y=True
+            )
         
-        panels = []
-        excluded = ['main']
-        for group, _layouts in tab_groups.items():
-            if group == 'main':
-                panels = panels + [_layout for _,_layout in _layouts]
-                continue
-            if len(_layouts) == 1:
-                panels.append(_layouts[0][1])
-                excluded.append(group)
-            else:
-                tabs = [sg.Tab(key, [[_layout]], expand_x=True) for key,_layout in tab_groups[group]]
-                tab_group = sg.TabGroup([tabs], tab_location='bottomright', key=f'-{group}-TABS-', 
-                                        expand_x=True, expand_y=True)
-                tab_groups[group] = tab_group
-                panels.append(tab_group)
-        # panels = panels + [element for group,element in tab_groups.items() if group not in excluded]
-        panel_list = [panels[0]]
-        for p in range(1, len(panels)):
-            panel_list.append(sg.VerticalSeparator(color="#ffffff", pad=5))
-            panel_list.append(panels[p])
-        
-        suite = sg.Column([panel_list], vertical_alignment='top')
+        suite = sg.Column([[panel]], vertical_alignment='top')
         layout = [
             [layout],
+            [sg.Push()],
             [suite]
         ]
         layout = sg.Column(layout, vertical_alignment='top')
         return layout
-    
-    def listenEvents(self, event:str, values:dict[str, str]) -> dict[str, dict]:
-        """
-        Listen to events and act on values
-
-        Args:
-            event (str): event triggered
-            values (dict[str, str]): dictionary of values from window
-
-        Returns:
-            dict: dictionary of updates
-        """
-        updates = {}
-        for panel in self.panels.values():
-            update = panel.listenEvents(event, values)
-            updates.update(update)
-        return updates
