56517490d936441b95e8d7fc68f5781b7549a736
Chang Jie Leong <changjie9@gmail.com>
2023-05-08 19:01:25 +0800

    Create test_mixture_shaker.py

---
+++ tests/test_mixture_shaker.py
@@ -0,0 +1,13 @@
+# %% 
+import init
+from controllably.Make.Mixture.QInstruments import BioShake
+me = BioShake('COM26')
+
+# %%
+import numpy as np
+type(np.nan) == float
+# %%
+bool(int(2.000)%2)
+# %%
+int(True)
+# %%
