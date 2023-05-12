c7e183976c16516a84d58e4e499f92bd07ce8956
Chang Jie Leong <changjie9@gmail.com>
2023-05-12 13:34:13 +0800

    Update __init__.py

--- controllably/Measure/Chemical/Sentron/__init__.py
+++ controllably/Measure/Chemical/Sentron/__init__.py
@@ -4,7 +4,7 @@ This sub-package imports the classes for chemical measurement tools from Sentron
 Classes:
     pHMeter (Measurer)
 """
-from .phmeter_utils import SentronProbe
+from .pHmeter_utils import SentronProbe
 
 from controllably import include_this_module
 include_this_module(get_local_only=False)
