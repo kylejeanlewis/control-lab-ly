86ab9c969f0ac516f5650c9594230c9ae2d25186
Chang Jie Leong <changjie9@gmail.com>
2023-05-17 13:44:31 +0800

    Move common math operations to `toolbox.py`

--- controllably/Analyse/Data/Impedance/eis_utils.py
+++ controllably/Analyse/Data/Impedance/eis_utils.py
@@ -1,6 +1,12 @@
 # %% -*- coding: utf-8 -*-
 """
+This module holds utility functions for EIS analysis.
 
+Functions:
+    analyse
+    generate_guess
+    nudge_points
+    trim
 """
 # Standard library imports
 from __future__ import annotations
@@ -9,12 +15,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy.signal import argrelextrema
-from typing import Optional
 
 # Third party imports
 from impedance.models.circuits.fitting import extract_circuit_elements
 
 # Local application imports
+from ..toolbox import intersection, perpendicular_bisector
 print(f"Import: OK <{__name__}>")
 
 def analyse(data:pd.DataFrame, order:int = 4) -> tuple:
@@ -211,27 +217,6 @@ def generate_guess(
             new_constants[k] = constants[k]
     return init_guess, new_constants
 
-def intersection(L1:tuple, L2:tuple) -> tuple[Optional[float], Optional[float]]:
-    """
-    Get the intersection between two lines
-
-    Args:
-        L1 (tuple): line 1
-        L2 (tuple): line 2
-
-    Returns:
-        tuple[Optional[float], Optional[float]]: x,y values of intersection. (None,None) if intersection not found.
-    """
-    D  = L1[0] * L2[1] - L1[1] * L2[0]
-    Dx = L1[2] * L2[1] - L1[1] * L2[2]
-    Dy = L1[0] * L2[2] - L1[2] * L2[0]
-    if D != 0:
-        x_i = Dx / D
-        y_i = Dy / D
-        return x_i,y_i
-    else:
-        return None,None
-    
 def nudge_points(x_values:np.ndarray, y_values:np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     """
     Nudge points to avoid curve from looping on itself
@@ -255,30 +240,6 @@ def nudge_points(x_values:np.ndarray, y_values:np.ndarray) -> tuple[np.ndarray, 
             # x_corr = y_diff/avg_y_x
             # x_values[i+1:] += x_corr
     return x_values, y_values
-
-def perpendicular_bisector(pt1:tuple, pt2:tuple) -> tuple[float, float, float]:
-    """
-    Get the formula of perpendicular bisector between two points
-
-    Args:
-        pt1 (tuple): x,y of point 1
-        pt2 (tuple): x,y of point 2
-
-    Returns:
-        tuple[float, float, float]: rise; run; _value_
-    """
-    mid = ((pt1[0]+pt2[0])/2, (pt1[1]+pt2[1])/2)
-    slope = (pt1[1] - pt2[1]) / (pt1[0] - pt2[0])
-    b = -1/slope
-    a = mid[1] - b*mid[0]
-    # print(f'slope, intercept: {b}, {a}')
-    p1 = (0, a)
-    p2 = mid
-    
-    A = (p1[1] - p2[1])
-    B = (p2[0] - p1[0])
-    C = (p1[0]*p2[1] - p2[0]*p1[1])
-    return A, B, -C
 
 def trim(f:np.ndarray, Z:np.ndarray, x:int, p:float = 0.15) -> tuple[np.ndarray, np.ndarray]:
     """
