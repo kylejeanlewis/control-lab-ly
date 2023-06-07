b553b9218bea9708cbffa195fd85d98e14bdd97d
Chang Jie Leong <changjie9@gmail.com>
2023-06-08 01:29:33 +0800

    Update guide_panel.py
    - add ability to toggle between all modules and current relevant modules to display

--- controllably/Control/GUI/guide_panel.py
+++ controllably/Control/GUI/guide_panel.py
@@ -10,6 +10,7 @@ Functions:
 """
 # Standard library imports
 from __future__ import annotations
+import copy
 import importlib
 import inspect
 import markdown
@@ -41,7 +42,7 @@ class Guide(Panel):
     - `listenEvents`: listen to events and act on values
     """
     
-    _default_flags = {'revealed': False}
+    _default_flags = {'tree_expanded': False, 'shown_all': False}
     def __init__(self, name: str = 'Guide', **kwargs):
         """
         Instantiate the class
@@ -50,6 +51,7 @@ class Guide(Panel):
             name (str, optional): name of panel. Defaults to 'Guide'.
         """
         super().__init__(name=name, **kwargs)
+        self._stored_tree = None
         return
     
     def getLayout(self, title_font_level: int = 0, **kwargs) -> sg.Column:
@@ -66,7 +68,7 @@ class Guide(Panel):
         font = (self.typeface, self.font_sizes[title_font_level])
         layout = super().getLayout('Guide', justification='center', font=font, **kwargs)
         tree_data = sg.TreeData()
-        tree_data,_ = self._update_tree(
+        tree_data,_ = self._generate_tree(
             tree_data = tree_data,
             index = 1,
             parent_name = '', 
@@ -90,7 +92,7 @@ class Guide(Panel):
                 sg.Combo(
                 [PLACEHOLDER_METHOD], PLACEHOLDER_METHOD, key="-METHODS-", size=(40,1), enable_events=True
                 ),
-                sg.Button("Show All", key="-GET-ALL-", size=(10,1), enable_events=True),
+                sg.Button("Show All", key="-TOGGLE-ALL-", size=(10,1), enable_events=True),
                 sg.Button("Expand", key="-TOGGLE-REVEAL-", size=(10,1), enable_events=True)
             ]
         ]
@@ -149,32 +151,31 @@ class Guide(Panel):
                 self._render_html(html)
         
         # 3. Show all button
-        if event == '-GET-ALL-':
+        if event == '-TOGGLE-ALL-':
             self._import_all()
-            tree_data = sg.TreeData()
-            tree_data,_ = self._update_tree(
-                tree_data = tree_data,
-                index = 1,
-                parent_name = '', 
-                modules_dictionary = modules._modules
-            )
+            tree: sg.Tree = self.window['-TREE-']
+            tree_data = self._update_tree(tree.TreeData)
+            shown_all = self.flags.get('shown_all', False)
+            label = "Show All" if shown_all else "Show Current"
+            updates['-TOGGLE-ALL-'] = dict(text=label)
             updates['-TREE-'] = dict(values=tree_data)
             updates['-METHODS-'] = dict(values=[PLACEHOLDER_METHOD], value=PLACEHOLDER_METHOD)
             updates['-TOGGLE-REVEAL-'] = dict(text='Expand')
             html = self._convert_md_to_html(md_text=DEFAULT_TEXT)
             self._render_html(html)
+            self.setFlag(shown_all=(not shown_all), tree_expanded=False)
         
         # 4. Reveal button
         if event == '-TOGGLE-REVEAL-':
             tree: sg.Tree = self.window['-TREE-']
-            revealed = self.flags.get('revealed', False)
-            if revealed:
+            tree_expanded = self.flags.get('tree_expanded', False)
+            if tree_expanded:
                 self._collapse_tree(tree)
                 updates['-TOGGLE-REVEAL-'] = dict(text='Expand')
             else:
                 self._expand_tree(tree)
                 updates['-TOGGLE-REVEAL-'] = dict(text='Collapse')
-            self.setFlag(revealed = (not revealed))
+            self.setFlag(tree_expanded=(not tree_expanded))
         return updates
     
     # Protected methods
@@ -242,6 +243,46 @@ class Guide(Panel):
             tree.Widget.item(tree_node_id, open=True)
         return
     
+    def _generate_tree(self, 
+        tree_data: sg.TreeData, 
+        index: int, 
+        parent_name: str, 
+        modules_dictionary: dict
+    ) -> tuple[sg.TreeData, int]:
+        """
+        Generate the tree data
+
+        Args:
+            tree_data (sg.TreeData): tree data
+            index (int): object index
+            parent_name (str): name of parent node
+            modules_dictionary (dict): modules dictionary
+
+        Returns:
+            tuple[sg.TreeData, int]: tree data; next index
+        """
+        search_order = sorted(modules_dictionary.items())
+        search_order = [p for p in search_order if not isinstance(p[1], dict)] + [p for p in search_order if isinstance(p[1], dict)]
+        for k, v in search_order:
+            fullname = '.'.join([parent_name, k])
+            obj_type = ''
+            if isinstance(v, dict):
+                doc = v.get("_doc_", "< No documentation >")
+                doc = doc if doc.strip() else "< No documentation >"
+                tree_data.insert(parent_name, fullname, k, values=[obj_type, '', doc])
+                tree_data, index = self._generate_tree(tree_data, index, fullname, v)
+            elif isinstance(v, str):
+                continue
+            else:
+                obj = eval(f"modules.at{fullname}")
+                doc = inspect.getdoc(obj)
+                obj_type = "function" if inspect.isfunction(obj) else obj_type
+                obj_type = "class" if inspect.isclass(obj) else obj_type
+                tree_data.insert(parent_name, fullname, k, values=[obj_type, index, doc])
+                index += 1
+        self.setFlag(tree_expanded=False)
+        return tree_data, index
+    
     def _import_all(self):
         """
         Import all the modules in the package
@@ -278,45 +319,29 @@ class Guide(Panel):
         set_html(html_widget, html)
         return
     
-    def _update_tree(self, 
-        tree_data: sg.TreeData, 
-        index: int, 
-        parent_name: str, 
-        modules_dictionary: dict
-    ) -> tuple[sg.TreeData, int]:
+    def _update_tree(self, current_tree:sg.TreeData) -> sg.TreeData:
         """
-        Update the tree data
+        Save current tree and return stored tree. Generates new tree if none is stored.
 
         Args:
-            tree_data (sg.TreeData): tree data
-            index (int): object index
-            parent_name (str): name of parent node
-            modules_dictionary (dict): modules dictionary
+            current_tree (sg.TreeData): tree data
 
         Returns:
-            tuple[sg.TreeData, int]: tree data; next index
+            sg.TreeData: new tree data
         """
-        search_order = sorted(modules_dictionary.items())
-        search_order = [p for p in search_order if not isinstance(p[1], dict)] + [p for p in search_order if isinstance(p[1], dict)]
-        for k, v in search_order:
-            fullname = '.'.join([parent_name, k])
-            obj_type = ''
-            if isinstance(v, dict):
-                doc = v.get("_doc_", "< No documentation >")
-                doc = doc if doc.strip() else "< No documentation >"
-                tree_data.insert(parent_name, fullname, k, values=[obj_type, '', doc])
-                tree_data, index = self._update_tree(tree_data, index, fullname, v)
-            elif isinstance(v, str):
-                continue
-            else:
-                obj = eval(f"modules.at{fullname}")
-                doc = inspect.getdoc(obj)
-                obj_type = "function" if inspect.isfunction(obj) else obj_type
-                obj_type = "class" if inspect.isclass(obj) else obj_type
-                tree_data.insert(parent_name, fullname, k, values=[obj_type, index, doc])
-                index += 1
-        self.setFlag(revealed=False)
-        return tree_data, index
+        if self._stored_tree is not None:
+            new_tree = copy.deepcopy(self._stored_tree)
+            self._stored_tree = None
+        else:
+            new_tree = sg.TreeData()
+            new_tree,_ = self._generate_tree(
+                tree_data = new_tree,
+                index = 1,
+                parent_name = '', 
+                modules_dictionary = modules._modules
+            )
+        self._stored_tree = current_tree
+        return new_tree
     
 def guide_me():
     """Start guide to view documentation"""
