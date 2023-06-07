95cf269c12a8c8a993ddb1d070dce7ad99a40bf9
Chang Jie Leong <changjie9@gmail.com>
2023-06-07 11:27:21 +0800

    Update __init__.py
    - add Dobot attachment objects at both module locations

--- controllably/Transfer/Substrate/Dobot/__init__.py
+++ controllably/Transfer/Substrate/Dobot/__init__.py
@@ -5,8 +5,10 @@ Classes:
     TwoJawGrip (DobotGripper)
     VacuumGrip (DobotGripper)
 """
-__where__ = 'Move.Jointed.Dobot.attachments'
 from .dobot_attachments import TwoJawGrip, VacuumGrip
 
 from controllably import include_this_module
+include_this_module(get_local_only=False)
+
+__where__ = 'Move.Jointed.Dobot.attachments'
 include_this_module(get_local_only=False)
