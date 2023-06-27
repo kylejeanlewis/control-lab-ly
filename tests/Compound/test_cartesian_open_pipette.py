ef0b75deef60d9bbbdcc66b347ea66f3d7ef98c1
Chang Jie Leong <changjie9@gmail.com>
2023-06-27 12:27:04 +0800

    Create test_cartesian_open_pipette.py

---
+++ tests/Compound/test_cartesian_open_pipette.py
@@ -0,0 +1,38 @@
+# %%
+import init
+from controllably import Factory, Helper, guide_me
+from controllably.Move.Cartesian import Primitiv
+from controllably.Transfer.Liquid.Sartorius import Sartorius
+
+from controllably.Compound.LiquidMover import LiquidMoverSetup
+from controllably.Control.GUI import CompoundPanel
+from controllably.Control.GUI.Basic import MoverPanel, LiquidPanel
+
+# %%
+details = Factory.get_details(Helper.read_yaml('../configs/primitiv2.yaml'))
+us = LiquidMoverSetup(**details['setup']['settings'])
+us.loadDeck(r'C:\Users\leongcj\Desktop\Astar_git\control-lab-le\library\deck\layoutL3.json')
+us.__dict__
+me = us.mover
+you = us.liquid
+me.__dict__
+# %%
+us.attachTip(tip_length=50.8)
+me.__dict__
+# %%
+us.returnTip()
+me.__dict__
+# %%
+us.ejectTip()
+me.__dict__
+# %%
+gui1 = MoverPanel(me, axes='XYZ')
+gui1.runGUI()
+# %%
+gui1 = MoverPanel(me, axes='XYZ')
+gui2 = LiquidPanel(you)
+gui = CompoundPanel(dict(
+    Mover=gui1,
+    Liquid=gui2
+))
+gui.runGUI()
