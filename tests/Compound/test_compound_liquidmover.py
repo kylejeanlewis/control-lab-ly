99b5b988a34bf8ead8a68ee5e5ec6ce0fa84170b
Chang Jie Leong <changjie9@gmail.com>
2023-07-03 10:46:14 +0800

    Update test_compound_liquidmover.py

--- tests/Compound/test_compound_liquidmover.py
+++ tests/Compound/test_compound_liquidmover.py
@@ -1,23 +1,22 @@
 # %%
-import init
+from init import library
 import numpy as np
 from controllably.Compound.LiquidMover import LiquidMoverSetup
 from controllably import Helper, Factory
-
-details = Factory.get_details(Helper.read_yaml('../configs/skwr.yaml'))['setup']
-me = LiquidMoverSetup(**details['settings'])
-me.liquid.getInfo('BRL1000')
-me.__dict__
 # %%
-me.loadDeck(r"C:\Users\leongcj\Desktop\Astar_git\control-lab-le\library\deck\layoutB1.json")
+details = Factory.get_details(Helper.read_yaml(library['configs']['skwr']))
+us = LiquidMoverSetup(**details['setup']['settings'])
+# us.liquid.getInfo('BRL1000')
+us.loadDeck(library['deck']['layoutB1'])
+us.__dict__
 # %%
-me.attachTip(start_tip='D4')
+us.attachTip(start_tip='D4')
 # %%
-me.mover.home()
+us.mover.home()
 # %%
-me.aspirateAt(me.mover.tool_position[0], 200)
+us.aspirateAt(us.mover.tool_position[0], 200)
 # %%
-me.dispenseAt(me.mover.tool_position[0]+np.array((10,10,10)), 200)
+us.dispenseAt(us.mover.tool_position[0]+np.array((10,10,10)), 200)
 # %%
-me.returnTip()
+us.returnTip()
 # %%
