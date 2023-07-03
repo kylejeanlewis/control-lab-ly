548397e4cdf73f369c14eabb70de1d2a99ec162d
Chang Jie Leong <changjie9@gmail.com>
2023-07-03 14:43:45 +0800

    Update test_init.py
    - update library with root directory of current machine

--- tests/test_init.py
+++ tests/test_init.py
@@ -9,4 +9,18 @@ Helper.get_ports()
 
 library = Helper.read_yaml(r'C:\Users\leongcj\Desktop\Astar_git\control-lab-le\library\catalogue.yaml')
 """File reference for layout and config files"""
-library['_root'] = ROOT.replace('\\','/')
+
+def update_root_directory(d: dict):
+    """
+    Updates relative filepaths in library with root directory
+
+    Args:
+        d (dict): library of relative filepaths
+    """
+    for k,v in list(d.items()):
+        if isinstance(v, dict):
+            update_root_directory(v)
+        elif type(v) == str:
+            d[k] = v.replace(REPO, ROOT.replace('\\','/')+REPO)
+
+update_root_directory(library)
