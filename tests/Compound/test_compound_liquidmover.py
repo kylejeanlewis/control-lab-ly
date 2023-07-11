a3e14fac545af283c690313351198df29835fb6d
Chang Jie Leong <changjie9@gmail.com>
2023-07-11 10:31:59 +0800

    Update test_compound_liquidmover.py

--- tests/Compound/test_compound_liquidmover.py
+++ tests/Compound/test_compound_liquidmover.py
@@ -6,7 +6,7 @@ from controllably import Helper, Factory
 # %%
 details = Factory.get_details(Helper.read_yaml(library['configs']['skwr']))
 us = LiquidMoverSetup(**details['setup']['settings'])
-# us.liquid.getInfo('BRL1000')
+us.liquid.getInfo('BRL1000')
 us.loadDeck(library['deck']['layoutB1'])
 us.__dict__
 # %%
@@ -19,4 +19,10 @@ us.aspirateAt(us.mover.tool_position[0], 200)
 us.dispenseAt(us.mover.tool_position[0]+np.array((10,10,10)), 200)
 # %%
 us.returnTip()
+# %%
+from controllably.Control.GUI.Basic import MoverPanel
+from controllably.Transfer.Substrate import Dobot
+gui = MoverPanel(us.mover, axes='XYZa')
+# %%
+gui.runGUI()
 # %%
