b3ae02ca3524db7aafe48433c8002209019e90cf
Chang Jie Leong <changjie9@gmail.com>
2023-07-31 10:35:03 +0800

    Update test_camera_optical.py

--- tests/View/test_camera_optical.py
+++ tests/View/test_camera_optical.py
@@ -1,10 +1,10 @@
 # %%
 import init
-from controllably.Control.GUI.Basic import ViewerPanel
 from controllably.View.Optical import Optical
 
 me = Optical(0)
-gui = ViewerPanel(me)
-gui.runGUI()
+me.view()
 me.__dict__
+# %%
+me.disconnect()
 # %%
