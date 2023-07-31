4e45f4aa840be31db5e6efe0d426696b657940d8
Chang Jie Leong <changjie9@gmail.com>
2023-07-31 10:35:08 +0800

    Update test_camera_thermal.py

--- tests/View/test_camera_thermal.py
+++ tests/View/test_camera_thermal.py
@@ -1,10 +1,22 @@
 # %%
 import init
-from controllably.Control.GUI.Basic import ViewerPanel
-from controllably.View.Thermal import Thermal
+from controllably.View.Thermal import Flir
 
-me = Thermal('192.168.1.111')
-gui = ViewerPanel(me) # FIXME: unable to connect to 192.168.1.120
-gui.runGUI()
+# %%
+me = Flir.AX8('192.168.1.111')   # FIXME: unable to connect to 192.168.1.120
+# me.view()
 me.__dict__
+# %%
+"""
+
+cam.set_spotmeter_parameters(<parameters>)
+cam.enable_spotmeter(instances=[(1,30,40), (2,50,20)])
+cam.get_spotmeter_temps([1,2])
+cam.disable_spotmeter([1,2])
+"""
+# %%
+spots = {1:(50,50)}
+me.enableSpotmeter(spots)
+
+me
 # %%
