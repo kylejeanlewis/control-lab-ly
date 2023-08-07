198df78f22241e3b9f03df63542ce0d2c6a89d27
Chang Jie Leong <changjie9@gmail.com>
2023-08-07 13:18:59 +0800

    Update test_electrical_keithley.py

--- tests/Measure/test_electrical_keithley.py
+++ tests/Measure/test_electrical_keithley.py
@@ -2,7 +2,7 @@
 import init
 from controllably.Measure.Electrical.Keithley import Keithley, programs
 
-me = Keithley('192.168.1.104')
+me = Keithley('192.109.209.100')
 me.__dict__
 # %%
 me.loadProgram(programs.OCV)
@@ -10,5 +10,32 @@ me.measure()
 
 # %%
 me.loadProgram(programs.IV_Scan)
-me.measure(parameters=dict(currents=[0.1,0.2,0.3])) #FIXME: unable to run IV properly
 # %%
+import numpy as np
+import time
+
+currents = np.linspace(0, 10E-9, 11, True)
+# %%
+me.measure(parameters=dict(currents=currents)) # FIXME: unable to run IV properly
+
+# %%
+voltages = np.linspace(0, 10E-9, 11, True)
+count = 1
+# %%
+device = me.device
+device.reset()
+device.sendCommands(['ROUTe:TERMinals FRONT'])
+device.configureSource('voltage', measure_limit=1)
+device.configureSense('current', limit=1, four_point=True, count=count)
+device.makeBuffer()
+device.beep()
+
+for voltage in voltages:
+    device.setSource(value=voltage)
+    device.toggleOutput(on=True)
+    device.run()
+    time.sleep(0.1*count)
+time.sleep(1)
+data_df = device.readAll()
+device.beep()
+device.getErrors()
