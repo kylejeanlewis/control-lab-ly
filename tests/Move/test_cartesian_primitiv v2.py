00177379d1eccb2a06268b4af2df88611c3592bf
Chang Jie Leong <changjie9@gmail.com>
2023-08-07 13:18:43 +0800

    Update test_cartesian_primitiv v2.py

--- tests/Move/test_cartesian_primitiv v2.py
+++ tests/Move/test_cartesian_primitiv v2.py
@@ -3,7 +3,9 @@ from init import library
 from controllably import Factory, Helper, guide_me
 from controllably.Move.Cartesian import Primitiv
 from controllably.Control.GUI.Basic import MoverPanel
-
+import numpy as np
+import time
+import random as rd
 # %%
 details = Factory.get_details(Helper.read_yaml(library['configs']['primitiv2']))
 me = Primitiv(**details['mover']['settings'])
@@ -37,8 +39,41 @@ me.moveTo((-190,-10,-10))
 me.home()
 # %%
 me.setSpeed(10) # NOTE: speed does not change for Primitiv
+
 # %%
-me._query("$$")
+me.move('y',-150)
 # %%
-me.device.write("$$\n".encode("utf-8"))
-me.device.readlines()
+x,y,z = me.tool_position[0]
+me.setSpeed(me.max_speed[2])
+threshold = 0.99
+start = time.time()
+target = np.array((x,y,20))
+me.moveTo(target, wait=False, jog=True)
+while True:
+    num = rd.random()
+    print(num)
+    time.sleep(0.1)
+    if num >= threshold:
+        me.stop()
+        break
+    if time.time() - start > 60:
+        break
+
+threshold = 0.90
+target = np.array((x,y,me.tool_position[0][2]))
+me.move('z',10)
+time.sleep(3)
+
+me.setSpeed(me.max_speed[2]*0.1)
+me.moveTo(target, wait=False, jog=True)
+while True:
+    num = rd.random()
+    print(num)
+    time.sleep(0.1)
+    if num >= threshold:
+        me.stop()
+        break
+    if time.time() - start > 60:
+        break
+me.setSpeed(me.max_speed[2])
+    # %%
