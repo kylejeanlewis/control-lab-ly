e90f851a96817507671024aa8621d8dedc43e8b5
Chang Jie Leong <changjie9@gmail.com>
2023-08-14 17:50:31 +0800

    Update forceclamper_utils.py
    - added `reset()` method
    - modified `clamp()` method

--- controllably/Compound/ForceClamper/forceclamper_utils.py
+++ controllably/Compound/ForceClamper/forceclamper_utils.py
@@ -14,6 +14,8 @@ from ..compound_utils import CompoundSetup
 print(f"Import: OK <{__name__}>")
 
 class Sensor(Protocol):
+    baseline: float
+    calibration_factor: float
     def getValue(self):
         ...
 
@@ -47,7 +49,8 @@ class ForceClampSetup (CompoundSetup):
             components=components,
             **kwargs
         )
-        self.threshold = 0  # TODO: baseline * 1.01
+        # self.threshold = 0  # TODO: baseline * 1.01
+        self.threshold = 1.1 * self.sensor.baseline
         return
     
     # Properties
@@ -59,19 +62,26 @@ class ForceClampSetup (CompoundSetup):
     def sensor(self) -> Sensor:
         return self.components.get('sensor')
     
-    def clamp(self, threshold:Optional[float] = None, retract_height:int = 5, timeout:float = 60):
+    def clamp(
+        self, 
+        threshold:Optional[float] = None, 
+        retract_height:int = 5, 
+        speed_fraction: float = 1.0,
+        timeout:float = 60
+    ):
         threshold = self.threshold if threshold is None else threshold
-        speed = self.mover.max_speed[2]
-        self.mover.setSpeed(speed)
+        speed_z = self.mover.max_speed[2] * speed_fraction
+        _,prevailing_speed = self.mover.setSpeed(speed_z, 'z')
         
         # Quick approach
         target_z = min(self.mover.limits[0][2], self.mover.limits[1][2])
-        target = np.array((*self.mover.tool_position[0][:2],target_z))
+        target = np.array((*self.mover.position[0][:2],target_z))
+        target = self.mover._transform_out(target)
         start = time.time()
         self.mover.moveTo(target, wait=False, jog=True)
         while True:
             time.sleep(0.001)
-            if self.sensor.getValue() >= threshold:
+            if abs(self.sensor.getValue()) >= abs(threshold):
                 self.mover.stop()
                 break
             if time.time() - start > timeout:
@@ -80,26 +90,30 @@ class ForceClampSetup (CompoundSetup):
         # Retract a little to avoid overshooting
         target = self.mover.tool_position[0]
         self.mover.move('z',retract_height)
-        time.sleep(1)
+        time.sleep(2)
         
         # Reduce movement speed and approach again
-        self.mover.setSpeed(speed*0.1)
+        self.mover.setSpeed(speed_z*0.1, 'z')
         start = time.time()
-        self.mover.moveTo(target, wait=False)
+        self.mover.moveTo(target, wait=False, jog=True)
         while True:
             time.sleep(0.001)
-            if self.sensor.getValue() >= threshold:
+            if abs(self.sensor.getValue()) >= abs(threshold):
                 self.mover.stop()
                 break
             if time.time() - start > timeout:
                 break
         
-        self.mover.setSpeed(speed)
+        self.mover.setSpeed(prevailing_speed[2], 'z')
+        return
+    
+    def reset(self):
+        self.mover.home()
         return
     
     def toggleClamp(self, on:bool = False, threshold:Optional[float] = None):
         if on:
-            self.clamp(threshold)
+            self.clamp(threshold=threshold)
         else:
             self.mover.home()
         return
