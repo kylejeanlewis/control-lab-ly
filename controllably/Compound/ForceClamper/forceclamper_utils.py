64c7e6670138a23ec1b43e2bafe3f82cbb80c753
Chang Jie Leong <changjie9@gmail.com>
2023-08-15 17:50:46 +0800

    Update forceclamper_utils.py
    - added documentation
    - added implementation for `reset()`

--- controllably/Compound/ForceClamper/forceclamper_utils.py
+++ controllably/Compound/ForceClamper/forceclamper_utils.py
@@ -1,6 +1,9 @@
 # %% -*- coding: utf-8 -*-
 """
+This module holds the class for force clamp setups.
 
+Classes:
+    ForceClampSetup (CompoundSetup)
 """
 # Standard library imports
 from __future__ import annotations
@@ -9,18 +12,11 @@ import time
 from typing import Optional, Protocol
 
 # Local application imports
-from ...misc.layout import Well
 from ..compound_utils import CompoundSetup
 print(f"Import: OK <{__name__}>")
 
-class Sensor(Protocol):
-    baseline: float
-    calibration_factor: float
-    def getValue(self):
-        ...
-
 class Mover(Protocol):
-    limits: np.ndarray
+    limits: tuple[tuple]
     max_speed: np.ndarray
     tool_position: tuple[np.ndarray]
     def home(self, *args, **kwargs):
@@ -31,8 +27,37 @@ class Mover(Protocol):
         ...
     def stop(self, *args, **kwargs):
         ...
+        
+class Sensor(Protocol):
+    baseline: float
+    def getValue(self):
+        ...
 
-class ForceClampSetup (CompoundSetup):
+class ForceClampSetup(CompoundSetup):
+    """
+    Force Clamp Setup routines
+
+    ### Constructor 
+    Args:
+        `config` (Optional[str], optional): filename of config .yaml file. Defaults to None.
+        `layout` (Optional[str], optional): filename of layout .json file. Defaults to None.
+        `component_config` (Optional[dict], optional): configuration dictionary of components. Defaults to None.
+        `layout_dict` (Optional[dict], optional): dictionary of layout. Defaults to None.
+        `components` (Optional[dict], optional): dictionary of components. Defaults to None.
+    
+    ### Attributes
+    - `threshold` (float): threshold value to trigger clamp to stop
+    
+    ### Properties
+    - `mover` (Mover): movement / translation robot
+    - `sensor` (Sensor): sensor tool to measure force exerted
+    
+    ### Methods
+    - `clamp`: clamp down on object
+    - `reset`: reset the z-height of mover
+    - `toggleClamp`:
+    """
+    
     def __init__(self, 
         config: Optional[str] = None, 
         layout: Optional[str] = None, 
@@ -41,6 +66,16 @@ class ForceClampSetup (CompoundSetup):
         components: Optional[dict] = None,
         **kwargs
     ):
+        """
+        Instantiate the class
+
+        Args:
+            config (Optional[str], optional): filename of config .yaml file. Defaults to None.
+            layout (Optional[str], optional): filename of layout .json file. Defaults to None.
+            component_config (Optional[dict], optional): configuration dictionary of components. Defaults to None.
+            layout_dict (Optional[dict], optional): dictionary of layout. Defaults to None.
+            components (Optional[dict], optional): dictionary of components. Defaults to None.
+        """
         super().__init__(
             config=config, 
             layout=layout, 
@@ -49,8 +84,7 @@ class ForceClampSetup (CompoundSetup):
             components=components,
             **kwargs
         )
-        # self.threshold = 0  # TODO: baseline * 1.01
-        self.threshold = 1.1 * self.sensor.baseline
+        self.threshold = 1.05 * self.sensor.baseline
         return
     
     # Properties
@@ -69,6 +103,15 @@ class ForceClampSetup (CompoundSetup):
         speed_fraction: float = 1.0,
         timeout:float = 60
     ):
+        """
+        Clamp down on object
+
+        Args:
+            threshold (Optional[float], optional): threshold value to trigger clamp to stop. Defaults to None.
+            retract_height (int, optional): z-distance to retract before re-approach at slower speed. Defaults to 5.
+            speed_fraction (float, optional): fraction of max speed to perform initial approach. Defaults to 1.0.
+            timeout (float, optional): timeout in seconds before aborting clamp. Defaults to 60.
+        """
         threshold = self.threshold if threshold is None else threshold
         speed_z = self.mover.max_speed[2] * speed_fraction
         _,prevailing_speed = self.mover.setSpeed(speed_z, 'z')
@@ -108,13 +151,24 @@ class ForceClampSetup (CompoundSetup):
         return
     
     def reset(self):
-        self.mover.home()
+        """Reset the z-height of mover"""
+        target_z = max(self.mover.limits[0][2], self.mover.limits[1][2]) - 5
+        target = np.array((*self.mover.position[0][:2],target_z))
+        target = self.mover._transform_out(target)
+        self.mover.moveTo(target)
         return
     
     def toggleClamp(self, on:bool = False, threshold:Optional[float] = None):
+        """
+        Close or open the clamp
+
+        Args:
+            on (bool, optional): whether to clamp down on sample. Defaults to False.
+            threshold (Optional[float], optional): threshold value to trigger clamp to stop. Defaults to None.
+        """
         if on:
             self.clamp(threshold=threshold)
         else:
-            self.mover.home()
+            self.reset()
         return
     
