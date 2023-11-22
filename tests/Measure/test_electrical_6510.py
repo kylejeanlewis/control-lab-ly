db16ecc9cc2f9cb16914cd142f9489060b5fcd4a
Chang Jie Leong <changjie9@gmail.com>
2023-11-22 15:35:50 +0800

    Update test_electrical_6510.py
    - added example for parameters variable

--- tests/Measure/test_electrical_6510.py
+++ tests/Measure/test_electrical_6510.py
@@ -1,5 +1,5 @@
 # %%
-import init
+# import init
 from controllably.Measure.Electrical.Keithley import Keithley, programs
 
 # %%
@@ -19,4 +19,16 @@ df.rename(columns={
 }, inplace = True)
 df
 
+# %%
+parameters = dict(
+    channel_count = 2, scan_count = 100, scan_interval = 0.1, 
+    fields = ('CHANnel','READing','RELative'), volt_range = 1
+)
+sensor.measure(parameters=parameters)
+
+df = sensor.getData()
+df.rename(columns={
+    'CHANnel':'Channel', 'READing':'Volt', 'RELative':'Relative time'
+}, inplace = True)
+df
 # %%
