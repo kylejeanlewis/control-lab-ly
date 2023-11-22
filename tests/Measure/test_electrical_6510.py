8f47fb0cc5bd9fb04eb7c13758ed65c5fbf59461
Chang Jie Leong <changjie9@gmail.com>
2023-11-22 13:43:12 +0800

    Update test_electrical_6510.py
    - added code for example of using DAQ6510 to scan channels

--- tests/Measure/test_electrical_6510.py
+++ tests/Measure/test_electrical_6510.py
@@ -1,7 +1,22 @@
 # %%
 import init
-from controllably.Measure.Electrical.Keithley import KeithleyDevice
+from controllably.Measure.Electrical.Keithley import Keithley, programs
+
 # %%
-K6510 = KeithleyDevice('192.109.209.102')
-K6510.__dict__
+sensor = Keithley('192.109.209.100', verbose=True)
+sensor.__dict__
+
+# %%
+sensor.loadProgram(program_type=programs.Scan_Channels)
+sensor.measure(
+    channel_count = 2, scan_count = 100, scan_interval = 0.1, 
+    fields = ('CHANnel','READing','RELative'), volt_range = 1
+)
+
+df = sensor.getData()
+df.rename(columns={
+    'CHANnel':'Channel', 'READing':'Volt', 'RELative':'Relative time'
+}, inplace = True)
+df
+
 # %%
