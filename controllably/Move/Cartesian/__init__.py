1c72e7943aa8cd2545dcbcef1fe7de8e19141551
Chang Jie Leong <changjie9@gmail.com>
2024-01-03 15:41:54 +0800

    Update __init__.py
    - import Marlin and Grbl classes

--- controllably/Move/Cartesian/__init__.py
+++ controllably/Move/Cartesian/__init__.py
@@ -7,8 +7,8 @@ Classes:
     Primitiv (Gantry)
 """
 from .cartesian_utils import Gantry
-from .ender_utils import Ender
-from .primitiv_utils import Primitiv
+from .ender_utils import Ender, Marlin
+from .primitiv_utils import Primitiv, Grbl
 
 from controllably import include_this_module
 include_this_module(get_local_only=False)
