a746104c9f864ae2e6a1d68b2b693fa18fd69838
kylejeanlewis <changjie9@gmail.com>
2024-07-23 00:33:04 +0800

    Update test_nicegui.py

--- tests/Control/test_nicegui.py
+++ tests/Control/test_nicegui.py
@@ -1,19 +1,44 @@
 # %%
 from __future__ import annotations
+from abc import ABC, abstractmethod
+import keyboard
 import logging
 import nest_asyncio
+import numpy as np
 import os
 from threading import Thread
-from typing import Optional, Union, Any
+from typing import Optional, Union, Any, Protocol
 import webbrowser
 
 from nicegui import ui, app
-from nicegui.events import ValueChangeEventArguments
+from nicegui.elements.button import Button
+from nicegui.elements.dialog import Dialog
+from nicegui.events import UiEventArguments
+
+nest_asyncio.apply()
 
 logger = logging.getLogger(__name__)
 panels: list[Panel] = []
-logging.basicConfig(level=logging.DEBUG)
-nest_asyncio.apply()
+
+class Mover(Protocol):
+    _place: str
+    heights: dict
+    home_coordinates: tuple
+    tool_position: tuple[np.ndarray]
+    def home(self, *args, **kwargs):
+        ...
+    def move(self, *args, **kwargs):
+        ...
+    def moveTo(self, *args, **kwargs):
+        ...
+    def reset(self, *args, **kwargs):
+        ...
+    def rotateTo(self, *args, **kwargs):
+        ...
+    def safeMoveTo(self, *args, **kwargs):
+        ...
+    def _transform_out(self, *args, **kwargs):
+        ...
 
 # %%
 class Panel:#(ABC):
@@ -25,42 +50,43 @@ class Panel:#(ABC):
     Args:
         `name` (str, optional): name of panel. Defaults to ''.
         `group` (Optional[str], optional): name of group. Defaults to None.
-        `font_sizes` (tuple[int], optional): list of font sizes. Defaults to (14,12,10,8,6).
-        `theme` (str, optional): name of theme. Defaults to 'LightGreen'.
-        `typeface` (str, optional): name of typeface. Defaults to "Helvetica".
+        `panels` (list[Panel], optional): list of sub-panels. Defaults to list().
     
     ### Attributes
-    #### Class
-    - `font_sizes` (tuple[int]): list of font sizes
-    - `theme` (str): name of theme
-    - `typeface` (str): name of typeface
-    #### Instance
     - `flags` (dict[str, bool]): keywords paired with boolean flags
     - `group` (str): name of group
-    - `name` (str): name of panel
+    - `panels` (list[Panel]): list of sub-panels
     - `tool` (Callable): tool to be controlled
-    - `window` (sg.Window): Window object
+    - `values` (dict[str,Any]): values from user interface
+    
+    ### Properties
+    - `host` (str): server host
+    - `name` (str): name of panel
+    - `page_address` (str): page route
+    - `port` (int): server port
+    - `root_address` (str): host and port of server
     
     ### Methods
     #### Abstract
     - `getLayout`: build `sg.Column` object
     - `listenEvents`: listen to events and act on values
     #### Public
-    - `arrangeElements`: arrange elements in a horizontal, vertical, or cross-shape pattern
-    - `close`: exit the application
+    - `close`: close the browser tab
     - `configure`: configure GUI defaults
-    - `getButtons`: get a list of panel buttons
-    - `getInputs`: get the layout for the input section
-    - `getWindow`: build `sg.Window` object
-    - `pad`: add spacer in GUI
+    - `getMultiPanel`: build multi-panel page
+    - `getPage`: build GUI page
+    - `getWindow`: get landing page window
     - `parseInput`: parse inputs from GUI
+    - `redirect`: redirect to panel's page address
     - `runGUI`: run the GUI loop
     - `setFlag`: set flags by using keyword arguments
+    - `shutdown`: exit the application
     """
     
     _default_flags: dict[str, bool] = dict(notify=True)
     _default_values: dict[str, Any] = dict()
-    _shutdown_dialog = None
+    _shutdown_dialog: Dialog | None = None
+    _thread: list[Thread] = []
     _used_names: list[str] = []
     def __init__(self, 
         name: str = '', 
@@ -73,6 +99,7 @@ class Panel:#(ABC):
         Args:
             name (str, optional): name of panel. Defaults to ''.
             group (Optional[str], optional): name of group. Defaults to None.
+            panels (list[Panel], optional): list of sub-panels. Defaults to list().
         """
         self._name = ''
         self.name = name
@@ -85,13 +112,12 @@ class Panel:#(ABC):
         
         self._host = os.environ.get('NICEGUI_HOST', '0.0.0.0')
         self._port = int(os.environ.get('NICEGUI_PORT', '8080'))
-        self._thread: Thread | None = None
-        # self.configure()
         return
     
     def __del__(self):
-        self.close(force=True)
+        self.shutdown(force=True)
     
+    # Properties
     @property
     def host(self) -> str:
         return '127.0.0.1' if self._host == '0.0.0.0' else self._host
@@ -121,63 +147,54 @@ class Panel:#(ABC):
     
     # @abstractmethod
     def getLayout(self):
-        """
-        Build the UI layout
-
-        Args:
-            title (str, optional): title of layout. Defaults to 'Panel'.
-            title_font_level (int, optional): index of font size from levels in `font_sizes`. Defaults to 0.
-
-        Returns:
-            sg.Column: Column object
-        """
-        self.values = dict(
-            checkbox1=True,
+        """Build the UI layout"""
+        self.values.update(dict(
+            checkbox=True,
             notify=False,
-            radio1='B',
-            input1='type here',
-            select1=None
-        )
+            radio='B',
+            input='type here',
+            select=None
+        ))
         with ui.card():
             ui.markdown(f'## {self.name}')
-            ui.button('Button', on_click=lambda: ui.notify('Click'))
+            ui.button('Button', on_click=self.listenEvents)
             with ui.row():
-                ui.checkbox('Checkbox', on_change=self.showValues).bind_value(self.values, 'checkbox1')
-                ui.switch('Switch', on_change=self.showValues).bind_value(self.values, 'notify')
-            ui.radio(['A', 'B', 'C'], value='A', on_change=self.showValues).props('inline').bind_value(self.values, 'radio1')
+                ui.checkbox('Checkbox', on_change=self.listenEvents).bind_value(self.values, 'checkbox')
+                ui.switch('Switch', on_change=self.listenEvents).bind_value(self.flags, 'notify')
+            ui.radio(['A', 'B', 'C'], value='A', on_change=self.listenEvents).props('inline').bind_value(self.values, 'radio')
             with ui.row():
-                ui.input('Text input', on_change=self.showValues).bind_value(self.values, 'input1')
-                ui.select(['One', 'Two'], value='One', on_change=self.showValues).bind_value(self.values, 'select1')
+                ui.input('Text input', on_change=self.listenEvents).bind_value(self.values, 'input')
+                ui.select(['One', 'Two'], value='One', on_change=self.listenEvents).bind_value(self.values, 'select')
             ui.link('And many more...', '/documentation').classes('mt-8')
         return
 
     # @abstractmethod
-    def listenEvents(self, ui_event: ValueChangeEventArguments) -> dict[str, str]:
+    def listenEvents(self, ui_event: UiEventArguments) -> dict[str, Any]:
         """
         Listen to events and act on values
 
         Args:
-            event (str): event triggered
-            values (dict[str, str]): dictionary of values from window
+            ui_event (UiEventArguments): event object from NiceGUI
 
         Returns:
-            dict: dictionary of updates
+            dict: dictionary of values
         """
         event = type(ui_event.sender).__name__
+        if event.lower() == 'button':
+            sender: Button = ui_event.sender
+            value = sender.text
+            self.values.update(dict(click=value))
         values = self.values
-        return self.showValues(ui_event=ui_event)
+        
+        ...
+        return self._show_values(ui_event=ui_event)
 
     @classmethod
-    def close(cls, force: bool = False):
-        """Exit the application"""
-        if not force:
-            proceed = input('Once GUI server closed, unable to start up again until kernel is restarted. Proceed? (y/n)')
-            if proceed.strip().lower() != 'y':
-                logger.warning('Abort closing GUI server.')
-                return
+    def close(cls):
+        """Close the browser tab"""
         try:
             cls._shutdown_dialog.close()
-            app.shutdown()
+            keyboard.press_and_release('ctrl+w')
         except Exception as e:
             logger.exception(e)
         return
@@ -185,29 +202,25 @@ class Panel:#(ABC):
     @classmethod
     def configure(cls, **kwargs):
         """Configure GUI defaults"""
+        ui.dark_mode(True)
         with ui.dialog() as dialog, ui.card():
-            def _close():
-                cls._shutdown_dialog.close()
-                cls.close(force=True)
-                return
             cls._shutdown_dialog = dialog
-            ui.label('Once GUI server closed, unable to start up again until kernel is restarted. Proceed?')
+            ui.label('Once shutdown, kernel needs to be restarted to open the GUI server again.')
             with ui.row().classes('w-full justify-center'):
-                ui.button('No', on_click=cls._shutdown_dialog.close)
-                ui.button('Yes', on_click=_close, color='negative')
+                ui.button('Cancel', on_click=cls._shutdown_dialog.close)
+                ui.button('Close tab', on_click=cls.close)
+                ui.button('Shutdown', on_click=lambda: cls.shutdown(True), color='negative')
         return
 
     def getMultiPanel(self):
-        """
-        Build multi-panel page
-        """
+        """Build multi-panel page"""
         @ui.page(self.page_address)
         async def get_multi_panel_page():
-            """Build multi panel page"""
+            """Build multi-panel page"""
             self.configure()
             with ui.row().classes('w-full justify-between items-center'):
                 ui.markdown(f'# {self.name}')
-                ui.button('Shutdown', on_click=self._shutdown_dialog.open, color='negative')
+                ui.button('✖', on_click=self._shutdown_dialog.open, color='negative')
                 
             with ui.row().classes('w-full justify-center'):
                 for panel in self.panels:
@@ -228,7 +241,7 @@ class Panel:#(ABC):
             """Build GUI page"""
             self.configure()
             self.getLayout()
-            ui.button('Shutdown', on_click=self._shutdown_dialog.open, color='negative')
+            ui.button('✖', on_click=self._shutdown_dialog.open, color='negative')
             
             await ui.context.client.connected()
             logging.debug(f'Connected: {self.root_address}{self.page_address}')
@@ -238,9 +251,7 @@ class Panel:#(ABC):
         return self.page_address
     
     def getWindow(self):
-        """
-        Get landing page window
-        """
+        """"""
         global panels
         if len(self.panels):
             self.getMultiPanel()
@@ -255,7 +266,7 @@ class Panel:#(ABC):
             self.configure()
             with ui.row().classes('w-full justify-between items-center'):
                 ui.markdown(f'# Control-lab-ly')
-                ui.button('Shutdown', on_click=self._shutdown_dialog.open, color='negative')
+                ui.button('✖', on_click=self._shutdown_dialog.open, color='negative')
                 
             with ui.tabs().classes('w-full') as ui_tabs:
                 tabs = []
@@ -329,17 +340,18 @@ class Panel:#(ABC):
         Run the GUI loop
 
         Args:
-            title (str, optional): title of window. Defaults to 'Application'.
+            title (str, optional): title of window. Defaults to 'Control-lab-ly'.
         """
         # self.configure()
         self.getWindow()
         if 'NICEGUI_HOST' in os.environ:
             webbrowser.open(f'http://{self.root_address}{self.page_address}')
-        elif self._thread is None:
+        elif not len(self._thread):
             self._port = int(os.environ.get('NICEGUI_PORT', '8080'))
             kwargs.update(dict(title=title, reload=False, host=self.host, port=self.port, show=False))
-            self._thread = Thread(target=ui.run, kwargs=kwargs)
-            self._thread.start()
+            thread = Thread(target=ui.run, kwargs=kwargs)
+            self._thread.append(thread)
+            self._thread[0].start()
             webbrowser.open(f'http://{self.root_address}{self.page_address}')
         else:
             logger.error('Unable to run GUI.')
@@ -359,16 +371,99 @@ class Panel:#(ABC):
             self.flags[key] = value
         return
 
-    def showValues(self, ui_event: ValueChangeEventArguments):
-        logger.info(f'{self.name} <- {self.values}')
+    @classmethod
+    def shutdown(cls, force: bool = False):
+        """Exit the application"""
+        if not force:
+            proceed = input('Once shut down, kernel needs to be restarted to open the GUI server again. Proceed? (y/n)')
+            if proceed.strip().lower() != 'y':
+                logger.warning('Abort closing GUI server.')
+                return
+        try:
+            cls._shutdown_dialog.close()
+            cls.close()
+            app.shutdown()
+        except Exception as e:
+            logger.exception(e)
+        return
+    
+    # Protected method(s)
+    def _show_values(self, ui_event: UiEventArguments) -> dict[str, Any]:
+        """
+        Show the GUI values
+
+        Args:
+            ui_event (UiEventArguments): event object from NiceGUI
+        """
         if self.flags.get('notify',True):
             name = type(ui_event.sender).__name__
-            ui.notify(f'{name}: {ui_event.value}')
+            value = self.values.get('click') if name.lower() == 'button' else getattr(ui_event,'value', name)
+            ui.notify(f'{name}: {value}')
+        logger.info(f'{self.name} <- {self.values}')
+        return self.values
+
+class MoverPanel(Panel):
+    def __init__(self, 
+        mover: Mover | None = None,
+        name: str = 'Mover', 
+        group: str | None = None, 
+        panels: list[Panel] = list(),
+        axes: Union[list, str] = 'XYZabc', 
+        **kwargs
+    ):
+        super().__init__(name, group, panels, **kwargs)
+        self.tool = mover
+        
+        self.axes = [*axes]
+        self.buttons = {}
+        self.current_attachment = ''
+        self.attachment_methods = []
+        self.method_map = {}
+        
+        self.flags['update_position'] = True
         return
+    
+    # Properties
+    @property
+    def mover(self) -> Mover:
+        return self.tool
+    
+    def getLayout(self):
+        axes = ['X','Y','Z','a','b','c']
+        increments = ['-10','-1','-0.1',0,'+0.1','+1','+10']
+        zpad = ['Z-10','Z-1','Z-0.1','Safe','Z+0.1','Z+1','Z+10']
+        dpad_positions = {
+            3:'Y+10',
+            10:'Y+1',
+            17:'Y+0.1',
+            21:'X-10',22:'X-1',23:'X-0.1',24:'Home',25:'X+0.1',26:'X+1',27:'X+10',
+            31:'Y-0.1',
+            38:'Y-1',
+            45:'Y-10'
+        }
+        with ui.card():
+            ui.markdown(f'## {self.name}')
+            with ui.row():
+                with ui.grid(rows=7,columns=1).classes('gap-0'):
+                    for z in zpad:
+                        color = 'accent' if z.lower() == 'safe' else 'primary'
+                        ui.button(z,color=color, on_click=self.listenEvents)
+                with ui.grid(rows=7,columns=7).classes('gap-0'):
+                    for d in range(7*7):
+                        if d in dpad_positions:
+                            color = 'accent' if dpad_positions[d].lower() == 'home' else 'primary'
+                            ui.button(dpad_positions[d], color=color, on_click=self.listenEvents)
+                        else:
+                            ui.space()
+        return
+    
+    def listenEvents(self, ui_event: UiEventArguments) -> dict[str, Any]:
+        return super().listenEvents(ui_event)
 
 # %%
 if __name__ == '__main__':
-    left = Panel('Left')
+    logging.basicConfig(level=logging.DEBUG)
+    left = MoverPanel()
     right = Panel('Right')
     gui = Panel('Outer', panels=[left,right])
     gui.runGUI()
