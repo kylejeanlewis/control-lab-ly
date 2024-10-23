17fef65614e7a506c1021ac77fe4889cda73f340
Chang Jie Leong <changjie9@gmail.com>
2024-10-23 18:37:59 +0800

    Update test_init.py

--- tests/test_init.py
+++ tests/test_init.py
@@ -7,6 +7,13 @@ sys.path.append(f'{ROOT}{REPO}')
 from controllably import Helper
 Helper.get_ports()
 
-library = Helper.read_yaml(f'{ROOT}{REPO}\\library\\catalogue.yaml')
-"""File reference for layout and config files"""
-Helper.update_root_directory(library, REPO)
+try:
+    library = Helper.read_yaml(f'{ROOT}{REPO}\\library\\catalogue.yaml')
+    """File reference for layout and config files"""
+except FileNotFoundError:
+    print('Catalogue file not found')
+    library = dict()
+try:
+    Helper.update_root_directory(library, REPO)
+except AttributeError:
+    print('Error with handling catalogue file')
