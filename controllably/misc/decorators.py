bd6f5317e6995b4f43d92fbe14815a283fbd310e
Chang Jie Leong <changjie9@gmail.com>
2024-10-25 12:54:34 +0800

    Update decorators.py

--- controllably/misc/decorators.py
+++ controllably/misc/decorators.py
@@ -1,4 +1,4 @@
-# %% -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
 This module holds the decorator functions in Control.lab.ly.
 
@@ -6,20 +6,28 @@ Functions:
     safety_measures (decorator)
 """
 # Standard library imports
+from enum import Enum
 from functools import wraps
-import logging
+from logging import getLogger
 import time
-from typing import Callable, Optional
+from typing import Callable
 
-logger = logging.getLogger(__name__)
-logger.debug(f"Import: OK <{__name__}>")
+logger = getLogger(__name__)
+logger.info(f"Import: OK <{__name__}>")
 
-def safety_measures(mode:Optional[str] = None, countdown:int = 3) -> Callable:
+class Safety(Enum):
+    """
+    Enum for safety modes
+    """
+    WAIT = 'wait'
+    PAUSE = 'pause'
+
+def safety_measures(mode:Safety|str|None = None, countdown:int = 3) -> Callable:
     """
     Wrapper for creating safe move functions
 
     Args:
-        mode (Optional[str], optional): mode for implementing safety measure. Defaults to None.
+        mode (str|None, optional): mode for implementing safety measure. Defaults to None.
         countdown (int, optional): time delay before executing action. Defaults to 3.
         
     Returns:
@@ -42,14 +50,16 @@ def safety_measures(mode:Optional[str] = None, countdown:int = 3) -> Callable:
             str_kwargs = ','.join([f'{k}={v}' for k,v in kwargs.items()])
             str_inputs = ','.join(filter(None, [str_args, str_kwargs]))
             str_call = f"{str_method}({str_inputs})"
-            if mode == 'wait':
-                print(f"Executing in {countdown} seconds:")
-                print(str_call)
-                time.sleep(countdown)
-            elif mode == 'pause':
-                print(f"Executing: {str_call}")
-                time.sleep(0.1)
-                input(f"Press 'Enter' to execute")
+            match mode:
+                case Safety.WAIT.value:
+                    logger.warning(f"Executing in {countdown} seconds: {str_call}")
+                    time.sleep(countdown)
+                case Safety.PAUSE.value:
+                    logger.warning(f"Executing: {str_call}")
+                    time.sleep(0.1)
+                    input(f"Press 'Enter' to execute")
+                case _:
+                    logger.warning(f"Executing: {str_call}")
             return func(*args, **kwargs)
         return wrapper
     return inner
