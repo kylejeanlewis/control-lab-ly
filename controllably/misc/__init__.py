dac4b696d4e6dcbb16b15a6301a289d655528dd1
Chang Jie Leong <changjie9@gmail.com>
2024-10-25 12:49:09 +0800

    Delete logger.py

--- controllably/misc/__init__.py
+++ controllably/misc/__init__.py
@@ -26,7 +26,6 @@ from . import factory as Factory
 from . import helper as Helper
 from . import layout as Layout
 from .factory import include_this_module, modules
-from .logger import Logger, LOGGER
 from .misc_utils import *
 
 include_this_module(get_local_only=False)
