dbfa78f7120c7fe1f4b620f0523bced400ddb33a
Chang Jie Leong <changjie9@gmail.com>
2024-10-29 22:01:28 +0800

    Create file_handler.py

---
+++ controllably/misc/file_handler.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Standard library imports
+from __future__ import annotations
+import json
+from logging import getLogger
+
+# Third party imports
+import yaml
+
+logger = getLogger(__name__)
+logger.info(f"Import: OK <{__name__}>")
+
+
