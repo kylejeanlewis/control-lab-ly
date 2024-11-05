b8b04ffd0090dd145207d1a5100c606c2f8d62b4
Chang Jie Leong <changjie9@gmail.com>
2024-11-05 19:19:01 +0800

    Create gcode_utils.py

---
+++ controllably/Move/gcode_utils.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Standard library imports
+from __future__ import annotations
+
+# Local application imports
+from . import Mover
+
+class GCode(Mover):
+    """
+    """
+    def __init__(self, **kwargs):
+        """
+        """
+        super().__init__(**kwargs)
+        return
+        
+    
