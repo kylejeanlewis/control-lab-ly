7d689da2ef2b22ebeca08f2a93ba2588c1e25cac
Chang Jie Leong <changjie9@gmail.com>
2024-11-12 16:02:49 +0800

    Update connection.py
    - added documentation

--- controllably/core/connection.py
+++ controllably/core/connection.py
@@ -1,12 +1,31 @@
 # -*- coding: utf-8 -*-
+""" 
+This module provides classes for handling connections to serial and socket devices.
+
+Attributes:
+    VALID_BAUDRATES (tuple[int]): valid baudrates for serial devices
+    
+## Classes:
+    `Device`: Protocol for device connection classes
+    `SerialDevice`: Interface for handling serial devices
+    `SocketDevice`: Interface for handling socket devices
+    `DeviceFactory`: Factory class for creating devices
+    
+## Functions:
+    `get_addresses`: Get the appropriate addresses for current machine
+    `get_node`: Get the unique identifier for current machine
+    `get_ports`: Get available serial ports connected to current machine
+    
+<i>Documentation last updated: 2024-11-12</i>
+"""
 # Standard library imports
 from __future__ import annotations
-import gc
+from copy import deepcopy
 import logging
 import socket
 import time
 from types import SimpleNamespace
-from typing import Protocol, Any
+from typing import Protocol, Any, Callable
 import uuid
 
 # Third party imports
@@ -17,18 +36,21 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
 logger.debug(f"Import: OK <{__name__}>")
 
+VALID_BAUDRATES = (110, 300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200)
+"""Valid baudrates for serial devices"""
+
 def get_addresses(registry:dict|None) -> dict|None:
     """
     Get the appropriate addresses for current machine
 
     Args:
-        registry (dict|None): dictionary with com port addresses and camera ids
+        registry (dict|None): dictionary with serial port addresses and camera ids
 
     Returns:
-        dict|None: dictionary of com port addresses and camera ids for current machine
+        dict|None: dictionary of serial port addresses and camera ids for current machine, if available
     """
     node_id = get_node()
-    addresses = registry.get('machine_id',{}).get(node_id,{})
+    addresses = registry.get('machine_id',{}).get(node_id,{}) if registry is not None else {}
     if len(addresses) == 0:
         logger.warning("Append machine id and camera ids/port addresses to registry file")
         logger.warning(f"Machine not yet registered. (Current machine id: {node_id})")
@@ -37,7 +59,7 @@ def get_addresses(registry:dict|None) -> dict|None:
 
 def get_node() -> str:
     """
-    Display the machine's unique identifier
+    Get the unique identifier for current machine
 
     Returns:
         str: machine unique identifier
@@ -50,7 +72,7 @@ def get_node() -> str:
 
 def get_ports() -> list[str]:
     """
-    Get available ports
+    Get available serial ports connected to current machine
 
     Returns:
         list[str]: list of connected serial ports
@@ -66,9 +88,7 @@ def get_ports() -> list[str]:
     return ports
 
 class Device(Protocol):
-    """
-    Protocol for device classes
-    """
+    """Protocol for device connection classes"""
     connection_details: dict
     is_connected: bool
     verbose: bool
@@ -84,7 +104,7 @@ class Device(Protocol):
         """Disconnect from the device"""
         raise NotImplementedError
 
-    def query(self, data:str, lines:bool = True) -> list[str]|None:
+    def query(self, data:Any, lines:bool = True) -> list[str]|None:
         """Query the device"""
         raise NotImplementedError
 
@@ -99,9 +119,39 @@ class Device(Protocol):
 
 class SerialDevice:
     """
-    Class for handling serial devices
+    SerialDevice provides an interface for handling serial devices
+    
+    ### Constructor:
+        `port` (str|None, optional): serial port for the device. Defaults to None.
+        `baudrate` (int, optional): baudrate for the device. Defaults to 9600.
+        `timeout` (int, optional): timeout for the device. Defaults to 1.
+        `init_timeout` (int, optional): timeout for initialization. Defaults to 2.
+        `message_end` (str, optional): message end character. Defaults to '\\n'.
+        `simulation` (bool, optional): whether to simulate the device. Defaults to False.
+        `verbose` (bool, optional): verbosity of class. Defaults to False.
+    
+    ### Attributes and properties:
+        `port` (str): device serial port
+        `baudrate` (int): device baudrate
+        `timeout` (int): device timeout
+        `connection_details` (dict): connection details for the device
+        `serial` (serial.Serial): serial object for the device
+        `init_timeout` (int): timeout for initialization
+        `message_end` (str): message end character
+        `flags` (SimpleNamespace[str, bool]): flags for the device
+        `is_connected` (bool): whether the device is connected
+        `verbose` (bool): verbosity of class
+        
+    ### Methods:
+        `clear`: clear the input and output buffers
+        `connect`: connect to the device
+        `disconnect`: disconnect from the device
+        `query`: query the device (i.e. write and read data)
+        `read`: read data from the device
+        `write`: write data to the device
     """
     
+    _default_flags: SimpleNamespace = SimpleNamespace(verbose=False, connected=False, simulation=False)
     def __init__(self,
         port: str|None = None, 
         baudrate: int = 9600, 
@@ -113,21 +163,24 @@ class SerialDevice:
         verbose: bool = False,
         **kwargs
     ):
-        """
-        Constructor for SerialDevice
-
+        """ 
+        Initialize SerialDevice class
+        
         Args:
-            port (str): port for the device
-            baudrate (int): baudrate for the device
+            port (str|None, optional): serial port for the device. Defaults to None.
+            baudrate (int, optional): baudrate for the device. Defaults to 9600.
             timeout (int, optional): timeout for the device. Defaults to 1.
             init_timeout (int, optional): timeout for initialization. Defaults to 2.
+            message_end (str, optional): message end character. Defaults to '\\n'.
+            simulation (bool, optional): whether to simulate the device. Defaults to False.
+            verbose (bool, optional): verbosity of class. Defaults to False.
         """
         self._port = ''
         self._baudrate = 0
         self._timeout = 0
         self.init_timeout = init_timeout
         self.message_end = message_end
-        self.flags = SimpleNamespace(verbose=False, connected=False, simulation=simulation)
+        self.flags = deepcopy(self._default_flags)
         
         self.serial = serial.Serial()
         self.port = port
@@ -135,10 +188,8 @@ class SerialDevice:
         self.timeout = timeout
         
         self.verbose = verbose
+        self.flags.simulation = simulation
         return
-    
-    def __repr__(self):
-        return f"{super().__repr__()} ref={len(gc.get_referrers(self))}"
     
     def __del__(self):
         self.disconnect()
@@ -146,45 +197,39 @@ class SerialDevice:
     
     @property
     def port(self) -> str:
-        """Get port for the device"""
+        """Device serial port"""
         return self._port
     @port.setter
     def port(self, value:str):
-        """Set port for the device"""
         self._port = value
         self.serial.port = value
         return
     
     @property
     def baudrate(self) -> int:
-        """Get baudrate for the device"""
+        """Device baudrate"""
         return self._baudrate
     @baudrate.setter
     def baudrate(self, value:int):
-        """Set baudrate for the device"""
+        assert isinstance(value, int), "Ensure baudrate is an integer"
+        assert value in VALID_BAUDRATES, f"Ensure baudrate is one of the standard values: {VALID_BAUDRATES}"
         self._baudrate = value
         self.serial.baudrate = value
         return
     
     @property
     def timeout(self) -> int:
-        """Get timeout for the device"""
+        """Device timeout"""
         return self._timeout
     @timeout.setter
     def timeout(self, value:int):
-        """Set timeout for the device"""
         self._timeout = value
         self.serial.timeout = value
         return
     
     @property
     def connection_details(self) -> dict:
-        """
-        Get connection details
-
-        Returns:
-            dict: connection details
-        """
+        """Connection details for the device"""
         return {
             'port': self.port,
             'baudrate': self.baudrate,
@@ -193,22 +238,16 @@ class SerialDevice:
     
     @property
     def is_connected(self) -> bool:
-        """
-        Check if the device is connected
-
-        Returns:
-            bool: whether the device is connected
-        """
+        """Whether the device is connected"""
         connected = self.flags.connected if self.flags.simulation else self.serial.is_open
         return connected
     
     @property
     def verbose(self) -> bool:
-        """Get verbosity of class"""
+        """Verbosity of class"""
         return self.flags.verbose
     @verbose.setter
     def verbose(self, value:bool):
-        """Set verbosity of class"""
         assert isinstance(value,bool), "Ensure assigned verbosity is boolean"
         self.flags.verbose = value
         level = logging.INFO if value else logging.WARNING
@@ -219,17 +258,13 @@ class SerialDevice:
         return
     
     def clear(self):
-        """
-        Clear the input and output buffers
-        """
+        """Clear the input and output buffers"""
         self.serial.reset_input_buffer()
         self.serial.reset_output_buffer()
         return
 
     def connect(self):
-        """
-        Connect to the device
-        """
+        """Connect to the device"""
         try:
             if self.is_connected:
                 return
@@ -244,9 +279,7 @@ class SerialDevice:
         return
 
     def disconnect(self):
-        """
-        Disconnect from the device
-        """
+        """Disconnect from the device"""
         try:
             if not self.is_connected:
                 return
@@ -261,13 +294,14 @@ class SerialDevice:
     
     def query(self, data:Any, lines:bool = True) -> list[str]|None:
         """
-        Query the device
+        Query the device (i.e. write and read data)
 
         Args:
-            data (str): data to query
+            data (Any): data to query
+            lines (bool, optional): whether to read multiple lines. Defaults to True.
 
         Returns:
-            list[str]: data read from the device
+            list[str]|None: data read from the device, if any
         """
         ret = self.write(str(data))
         if ret:
@@ -278,9 +312,12 @@ class SerialDevice:
     def read(self, lines:bool = False) -> str|list[str]:
         """
         Read data from the device
+        
+        Args:
+            lines (bool, optional): whether to read multiple lines. Defaults to False.
 
         Returns:
-            list[str]: data read from the device
+            str|list[str]: line(s) of data read from the device
         """
         data = '' if not lines else []
         try:
@@ -318,16 +355,45 @@ class SerialDevice:
 
 class SocketDevice:
     """
-    Class for handling socket devices
+    SocketDevice provides an interface for handling socket devices
+    
+    ### Constructor:
+        `host` (str): host for the device
+        `port` (int): port for the device
+        `timeout` (int, optional): timeout for the device. Defaults to 1.
+        `simulation` (bool, optional): whether to simulate the device. Defaults to False.
+        `verbose` (bool, optional): verbosity of class. Defaults to False.
+    
+    ### Attributes and properties:
+        `host` (str): device host
+        `port` (int): device port
+        `timeout` (int): device timeout
+        `connection_details` (dict): connection details for the device
+        `socket` (socket.socket): socket object for the device
+        `flags` (SimpleNamespace[str, bool]): flags for the device
+        `is_connected` (bool): whether the device is connected
+        `verbose` (bool): verbosity of class
+        
+    ### Methods:
+        `clear`: clear the input and output buffers
+        `connect`: connect to the device
+        `disconnect`: disconnect from the device
+        `query`: query the device (i.e. write and read data)
+        `read`: read data from the device
+        `write`: write data to the device
     """
+    
+    _default_flags: SimpleNamespace = SimpleNamespace(verbose=False, connected=False, simulation=False)
     def __init__(self, host:str, port:int, timeout:int=1, *, simulation:bool=False, **kwargs):
         """
-        Constructor for SocketDevice
-
+        Initialize SocketDevice class
+        
         Args:
             host (str): host for the device
             port (int): port for the device
             timeout (int, optional): timeout for the device. Defaults to 1.
+            simulation (bool, optional): whether to simulate the device. Defaults to False.
+            verbose (bool, optional): verbosity of class. Defaults to False.
         """
         self.host = host
         self.port = port
@@ -340,12 +406,7 @@ class SocketDevice:
 
     @property
     def connection_details(self) -> dict:
-        """
-        Get connection details
-
-        Returns:
-            dict: connection details
-        """
+        """Connection details for the device"""
         return {
             'host': self.host,
             'port': self.port,
@@ -354,22 +415,16 @@ class SocketDevice:
     
     @property
     def is_connected(self) -> bool:
-        """
-        Check if the device is connected
-
-        Returns:
-            bool: whether the device is connected
-        """
+        """Whether the device is connected"""
         connected = self.flags.connected if self.flags.simulation else (self.socket.fileno() != -1)
         return connected
     
     @property
     def verbose(self) -> bool:
-        """Get verbosity of class"""
+        """Verbosity of class"""
         return self.flags.verbose
     @verbose.setter
     def verbose(self, value:bool):
-        """Set verbosity of class"""
         assert isinstance(value,bool), "Ensure assigned verbosity is boolean"
         self.flags.verbose = value
         level = logging.INFO if value else logging.WARNING
@@ -380,18 +435,14 @@ class SocketDevice:
         return
     
     def clear(self):
-        """
-        Clear the input and output buffers
-        """
+        """Clear the input and output buffers"""
         self.socket.settimeout(0)
         self.socket.recv(1024)
         self.socket.settimeout(self.timeout)
         return
 
     def connect(self):
-        """
-        Connect to the device
-        """
+        """Connect to the device"""
         try:
             if self.is_connected:
                 return
@@ -405,9 +456,7 @@ class SocketDevice:
         return
 
     def disconnect(self):
-        """
-        Disconnect from the device
-        """
+        """Disconnect from the device"""
         try:
             if not self.is_connected:
                 return
@@ -420,26 +469,31 @@ class SocketDevice:
         self.flags.connected = False
         return
     
-    def query(self, data:str, lines:bool = True) -> list[str]|None:
+    def query(self, data:Any, lines:bool = True) -> list[str]|None:
         """
         Query the device
 
         Args:
-            data (str): data to query
+            data (Any): data to query
 
         Returns:
-            list[str]: data read from the device
+            list[str]|None: data read from the device, if any
         """
-        self.write(data)
-        response = self.read(lines=lines)
-        return response if isinstance(response, list) else [response]
-    
+        ret = self.write(str(data))
+        if ret:
+            response = self.read(lines=lines)
+            return response if isinstance(response, list) else [response]
+        return
+
     def read(self, lines:bool = False) -> str|list[str]:
         """
         Read data from the device
-
+        
+        Args:
+            lines (bool, optional): whether to read multiple lines. Defaults to False.
+            
         Returns:
-            list[str]: data read from the device
+            str|list[str]: line(s) of data read from the device
         """
         data = []
         try:
@@ -471,14 +525,18 @@ class SocketDevice:
 class DeviceFactory:
     """
     Factory class for creating devices
+    
+    ## Class methods:
+        `createDevice`: create a device
+        `createDeviceFromDict`: create a device from a dictionary
     """
     @staticmethod
-    def createDevice(device_type:str, *args, **kwargs) -> Device:
+    def createDevice(device_type:Callable, *args, **kwargs) -> Device:
         """
         Create a device
 
         Args:
-            device_type (str): type of device to create
+            device_type (Callable): device class to be created
 
         Returns:
             Device: created device
