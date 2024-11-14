9308f016a7da61e2a6a89ed55b3db3b4d19c798d
Chang Jie Leong <changjie9@gmail.com>
2024-11-15 00:40:01 +0800

    Update __init__.py

--- controllably/Compound/LiquidMover/__init__.py
+++ controllably/Compound/LiquidMover/__init__.py
@@ -4,7 +4,7 @@ This sub-package imports the class for liquid mover setups.
 Classes:
     LiquidMoverSetup (CompoundSetup)
 """
-from .liquidmover_utils import LiquidMoverSetup
+from .liquidmover_utils import LiquidMoverSetup, LiquidMover
 
 from controllably import include_this_module
 include_this_module(get_local_only=False)
