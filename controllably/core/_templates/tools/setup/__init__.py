f7e64172d02a2047544ad8279d003c7e2a31ebca
Chang Jie Leong <changjie9@gmail.com>
2024-11-16 16:24:18 +0800

    Update __init__.py
    - update init template for tools
    - uses `setup()` function instead to initialise setup and returns the same instance if already initialised

--- controllably/core/_templates/tools/setup/__init__.py
+++ controllably/core/_templates/tools/setup/__init__.py
@@ -2,24 +2,24 @@
 This __init__.py file initializes and imports the setups defined in this sub-folder.
 
 Attributes:
-    setup (Platform|namedtuple|dict)
     CONFIG_FILE (str)
     LAYOUT_FILE (str)
     REGISTRY_FILE (str)
+    
+## Functions:
+    setup: Load setup from files and return as NamedTuple or Platform
 """
 from dataclasses import dataclass
 from pathlib import Path
-from controllably.core.factory import load_setup_from_files         # pip install control-lab-ly
+from controllably.core.factory import get_setup         # pip install control-lab-ly
 __all__ = ['CONFIG_FILE', 'LAYOUT_FILE', 'REGISTRY_FILE', 'setup']
+__setup__ = None
 
 HERE = Path(__file__).parent.absolute()
 CONFIGS = Path(__file__).parent.parent.absolute()
 CONFIG_FILE = HERE/"config.yaml"
 LAYOUT_FILE = HERE/"layout.json"
 REGISTRY_FILE = CONFIGS/"registry.yaml"
-
-setup = load_setup_from_files(CONFIG_FILE, REGISTRY_FILE, create_tuple=True)
-"""NOTE: importing SETUP gives the same instance wherever you import it"""
 
 # ========== Optional (for typing) ========== #
 # from ... import _tool_class
@@ -29,6 +29,13 @@ class Platform:
     ...
     # Add fields and types here
     # _tool_name: _tool_class
+    
+# ========================================== #
 
-if len(Platform.__annotations__) > 0:
-    setup = Platform(**setup._asdict())
+def setup() -> tuple|Platform:
+    global __setup__
+    if __setup__ is not None:
+        print(f"Setup already loaded from {CONFIG_FILE}")
+        return __setup__
+    __setup__ = get_setup(CONFIG_FILE, REGISTRY_FILE, Platform)
+    return __setup__
