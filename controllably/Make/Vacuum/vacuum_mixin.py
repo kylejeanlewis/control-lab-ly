e56e59a01340086ebf69157db9e4f8ce586e2553
Chang Jie Leong <changjie9@gmail.com>
2024-11-19 11:22:26 +0800

    Update vacuum_mixin.py
    - add delays

--- controllably/Make/Vacuum/vacuum_mixin.py
+++ controllably/Make/Vacuum/vacuum_mixin.py
@@ -2,10 +2,14 @@
 """
 This module contains the VacuumMixin class.
 
+Attributes:
+    VACUUM_ON_DELAY (int): delay for vacuum on
+    VACUUM_OFF_DELAY (int): delay for vacuum off
+
 ## Classes:
     `VacuumMixin`: Mixin class for vacuum control
     
-<i>Documentation last updated: 2024-11-14</i>
+<i>Documentation last updated: 2024-11-19</i>
 """
 # Standard library imports
 from __future__ import annotations
@@ -15,9 +19,15 @@ import time
 logger = logging.getLogger("controllably.Make")
 logger.debug(f"Import: OK <{__name__}>")
 
+VACUUM_ON_DELAY = 3
+VACUUM_OFF_DELAY = 3
+
 class VacuumMixin:
     """
     Mixin class for vacuum control
+    
+    ### Attributes
+        `vacuum_delays` (dict): delays for vacuum control
     
     ### Methods
         `evacuate`: Evacuate to create vacuum
@@ -28,20 +38,33 @@ class VacuumMixin:
     def __init__(self):
         """Initialize VacuumMixin class"""
         super().__init__()
+        self.vacuum_delays = dict(on=VACUUM_ON_DELAY, off=VACUUM_OFF_DELAY)
         return
     
-    def evacuate(self):
-        """Evacuate to create vacuum"""
+    def evacuate(self, wait:float|None = None):
+        """
+        Evacuate to create vacuum
+        
+        Args:
+            wait (float|None): Time to wait after evacuating. Defaults to None.
+        """
         logger.warning("Pulling vacuum")
         self.toggleVacuum(True)
-        time.sleep(3)
+        wait = self.vacuum_delays["on"] if wait is None else wait
+        time.sleep(wait)
         return
     
-    def vent(self):
-        """Vent to release vacuum"""
+    def vent(self, wait:float|None = None):
+        """
+        Vent to release vacuum
+        
+        Args:
+            wait (float|None): Time to wait after venting. Defaults to None.
+        """
         logger.warning("Venting vacuum")
         self.toggleVacuum(False)
-        time.sleep(3)
+        wait = self.vacuum_delays["off"] if wait is None else wait
+        time.sleep(wait)
         return
     
     def toggleVacuum(self, on:bool):
