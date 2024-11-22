528e77c5b667ac3cbb39636697a2f8630cb2cbc4
Chang Jie Leong <changjie9@gmail.com>
2024-11-22 16:41:31 +0800

    Update __init__.py

--- controllably/Move/Jointed/Dobot/dobot_api/__init__.py
+++ controllably/Move/Jointed/Dobot/dobot_api/__init__.py
@@ -1,2 +1,7 @@
-from .dobot_api_old import dobot_api_dashboard, dobot_api_feedback
+""" 
+Newest version of the Dobot API.
+Retrieved from https://github.com/Dobot-Arm/TCP-IP-4Axis-Python/tree/main 
+on 2024-11-22
+"""
 from .dobot_api import DobotApiDashboard, DobotApiMove
+from .dobot_api_old import dobot_api_dashboard, dobot_api_feedback
