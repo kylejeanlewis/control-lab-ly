198900adfad99f1fce4537073cb966d03512dd0a
Chang Jie Leong <changjie9@gmail.com>
2025-01-06 15:53:14 +0800

    Update __init__.py

--- controllably/Move/Jointed/__init__.py
+++ controllably/Move/Jointed/__init__.py
@@ -1,10 +1,2 @@
-"""
-This sub-package imports the classes for jointed mover tools.
-
-Classes:
-    RobotArm (Mover)
-"""
+# -*- coding: utf-8 -*-
 from .jointed_utils import RobotArm
-
-from controllably import include_this_module
-include_this_module(get_local_only=False)
