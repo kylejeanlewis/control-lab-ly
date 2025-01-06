6c6654d1ecdea37b45f77aca4a9f60cb577fc97f
Chang Jie Leong <changjie9@gmail.com>
2025-01-06 15:55:26 +0800

    Remove old dobot api

--- controllably/Move/Jointed/Dobot/dobot_api/__init__.py
+++ controllably/Move/Jointed/Dobot/dobot_api/__init__.py
@@ -4,4 +4,3 @@ Retrieved from https://github.com/Dobot-Arm/TCP-IP-4Axis-Python/tree/main
 on 2024-11-22
 """
 from .dobot_api import DobotApiDashboard, DobotApiMove
-from .dobot_api_old import dobot_api_dashboard, dobot_api_feedback
