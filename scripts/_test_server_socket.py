1b203da9cbd314dc3e7db419481b2c11c08ca970
Chang Jie Leong <changjie9@gmail.com>
2025-01-09 17:47:41 +0800

    Update _test_server_socket.py

--- scripts/_test_server_socket.py
+++ scripts/_test_server_socket.py
@@ -1,79 +1,176 @@
 # %%
 from __future__ import annotations
 from collections import deque
+from copy import deepcopy
 import queue
 from random import random
 import select
 import socket
 import threading
 import time
+from typing import Callable, Mapping
 
-HOST_IP = socket.gethostbyname(socket.gethostname())
-HOST_PORT = 12345
-ENCODER = 'utf-8'
 BYTESIZE = 1024
+ENCODER = 'utf-8'
+
+CONNECT_MESSAGE = '[CONNECTED]'
 DISCONNECT_MESSAGE = '!EXIT'
 SHUTDOWN_MESSAGE = '!SHUTDOWN'
 
+def read_all(connection: socket.socket, *, bytesize: int = BYTESIZE, encoder: str = ENCODER) -> str:
+    data = ''
+    while True:
+        out = read(connection, bytesize=bytesize, encoder=encoder)
+        data += out
+        if not out or len(data) > bytesize:
+            break
+    return data
+
+def read(connection: socket.socket, *, bytesize: int = BYTESIZE, encoder: str = ENCODER) -> str:
+    out = ''
+    try:
+        out = connection.recv(bytesize).decode(encoder, "replace")
+    except OSError as e:
+        pass
+    except TimeoutError:
+        pass
+    except (ConnectionResetError, ConnectionAbortedError):
+        pass
+    except KeyboardInterrupt:
+        pass
+    return out
+
+def write(data: str, connection: socket.socket, *, encoder: str = ENCODER, wait: bool = False):
+    try:
+        connection.sendall(data.encode(encoder))
+        if wait:
+            time.sleep(1)
+    except (ConnectionResetError, ConnectionAbortedError):
+        pass
+    except KeyboardInterrupt:
+        pass
+    return
+
 class Server:
+    
+    _default_keywords = dict(connect=CONNECT_MESSAGE, disconnect=DISCONNECT_MESSAGE, shutdown=SHUTDOWN_MESSAGE)
     def __init__(self, 
-        host:str, 
-        port:int, 
-        trigger:threading.Event = threading.Event(), 
-        print_queue:queue.Queue = queue.Queue()
+        host: str, 
+        port: int,
+        terminate: threading.Event = threading.Event(), 
+        print_queue: queue.Queue = queue.Queue(),
+        *,
+        bytesize: int = 1024,
+        encoder: str = 'utf-8',
+        keywords: Mapping[str]|None = None
     ):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host = host
         self.port = port
         self.address = f'{host}:{port}'
-        self.trigger = trigger
-        self.print_queue = print_queue
-        self.conn_list = deque()
         
+        self.print_queue = print_queue
+        self.connections = deque()
+        self.removal_list = deque()
+        self.triggers = dict(
+            started = threading.Event(),
+            update_connections = threading.Event(),
+            terminate = terminate,
+            jam = threading.Event()
+        )
+        
+        self.bytesize = bytesize
+        self.encoder = encoder
+        self.keywords = deepcopy(self._default_keywords) if keywords is None else keywords
+        
+        self.client_threads = dict()
         self._printer_thread = None
         self._listener_thread = None
-        self._client_threads = dict()
-        self._conn_change = threading.Event()
-        self._conn_remove = deque()
-        self._started = threading.Event()
         return
     
     def __del__(self):
         self.stop()
+        time.sleep(1)
         return
     
-    def start(self):
-        if self._started.is_set():
+    def start(self, blocking: bool = False):
+        if self.triggers['started'].is_set():
             return
-        self.start_server(self.host, self.port, self.trigger, self.print_queue)
-        self._started.set()
+        self.start_server(
+            self.host, 
+            self.port, 
+            triggers = self.triggers['terminate'], 
+            print_queue = self.print_queue,
+            blocking = blocking
+        )
         return
         
     def stop(self):
-        self.trigger.set()
-        self._started.clear()
+        self.triggers['terminate'].set()
+        self.triggers['started'].clear()
+        return
+    
+    def start_server(self, 
+        host: str, 
+        port: int, 
+        client_handler: Callable|None = None,
+        *,
+        terminate: threading.Event|None = None,
+        print_queue: queue.Queue|None = None,
+        blocking: bool = False,
+        **kwargs
+    ):
+        client_handler = self.handle_client if client_handler is None else client_handler
+        self.triggers['terminate'] = self.triggers['terminate'] if terminate is None else terminate
+        print_queue = self.print_queue if print_queue is None else print_queue
+        
+        self.server = socket.create_server((host, port))
+        self.triggers['started'].set()
+        print_queue.put(f'[START] Server started {host}:{port}')
+        
+        self.server.listen()
+        print_queue.put('[START] Listening for incoming connections...')
+        
+        if not (isinstance(self._printer_thread, threading.Thread) and self._printer_thread.is_alive()):
+            self._printer_thread = threading.Thread(target=self.printer, args=(print_queue, self.triggers['jam']), daemon=True)
+            self._printer_thread.start()
+        
+        kwargs.update(
+            triggers=self.triggers, print_queue=print_queue,
+            connections=self.connections, removal_list=self.removal_list,
+            threads=self.client_threads, keywords=self.keywords
+        )
+        if blocking:
+            self.listen(self.server, client_handler, **kwargs)
+        elif not (isinstance(self._listener_thread, threading.Thread) and self._listener_thread.is_alive()):
+            self._listener_thread = threading.Thread(
+                target=self.listen, 
+                args=(self.server, client_handler),
+                kwargs=kwargs
+            )
+            self._listener_thread.start()
         return
     
     @staticmethod
     def listen(
         server: socket.socket, 
-        trigger: threading.Event, 
+        client_handler: Callable,
+        *,
+        triggers: dict[str, threading.Event],
         print_queue: queue.Queue, 
+        connections: deque,
+        removal_list: deque,
         threads: dict[str, threading.Thread], 
-        conn_list: deque,
-        conn_trigger: threading.Event,
-        conn_remove: deque,
-        started: threading.Event,
-        client_handler: callable
+        **kwargs
     ):
-        while not trigger.is_set():
-            if conn_trigger.is_set():
-                message = f'[CONNECTIONS] {len(conn_list)}\n'
-                message += f'{"\n".join([("- "+a) for a in conn_list])}'
+        while not triggers['terminate'].is_set():
+            if triggers['update_connections'].is_set():
+                message = f'[CONNECTIONS] {len(connections)}\n'
+                message += f'{"\n".join([("- "+a) for a in connections])}'
                 print_queue.put(message)
-                while len(conn_remove):
-                    del threads[conn_remove.pop()]
-                conn_trigger.clear()
+                while len(removal_list):
+                    del threads[removal_list.pop()]
+                triggers['update_connections'].clear()
                 continue
             
             read_list, _, _ = select.select([server], [], [], 1)
@@ -88,36 +185,43 @@ class Server:
             except KeyboardInterrupt:
                 break
             addr = f'{":".join([str(a) for a in addr])}'
+            kwargs.update(
+                triggers=triggers, print_queue=print_queue, 
+                connections=connections, removal_list=removal_list
+            )
             thread = threading.Thread(
                 target=client_handler, 
-                args=(conn,addr,trigger,print_queue,conn_list,conn_trigger,conn_remove)
+                args=(conn, addr),
+                kwargs=kwargs
             )
             thread.start()
             threads[addr] = thread
             
-            conn_list.append(addr)
-            conn_trigger.set()
+            connections.append(addr)
+            triggers['update_connections'].set()
         
-        trigger.set()
+        triggers['terminate'].set()
         print_queue.put('[STOP] Termination triggered')
         
         for _,thread in threads.items():
             thread.join()
+        addr = f'{":".join([str(a) for a in server.getsockname()])}'
         server.close()
-        print_queue.put('[EXIT] Main')
+        print_queue.put(f'[EXIT:SERVER] {addr}')
         
         # Reset
-        if conn_trigger.is_set():
-            while len(conn_remove):
-                del threads[conn_remove.pop()]
-            conn_trigger.clear()
-        trigger.clear()
-        started.clear()
+        if triggers['update_connections'].is_set():
+            while len(removal_list):
+                del threads[removal_list.pop()]
+            triggers['update_connections'].clear()
+        triggers['terminate'].clear()
+        triggers['started'].clear()
+        triggers['jam'].set()
         return
     
     @staticmethod
-    def printer(print_queue: queue.Queue, trigger: threading.Event):
-        while not trigger.is_set():
+    def printer(print_queue: queue.Queue, jam: threading.Event):
+        while not jam.is_set():
             try:
                 print(print_queue.get())
                 print_queue.task_done()
@@ -133,110 +237,76 @@ class Server:
             except KeyboardInterrupt:
                 break
         print('[EXIT] Printer')
-        return
-    
-    def start_server(self, 
-        host: str, 
-        port: int, 
-        trigger: threading.Event,
-        print_queue: queue.Queue
-    ):
-        self.server = socket.create_server((host, port))
-        print_queue.put(f'[START] Server started {host}:{port}')
-        self.server.listen()
-        print_queue.put('[START] Listening for incoming connections...')
-        
-        if isinstance(self._printer_thread, threading.Thread) and self._printer_thread.is_alive():
-            pass
-        else:
-            self._printer_thread = threading.Thread(target=self.printer, args=(print_queue, trigger), daemon=True)
-            self._printer_thread.start()
-        
-        if isinstance(self._listener_thread, threading.Thread) and self._listener_thread.is_alive():
-            pass
-        else:
-            self._listener_thread = threading.Thread(
-                target=self.listen, 
-                args=(
-                    self.server, 
-                    trigger, 
-                    print_queue, 
-                    self._client_threads, 
-                    self.conn_list, 
-                    self._conn_change,
-                    self._conn_remove,
-                    self._started,
-                    self.handle_client
-                )
-            )
-            self._listener_thread.start()
+        jam.clear()
         return
     
     @staticmethod
     def handle_client(
         conn: socket.socket, 
         addr: str, 
-        trigger: threading.Event,
+        *,
+        triggers: dict[str, threading.Event],
         print_queue: queue.Queue,
-        conn_list: deque,
-        conn_trigger: threading.Event,
-        conn_remove: deque
+        connections: deque,
+        removal_list: deque,
+        keywords: Mapping[str,str],
+        **kwargs
     ):
+        bytesize = kwargs.get('bytesize', BYTESIZE)
+        encoder = kwargs.get('encoder', ENCODER)
+        connect_message = keywords.get('connect', CONNECT_MESSAGE)
+        disconnect_message = keywords.get('disconnect', DISCONNECT_MESSAGE)
+        shutdown_message = keywords.get('shutdown', SHUTDOWN_MESSAGE)
+        
         print_queue.put(f'[NEW] {addr}')
         conn.setblocking(False)
         
-        while not trigger.is_set():
-            try:
-                data = conn.recv(BYTESIZE).decode(ENCODER)
-            except OSError as e:
+        while not triggers['terminate'].is_set():
+            data = read(conn, bytesize=bytesize, encoder=encoder)
+            if not data:
                 time.sleep(0.01)
                 continue
-            except (ConnectionResetError, ConnectionAbortedError):
-                break
-            except KeyboardInterrupt:
-                break
-            data = data.strip()
-            if data:
+            elif data.strip():
                 print_queue.put(f"[{addr}] {data}")
+            data = data.strip()
             
-            if data == DISCONNECT_MESSAGE:
-                conn.sendall(DISCONNECT_MESSAGE.encode(ENCODER))
-                time.sleep(1)
+            if data == disconnect_message:
                 break
-            elif data == SHUTDOWN_MESSAGE:
-                conn.sendall(SHUTDOWN_MESSAGE.encode(ENCODER))
-                time.sleep(1)
-                trigger.set()
+            elif data == shutdown_message:
+                write(shutdown_message, conn, encoder=encoder, wait= True)
+                triggers['terminate'].set()
                 break
-            elif data.startswith('[CONNECTED]'):
+            elif data.startswith(connect_message):
                 time.sleep(1)
-                if addr in conn_list:
-                    data = f'[CONNECTED] {addr}'
+                if addr in connections:
+                    data = f'{connect_message} {addr}'
             
             if not data:
                 time.sleep(0.01)
                 data = f"{random()*10:.3f};{random()*10:.3f};{random()*10:.3f}\n"
-            conn.sendall(data.encode(ENCODER))
+            write(data, conn, encoder=encoder)
         
-        try:
-            conn.sendall(DISCONNECT_MESSAGE.encode(ENCODER))
-        except (ConnectionResetError, ConnectionAbortedError):
-            pass
-        except KeyboardInterrupt:
-            pass
+        write(disconnect_message, conn, encoder=encoder, wait= True)
         
-        conn_remove.append(addr)
-        conn_list.remove(addr)
-        conn_trigger.set()
+        removal_list.append(addr)
+        connections.remove(addr)
+        triggers['update_connections'].set()
         time.sleep(1)
-        if len(conn_list) == 0:
-            trigger.set()
+        if len(connections) == 0:
+            triggers['terminate'].set()
             print_queue.put('[SHUTDOWN] No active connections')
-        print_queue.put(f'[EXIT] {addr}')
+        print_queue.put(f'[EXIT:CLIENT] {addr}')
         return
     
 # %%
-server = Server(HOST_IP, HOST_PORT)
+import socket
+from _test_socket import Server
+
+host_ip = socket.gethostbyname(socket.gethostname())
+host_port = 12345
+
+# %%
+server = Server(host_ip, host_port)
 server.start()
 
 # %%
