1bca4b9d159faab53821f6da3b85a454daa34120
Chang Jie Leong <changjie9@gmail.com>
2025-01-21 17:54:48 +0800

    Create load_cell.py

---
+++ controllably/Measure/Mechanical/load_cell.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+# Standard library imports
+from __future__ import annotations
+import logging
+import time
+from typing import NamedTuple
+
+# Local application imports
+from ..measure import Measurer
+
+logger = logging.getLogger(__name__)
+logger.debug(f"Import: OK <{__name__}>")
+
+MAX_LEN = 100
+READ_FORMAT = "{yyyymmdd} {hhmmss} {pH} {temperature}\r\n"
+pHData = NamedTuple('pHData', [('yyyymmdd',str),('hhmmss',str),('pH',float),('temperature',float)])
+
+class LoadCell(Measurer):
+    
+    def __init__(self,
+        port: str,
+        stabilize_timeout: float = 10, 
+        pH_tolerance: float = 1.5, 
+        temp_tolerance: float = 1.5, 
+        *, 
+        baudrate: int = 9600,
+        verbose: bool = False, 
+        **kwargs
+    ):
+        super().__init__(
+            port=port, baudrate=baudrate, verbose=verbose, 
+            read_format=READ_FORMAT, data_type=pHData, **kwargs
+        )
+        
+        self.pH_tolerance = pH_tolerance
+        self.temp_tolerance = temp_tolerance
+        self.stabilize_timeout = stabilize_timeout
+        self._stabilize_start_time = None
+        return
+    
+    def getData(self, *args, **kwargs) -> pHData|None:
+        """
+        Get data from device
+        """
+        return super().getData(query='ACT', *args, **kwargs)
+    
+    def atPH(self, 
+        pH: float, 
+        *, 
+        tolerance: float|None = None,
+        stabilize_timeout: float = 0
+    ) -> bool:
+        """
+        Check if the device is at the target pH
+        """
+        data = self.getData()
+        if data is None:
+            return False
+        
+        tolerance = tolerance or self.pH_tolerance
+        stabilize_timeout = stabilize_timeout or self.stabilize_timeout
+        if abs(data.pH - pH) > tolerance:
+            self._stabilize_start_time = None
+            return False
+        self._stabilize_start_time = self._stabilize_start_time or time.perf_counter()
+        if ((time.perf_counter()-self._stabilize_start_time) < stabilize_timeout):
+            return False
+        return True
+    
+    def atTemperature(self, 
+        temperature: float, 
+        *, 
+        tolerance: float|None = None,
+        stabilize_timeout: float = 0
+    ) -> bool:
+        """
+        Check if the device is at the target temperature
+        """
+        data = self.getData()
+        if data is None:
+            return False
+        
+        tolerance = tolerance or self.temp_tolerance
+        stabilize_timeout = stabilize_timeout or self.stabilize_timeout
+        if abs(data.temperature - temperature) > tolerance:
+            self._stabilize_start_time = None
+            return False
+        self._stabilize_start_time = self._stabilize_start_time or time.perf_counter()
+        if ((time.perf_counter()-self._stabilize_start_time) < stabilize_timeout):
+            return False
+        return True
+    
+    def getPH(self) -> float|None:
+        """
+        Get temperature
+        """
+        data = self.getData()
+        if data is None:
+            return None
+        return data.pH
+    
+    def getTemperature(self) -> float|None:
+        """
+        Get temperature
+        """
+        data = self.getData()
+        if data is None:
+            return None
+        return data.temperature
+    
