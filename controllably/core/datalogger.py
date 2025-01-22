678a2dcf2edd509ca312a9109e589cf3c833b1eb
Chang Jie Leong <changjie9@gmail.com>
2025-01-22 16:33:00 +0800

    Update datalogger.py
    - added `monitor_plot()`

--- controllably/core/datalogger.py
+++ controllably/core/datalogger.py
@@ -9,6 +9,7 @@ import time
 from typing import NamedTuple, Any, Iterable
 
 # Third party imports
+import matplotlib.pyplot as plt
 import pandas as pd
 
 # Local application imports
@@ -65,3 +66,29 @@ def stream(
         event.clear()
     return
 
+def monitor_plot(data_store: Iterable[tuple[NamedTuple,datetime]], y: str, x: str = 'timestamp', stop_trigger: threading.Event|None = None):
+    assert hasattr(__builtins__,'__IPYTHON__'), "This function is intended for use in Jupyter notebooks / IPython sessions"
+    from IPython.display import display, clear_output
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    timestamp = None
+    stop_trigger = stop_trigger if isinstance(stop_trigger, threading.Event) else threading.Event()
+    count = 0
+    while not stop_trigger.is_set() and count<10:
+        time.sleep(0.1)
+        if not len(data_store):
+            continue
+        if data_store[-1][1] != timestamp:
+            count = 0
+            timestamp = data_store[-1][1]
+            df = get_dataframe(data_store=data_store, fields=(x,y))
+            ax.cla()
+            ax.plot(df[x], df[y], label=y.title())
+            ax.legend(loc='upper left')
+            plt.tight_layout()
+            display(fig)
+            clear_output(wait=True)
+        else:
+            count += 1
+    # display(fig)
+    return stop_trigger
