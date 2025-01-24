2c16483e5d2be4677d25a3615b12be75e88aa1f7
Chang Jie Leong <changjie9@gmail.com>
2025-01-24 17:39:30 +0800

    Update sartorius_lib.py

--- controllably/Transfer/Liquid/Sartorius/sartorius_lib.py
+++ controllably/Transfer/Liquid/Sartorius/sartorius_lib.py
@@ -23,8 +23,8 @@ import logging
 logger = logging.getLogger(__name__)
 logger.debug(f"Import: OK <{__name__}>")
 
-PresetSpeeds = namedtuple('PresetSpeeds', ['s1','s2','s3','s4','s5','s6'])
-"""PresetSpeeds is a named tuple for a set of the 6 preset speeds provided with each model"""
+# PresetSpeeds = namedtuple('PresetSpeeds', ['s1','s2','s3','s4','s5','s6'])
+# """PresetSpeeds is a named tuple for a set of the 6 preset speeds provided with each model"""
 SpeedParameters = namedtuple('SpeedParameters', ['preset', 'intervals', 'step_size', 'delay'])
 """SpeedParameters is a named tuple for a set of calculated parameters for achieving intermediate speeds"""
 
@@ -50,7 +50,7 @@ class Model:
     max_position: int
     tip_eject_position: int 
     resolution: float
-    preset_speeds: PresetSpeeds
+    preset_speeds: tuple[int|float] # PresetSpeeds
 
 class ErrorCode(Enum):
     er1 = 'The command has not been understood by the module'
@@ -59,10 +59,10 @@ class ErrorCode(Enum):
     er4 = 'The drive is on and the command or query cannot be answered'
     
 class ModelInfo(Enum):
-    BRL0        = Model('BRL0',0,30,443,-40,0.5,PresetSpeeds(60,106,164,260,378,448))
-    BRL200      = Model('BRL200',200,30,443,-40,0.5,PresetSpeeds(31,52,80,115,150,190))
-    BRL1000     = Model('BRL1000',1000,30,443,-40,2.5,PresetSpeeds(150,265,410,650,945,1120))
-    BRL5000     = Model('BRL5000',5000,30,580,-55,10,PresetSpeeds(550,1000,1500,2500,3650,4350))
+    BRL0        = Model('BRL0',0,30,443,-40,0.5,(60,106,164,260,378,448))
+    BRL200      = Model('BRL200',200,30,443,-40,0.5,(31,52,80,115,150,190))
+    BRL1000     = Model('BRL1000',1000,30,443,-40,2.5,(150,265,410,650,945,1120))
+    BRL5000     = Model('BRL5000',5000,30,580,-55,10,(550,1000,1500,2500,3650,4350))
 
 class StatusCode(Enum):
     Normal          = 0
