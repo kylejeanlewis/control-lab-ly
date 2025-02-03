921babc256d89185050062e12d04514a8d012b86
Chang Jie Leong <changjie9@gmail.com>
2025-02-03 14:32:58 +0800

    Update __init__.py

--- controllably/Transfer/Liquid/Sartorius/__init__.py
+++ controllably/Transfer/Liquid/Sartorius/__init__.py
@@ -5,6 +5,7 @@ Classes:
     Sartorius (LiquidHandler)
 """
 from .sartorius_utils import Sartorius
+# from .sartorius import Sartorius
 
 from controllably import include_this_module
 include_this_module(get_local_only=False)
