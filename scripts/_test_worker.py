f37e4f4370b299aeef417085d2c3afefbe90fad8
Chang Jie Leong <changjie9@gmail.com>
2025-02-09 19:59:47 +0800

    Create _test_worker.py

---
+++ scripts/_test_worker.py
@@ -0,0 +1,39 @@
+# %%
+import threading
+
+import test_init
+from controllably.core.control import Controller, JSONInterpreter, TwoTierQueue, start_server, start_client
+
+# %% Client-server version
+if __name__ == "__main__":
+    host = "127.0.0.1"  # Or "localhost"
+    port = 12345       # Choose a free port (above 1024 is recommended)
+    worker = Controller('model', JSONInterpreter())
+    worker.start()
+    
+    # Start server in a separate thread (so the client can run concurrently)
+    server_thread = threading.Thread(target=start_server, args=(host, port, worker))
+    server_thread.daemon = True  # Allow the main thread to exit even if the server is running
+    server_thread.start()
+
+# %% Hub-spoke version
+if __name__ == "__main__":
+    host = "127.0.0.1"  # Or "localhost"
+    port = 12345       # Choose a free port (above 1024 is recommended)
+    worker = Controller('model', JSONInterpreter())
+    worker.start()
+    
+    # Start client in a separate thread
+    worker_thread = threading.Thread(target=start_client, args=(host, port, worker, True))
+    worker_thread.daemon = True  # Allow the main thread to exit even if the server is running
+    worker_thread.start()
+
+# %%
+q = TwoTierQueue()
+worker.register(q)
+
+# %%
+q.put_nowait('12345')
+
+
+# %%
