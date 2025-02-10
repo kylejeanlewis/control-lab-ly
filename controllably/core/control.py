b461cf5791787767ac911cf863b93e08f146e78d
kylejeanlewis <changjie9@gmail.com>
2025-02-10 22:59:37 +0800

    Transfer `Proxy` from gui to control

--- controllably/core/control.py
+++ controllably/core/control.py
@@ -634,3 +634,54 @@ def start_client(host:str, port:int, controller: Controller, relay:bool = False)
     except Exception as e:
         print(f"Error connecting to server: {e}")
     return
+
+
+class Proxy:
+    def __new__(cls, prime:Callable, object_id:str|None = None):
+        new_class = cls.factory(prime, object_id)
+        return super(Proxy,cls).__new__(new_class)
+    
+    def __init__(self, prime:Callable, object_id:str|None = None):
+        self.prime = prime
+        self.object_id = object_id or id(prime)
+        self.controller: Controller|None = None
+        self.remote = False
+        return
+    
+    @classmethod
+    def factory(cls, prime:Callable, object_id:str|None = None):
+        name = prime.__name__ if inspect.isclass(prime) else prime.__class__.__name__
+        object_id = object_id or id(prime)
+        attrs = {attr:cls.makeEmitter(getattr(prime,attr)) for attr in dir(prime) if callable(getattr(prime,attr)) and (attr not in dir(cls))}
+        attrs.update({"_channel_class":prime})
+        new_class = type(f"{name}_Proxy-{object_id}", (cls,), attrs)
+        return new_class
+    
+    @staticmethod
+    def makeEmitter(method):
+        def emitter(self, *args, **kwargs):
+            if not self.remote and not inspect.isclass(self.prime):
+                prime_method = getattr(self.prime, method.__name__)
+                return prime_method(*args, **kwargs)
+            
+            assert isinstance(self.controller, Controller), 'No controller is bound to this Proxy.'
+            command = dict(
+                object_id = self.object_id,
+                method = method.__name__,
+                args = args,
+                kwargs = kwargs
+            )
+            return self.controller.transmitRequest(command)
+        emitter.__name__ = method.__name__
+        emitter.__doc__ = method.__doc__
+        emitter.__signature__ = inspect.signature(method)
+        return emitter
+    
+    def bindController(self, controller: Controller):
+        self.controller = controller
+        return
+    
+    def unbindController(self):
+        self.controller = None
+        return
+    
--- controllably/core/gui.py
+++ controllably/core/gui.py
@@ -44,54 +44,7 @@ class GUI:
         return
     
     def updateValues(self):
-        raise NotImplementedError    
-
-
-class Proxy:
-    def __new__(cls, prime:Callable, object_id:str|None = None):
-        new_class = cls.factory(prime, object_id)
-        return super(Proxy,cls).__new__(new_class)
+        raise NotImplementedError
     
-    def __init__(self, prime:Callable, object_id:str|None = None):
-        self.prime = prime
-        self.object_id = object_id or id(prime)
-        self.controller = None
-        self.remote = False
-        return
-    
-    @classmethod
-    def factory(cls, prime:Callable, object_id:str|None = None):
-        name = prime.__name__ if inspect.isclass(prime) else prime.__class__.__name__
-        object_id = object_id or id(prime)
-        attrs = {attr:cls.makeEmitter(getattr(prime,attr)) for attr in dir(prime) if callable(getattr(prime,attr)) and (attr not in dir(cls))}
-        attrs.update({"_channel_class":prime})
-        new_class = type(f"{name}_Proxy-{object_id}", (cls,), attrs)
-        return new_class
-    
-    @staticmethod
-    def makeEmitter(method):
-        method_signature = inspect.signature(method)
-        def emitter(self, *args, **kwargs):
-            assert isinstance(self.controller, Controller), 'No controller is bound to this Proxy.'
-            command = dict(
-                object_id = self.object_id,
-                method = method.__name__,
-                args = args,
-                kwargs = kwargs
-            )
-            if not self.remote and not inspect.isclass(self.prime):
-                prime_method = getattr(self.prime, method.__name__)
-                return prime_method(*args, **kwargs)
-            return self.controller.transmitRequest(command)
-        emitter.__name__ = method.__name__
-        emitter.__doc__ = method.__doc__
-        emitter.__signature__ = method_signature
-        return emitter
-    
-    def bindController(self, controller: Controller):
-        self.controller = controller
-        return
-    
-    def unbindController(self):
-        self.controller = None
-        return
+    def addTo(self, master: tk.Misc):
+        raise NotImplementedError
