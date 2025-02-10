3418d070f100cdfd46d38f95032f33bea4a2428b
Chang Jie Leong <changjie9@gmail.com>
2025-02-10 16:05:51 +0800

    Create gui.py

---
+++ controllably/core/gui.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Standard library imports
+import tkinter as tk
+
+# Local imports
+from controllably.core.control import Controller
+
+class GUI:
+    def __init__(self, controller: Controller):
+        self.controller = controller
+        self.object_id = ''
+        self.widget = None
+        return
+    
+    def bindObject(self, object_id: str):
+        self.object_id = object_id
+        return
+    
+    def unbindObject(self):
+        self.object_id = ''
+        return
+    
+    def bindWidget(self, widget: tk.Tk):
+        self.widget = widget
+        return
+    
+    def unbindWidget(self):
+        self.widget = None
+        return
+    
+    def close(self):
+        assert isinstance(self.widget, tk.Tk), 'No widget is bound to this GUI'
+        self.widget.quit()
+        self.widget.destroy()
+        return
+
+    def sendCommand(self, command: dict):
+        assert len(self.object_id), 'No tool is bound to this GUI'
+        request = dict(object_id=self.object_id)
+        request.update(command)
+        self.controller.transmitRequest(request)
+        return
+    
+    def updateValues(self):
+        raise NotImplementedError    
+
