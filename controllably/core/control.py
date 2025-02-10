753997d89e67a054baaef6d7666d9dd3746f10fe
kylejeanlewis <changjie9@gmail.com>
2025-02-10 23:25:20 +0800

    Update control.py
    - arrange `Proxy` to top

--- controllably/core/control.py
+++ controllably/core/control.py
@@ -130,9 +130,18 @@ class Interpreter:
         data = package
         return data
     
+class Proxy:
+    def __new__(cls, prime:Callable, object_id:str|None = None):
+        new_class = cls.factory(prime, object_id)
+        return super(Proxy,cls).__new__(new_class)
     
 class JSONInterpreter(Interpreter):
     def __init__(self):
+    def __init__(self, prime:Callable, object_id:str|None = None):
+        self.prime = prime
+        self.object_id = object_id or id(prime)
+        self.controller: Controller|None = None
+        self.remote = False
         return
     
     @staticmethod
@@ -148,17 +157,50 @@ class JSONInterpreter(Interpreter):
             data.update(dict(data=f"{data['data'].__class__.__name__}[{data['data']!r}]"))
             package = json.dumps(data).encode('utf-8')
         return package
+    @classmethod
+    def factory(cls, prime:Callable, object_id:str|None = None):
+        name = prime.__name__ if inspect.isclass(prime) else prime.__class__.__name__
+        object_id = object_id or id(prime)
+        attrs = {attr:cls.makeEmitter(getattr(prime,attr)) for attr in dir(prime) if callable(getattr(prime,attr)) and (attr not in dir(cls))}
+        attrs.update({"_channel_class":prime})
+        new_class = type(f"{name}_Proxy-{object_id}", (cls,), attrs)
+        return new_class
     
     @staticmethod
     def encodeRequest(command: Mapping[str, Any]) -> Message|str|bytes:
         request = json.dumps(command).encode('utf-8')
         return request
+    def makeEmitter(method):
+        def emitter(self, *args, **kwargs):
+            if not self.remote and not inspect.isclass(self.prime):
+                prime_method = getattr(self.prime, method.__name__)
+                return prime_method(*args, **kwargs)
+            
+            assert isinstance(self.controller, Controller), 'No controller is bound to this Proxy.'
+            command = dict(
+                object_id = self.object_id,
+                method = method.__name__,
+                args = args,
+                kwargs = kwargs
+            )
+            return self.controller.transmitRequest(command)
+        emitter.__name__ = method.__name__
+        emitter.__doc__ = method.__doc__
+        emitter.__signature__ = inspect.signature(method)
+        return emitter
     
     @staticmethod
     def decodeData(package: Message|str|bytes) -> Any:
         data = json.loads(package)
         return data
+    def bindController(self, controller: Controller):
+        self.controller = controller
+        return
     
+    def unbindController(self):
+        self.controller = None
+        return
+
 
 class Controller:
     def __init__(self, role: str, interpreter: Interpreter):
@@ -634,54 +676,3 @@ def start_client(host:str, port:int, controller: Controller, relay:bool = False)
     except Exception as e:
         print(f"Error connecting to server: {e}")
     return
-
-
-class Proxy:
-    def __new__(cls, prime:Callable, object_id:str|None = None):
-        new_class = cls.factory(prime, object_id)
-        return super(Proxy,cls).__new__(new_class)
-    
-    def __init__(self, prime:Callable, object_id:str|None = None):
-        self.prime = prime
-        self.object_id = object_id or id(prime)
-        self.controller: Controller|None = None
-        self.remote = False
-        return
-    
-    @classmethod
-    def factory(cls, prime:Callable, object_id:str|None = None):
-        name = prime.__name__ if inspect.isclass(prime) else prime.__class__.__name__
-        object_id = object_id or id(prime)
-        attrs = {attr:cls.makeEmitter(getattr(prime,attr)) for attr in dir(prime) if callable(getattr(prime,attr)) and (attr not in dir(cls))}
-        attrs.update({"_channel_class":prime})
-        new_class = type(f"{name}_Proxy-{object_id}", (cls,), attrs)
-        return new_class
-    
-    @staticmethod
-    def makeEmitter(method):
-        def emitter(self, *args, **kwargs):
-            if not self.remote and not inspect.isclass(self.prime):
-                prime_method = getattr(self.prime, method.__name__)
-                return prime_method(*args, **kwargs)
-            
-            assert isinstance(self.controller, Controller), 'No controller is bound to this Proxy.'
-            command = dict(
-                object_id = self.object_id,
-                method = method.__name__,
-                args = args,
-                kwargs = kwargs
-            )
-            return self.controller.transmitRequest(command)
-        emitter.__name__ = method.__name__
-        emitter.__doc__ = method.__doc__
-        emitter.__signature__ = inspect.signature(method)
-        return emitter
-    
-    def bindController(self, controller: Controller):
-        self.controller = controller
-        return
-    
-    def unbindController(self):
-        self.controller = None
-        return
-    
