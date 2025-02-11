7dfc49a58f883fde7fb3ba1afe98991a3bdbf62e
Chang Jie Leong <changjie9@gmail.com>
2025-02-11 10:24:24 +0800

    Update _test_hub.py

--- scripts/_test_hub.py
+++ scripts/_test_hub.py
@@ -2,16 +2,15 @@
 import threading
 
 import test_init
-from controllably.core.control import Controller, JSONInterpreter, start_server
+from controllably.core.control import Controller, start_server
+from controllably.core.interpreter import JSONInterpreter
 
-if __name__ == "__main__":
-    host = "127.0.0.1"  # Or "localhost"
-    port = 12345       # Choose a free port (above 1024 is recommended)
-    hub = Controller('relay', JSONInterpreter())
-    
-    # Start server in a separate thread (so the client can run concurrently)
-    server_thread = threading.Thread(target=start_server, args=(host, port, hub))
-    server_thread.daemon = True  # Allow the main thread to exit even if the server is running
-    server_thread.start()
+host = "127.0.0.1"  # Or "localhost"
+port = 12345       # Choose a free port (above 1024 is recommended)
+hub = Controller('relay', JSONInterpreter())
+
+# Start server in a separate thread (so the client can run concurrently)
+hub_thread = threading.Thread(target=start_server, args=(host, port, hub) ,daemon=True)
+hub_thread.start()
     
 # %%
