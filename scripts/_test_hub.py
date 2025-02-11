981a871dac624dc748d5378db40dde5ce1963855
Chang Jie Leong <changjie9@gmail.com>
2025-02-11 12:13:58 +0800

    Update _test_hub.py

--- scripts/_test_hub.py
+++ scripts/_test_hub.py
@@ -5,12 +5,14 @@ import test_init
 from controllably.core.control import Controller, start_server
 from controllably.core.interpreter import JSONInterpreter
 
-host = "127.0.0.1"  # Or "localhost"
-port = 12345       # Choose a free port (above 1024 is recommended)
+host = "127.0.0.1"
+port = 12345
 hub = Controller('relay', JSONInterpreter())
+terminate = threading.Event()
+args = [host, port, hub]
+kwargs = dict(terminate=terminate)
 
-# Start server in a separate thread (so the client can run concurrently)
-hub_thread = threading.Thread(target=start_server, args=(host, port, hub) ,daemon=True)
+hub_thread = threading.Thread(target=start_server, args=args, kwargs=kwargs, daemon=True)
 hub_thread.start()
     
 # %%
