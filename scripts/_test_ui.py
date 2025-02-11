f5fe7be62edbd4b7291c83e613087c7601417989
Chang Jie Leong <changjie9@gmail.com>
2025-02-11 12:14:10 +0800

    Update _test_ui.py

--- scripts/_test_ui.py
+++ scripts/_test_ui.py
@@ -6,19 +6,20 @@ import test_init
 from controllably.core.control import Controller, Proxy, start_client
 from controllably.core.interpreter import JSONInterpreter
 
-# %%
-host = "127.0.0.1"  # Or "localhost"
-port = 12345       # Choose a free port (above 1024 is recommended)
+host = "127.0.0.1"
+port = 12345 
 ui = Controller('view', JSONInterpreter())
+terminate = threading.Event()
 args = [host, port, ui]
+kwargs = dict(terminate=terminate)
 
 # %% Server-client version
-ui_thread = threading.Thread(target=start_client, args=args, daemon=True)
+ui_thread = threading.Thread(target=start_client, args=args, kwargs=kwargs, daemon=True)
 ui_thread.start()
     
 # %% Hub-spoke version
 args.append(True)
-ui_thread = threading.Thread(target=start_client, args=args, daemon=True)
+ui_thread = threading.Thread(target=start_client, args=args, kwargs=kwargs, daemon=True)
 ui_thread.start()
 
 # %%
@@ -26,28 +27,30 @@ methods = ui.getMethods(private=True)
 
 # %%
 command = dict(object_id=list(methods.keys())[0], method='qsize')
-ui.transmitRequest(command)
+size_request = ui.transmitRequest(command)
 
 command = dict(object_id=list(methods.keys())[0], method='get_nowait')
-ui.transmitRequest(command)
+content_request = ui.transmitRequest(command)
 
 # %%
+size = ui.retrieveData(size_request)
+content = ui.retrieveData(content_request)
 ui.data_buffer
 
 # %%
 from controllably.Move.Cartesian import Gantry
 from controllably.GUI import MoveGUI
 
-g = Gantry('COM0',[[100,100,100],[-100,-100,-100]], simulation=True)
-p = Proxy(g, 'MOVER')
-p.bindController(ui)
-
-# %%
-gui = MoveGUI(p)
+gantry = Gantry('COM0',[[100,100,100],[-100,-100,-100]], simulation=True)
+proxy = Proxy(gantry, 'MOVER')
+gui = MoveGUI()
 
 # %%
 root = tk.Tk()
 gui.addTo(root)
+
+proxy.bindController(ui)
+gui.bindObject(proxy)
 gui.bindWidget(root)
 root.mainloop()
 
