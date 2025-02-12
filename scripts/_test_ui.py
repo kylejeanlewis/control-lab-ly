b8fc1378e6428ca43fc910b8c8ddd7ad31d52819
kylejeanlewis <changjie9@gmail.com>
2025-02-13 01:01:13 +0800

    Update _test_ui.py

--- scripts/_test_ui.py
+++ scripts/_test_ui.py
@@ -1,8 +1,37 @@
 # %%
-import test_init
-from controllably.GUI import MoveGUI
+import tkinter as tk
 
-app = MoveGUI()
+import test_init
+from controllably.GUI import gui
+from controllably.GUI import move_gui
+
+# %%
+import importlib
+importlib.reload(gui)
+importlib.reload(move_gui)
+
+# %%
+app = move_gui.MoveGUI()
+app.show()
+
+# %%
+root = tk.Tk()
+left = tk.Frame(root)
+right = tk.Frame(root)
+
+left.grid(row=0, column=0)
+right.grid(row=0, column=1)
+
+lgui = app.addTo(left)
+rgui = app.addTo(right)
+root.mainloop()
+
+# %%
+from controllably.Move.Cartesian import Gantry
+gantry = Gantry('COM0', limits=[[100,100,100],[-100,-100,-100]], simulation=True)
+
+# %%
+app.bindObject(gantry)
 app.show()
 
 # %%
