07a1d98f4b86f83b678b4e86a287388933d14842
Chang Jie Leong <changjie9@gmail.com>
2025-02-17 14:16:48 +0800

    Update transfer_gui.py

--- controllably/GUI/transfer_gui.py
+++ controllably/GUI/transfer_gui.py
@@ -13,7 +13,7 @@ from .gui import Panel
 logger = logging .getLogger(__name__)
 
 PRECISION = 1
-TICK_INTERVAL = 100
+TICK_INTERVAL = 200
 
 class Liquid(Protocol):
     capacity: float
@@ -43,17 +43,15 @@ class LiquidPanel(Panel):
         self.title = "Liquid Handler Control"
         self.status = 'Disconnected'
     
-        # Initialize volume values
+        # Initialize values
         self.reagent: str|None =  None
         self.capacity = 1000
         self.volume = 0
+        self.channel = 0
         
         # Fields
-        self.reagent_field = '<empty>'
         self.volume_field = 0
         self.speed_field = None
-        self.cycles_field = 1
-        self.delay_field = 0
         
         # Settings
         self.precision = PRECISION
@@ -64,33 +62,23 @@ class LiquidPanel(Panel):
         # Status
         if not self.getAttribute('is_connected', False):
             self.status = 'Disconnected'
-            return self
+            # return self.refresh()
         elif self.getAttribute('is_busy', False):
             self.status = 'Busy'
-            return self.refresh()
+            # return self.refresh()
         else:
             self.status = 'Connected'
             
         # Values
         self.capacity = self.getAttribute('capacity') or self.capacity
         self.volume = self.getAttribute('volume') or self.volume
+        self.channel = self.getAttribute('channel') or self.channel
         
         # Fields
-        reagent = self.entry_reagent.get()
-        volume = self.entry_volume.get()
-        speed = self.entry_speed.get()
-        cycles = self.entry_cycles.get()
-        delay = self.entry_delay.get()
-        print(f"{reagent=}")
-        print(f"{volume=}")
-        print(f"{speed=}")
-        print(f"{cycles=}")
-        print(f"{delay=}")
-        self.reagent_field = reagent if len(reagent) else self.reagent
-        self.volume_field = volume if len(volume) else 0
-        self.speed_field = speed if len(speed) else None
-        self.cycles_field = cycles if len(cycles) else 1
-        self.delay_field = delay if len(delay) else 0
+        # volume = self.entry_volume.get()
+        # speed = self.entry_speed.get()
+        # self.volume_field = volume if len(volume) else 0
+        # self.speed_field = speed if len(speed) else None
         return self.refresh()
     
     def refresh(self, **kwargs):
@@ -99,7 +87,7 @@ class LiquidPanel(Panel):
         
         # Update labels
         self.label_status.config(text=self.status)
-        self.label_current_reagent.config(text=self.reagent)
+        self.label_current_reagent.config(text=(self.reagent or "<None>"))
         self.label_capacity.config(text=f"{self.capacity} µL")
         
         # Update scales
@@ -107,78 +95,84 @@ class LiquidPanel(Panel):
         
         # Update entries
         self.entry_reagent.delete(0, tk.END)
-        self.entry_reagent.insert(0, self.reagent_field)
+        self.entry_reagent.insert(0, str(self.reagent).replace('None',''))
+        entry_reagent_state = tk.DISABLED if self.reagent else tk.NORMAL
+        self.entry_reagent.config(state=entry_reagent_state)
+        
         self.entry_volume.delete(0, tk.END)
         self.entry_volume.insert(0, str(self.volume_field))
+        
         self.entry_speed.delete(0, tk.END)
-        self.entry_speed.insert(0, str(self.speed_field))
-        self.entry_cycles.delete(0, tk.END)
-        self.entry_cycles.insert(0, str(self.cycles_field))
-        self.entry_delay.delete(0, tk.END)
-        self.entry_delay.insert(0, str(self.delay_field))
+        self.entry_speed.insert(0, str(self.speed_field).replace('None',''))
+        
+        # self.entry_cycles.delete(0, tk.END)
+        # self.entry_cycles.insert(0, str(self.cycles_field))
+        
+        # self.entry_delay.delete(0, tk.END)
+        # self.entry_delay.insert(0, str(self.delay_field))
         return
     
     def addTo(self, master: tk.Tk|tk.Frame, size: tuple[int,int]|None = None) -> tuple[int,int]|None:
         BUTTON_HEIGHT = 1
         BUTTON_WIDTH = 6
+        SCALE_LENGTH = 200
         
         # Add layout
         master.rowconfigure(1,weight=1, minsize=13*BUTTON_WIDTH)
         master.columnconfigure(0,weight=1, minsize=9*BUTTON_WIDTH)
         
         # Add keyboard events
-        master.bind('<Up>', lambda event: self.aspirate(axis='y', value=0.1))
-        master.bind('<Down>', lambda event: self.dispense(axis='y', value=-0.1))
-        master.bind('<Shift-Up>', lambda event: self.fill(axis='z', value=0.1))
-        master.bind('<Shift-Down>', lambda event: self.empty(axis='z', value=-0.1))
+        master.bind('<Up>', lambda event: self.aspirate(volume=float(self.entry_volume.get()), speed=self.entry_speed.get(), reagent=self.entry_reagent.get()))
+        master.bind('<Down>', lambda event: self.dispense(volume=float(self.entry_volume.get()), speed=self.entry_speed.get()))
+        master.bind('<Shift-Up>', lambda event: self.fill(speed=self.entry_speed.get(), reagent=self.entry_reagent.get()))
+        master.bind('<Shift-Down>', lambda event: self.empty(speed=self.entry_speed.get()))
         master.bind('.', lambda event: self.blowout())
         
         # Create frames for organization
         status_frame = ttk.Frame(master)
         status_frame.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
-        status_frame.columnconfigure(0,weight=1)
+        status_frame.grid_columnconfigure(0,weight=1)
         
         volume_frame = ttk.Frame(master)
         volume_frame.grid(row=1, column=0, padx=10, pady=10, sticky='nsew')
-        volume_frame.grid_rowconfigure([0,1],weight=1)
-        volume_frame.grid_columnconfigure(1,weight=1)
+        volume_frame.grid_rowconfigure(0,weight=1)
+        # volume_frame.grid_columnconfigure(1,weight=1)
         
         scale_frame = ttk.Frame(volume_frame)
         scale_frame.grid(row=0, column=0, padx=10, pady=10, sticky='nsew')
         scale_frame.grid_rowconfigure(2,weight=1)
         
         button_frame = ttk.Frame(volume_frame)
-        button_frame.grid(row=0, column=1, padx=10, pady=10, sticky='nsew')
+        button_frame.grid(row=0, column=1, padx=10, pady=10, sticky='nse')
         button_frame.grid_rowconfigure([0,1,2,3,4],weight=1)
-        button_frame.grid_columnconfigure(0,weight=1)
+        # button_frame.grid_columnconfigure(0,weight=1)
         
         label_frame = ttk.Frame(volume_frame)
         label_frame.grid(row=1, column=0, padx=(10,0), pady=10, sticky='nsew')
-        label_frame.grid_rowconfigure([0,1,2,3,4],weight=1)
+        # label_frame.grid_rowconfigure([0,1,2,3,4],weight=1)
         
         input_frame = ttk.Frame(volume_frame)
         input_frame.grid(row=1, column=1, padx=(0,10), pady=10, sticky='nsew')
-        input_frame.grid_rowconfigure([0,1,2,3,4],weight=1)
-        input_frame.grid_columnconfigure(0,weight=1)
+        # input_frame.grid_rowconfigure([0,1,2,3,4],weight=1)
+        # input_frame.grid_columnconfigure(0,weight=1)
         
         # Status Display
-        self.button_close = ttk.Button(status_frame, text='Terminate', command=self.close)
         self.button_refresh = ttk.Button(status_frame, text='Refresh', command=self.update)
         self.label_status = ttk.Label(status_frame, text="Disconnected")
-        self.button_close.grid(row=0, column=1)
-        self.button_refresh.grid(row=1, column=1)
-        self.label_status.grid(row=2, column=1)
+        self.button_refresh.grid(row=0, column=1)
+        self.label_status.grid(row=1, column=1)
 
         # Volume Controls
-        self.label_current_reagent = ttk.Label(scale_frame, text="<empty>")
+        self.label_channel = ttk.Label(scale_frame, text=f"Channel {self.channel}")
         self.label_capacity = ttk.Label(scale_frame, text=f"{self.capacity} µL")
-        self.label_zero = ttk.Label(scale_frame, text="0 µL")
-        self.label_current_reagent.grid(row=0, column=0)
+        self.label_current_reagent = ttk.Label(scale_frame, text="<None>")
+        self.label_channel.grid(row=0, column=0)
         self.label_capacity.grid(row=1, column=0)
-        self.label_zero.grid(row=3, column=0)
+        self.label_current_reagent.grid(row=3, column=0)
         
-        self.scale_volume = tk.Scale(scale_frame, from_=1000, to=0, orient=tk.VERTICAL, length=BUTTON_HEIGHT*10, width=BUTTON_WIDTH, tickinterval=100)
-        self.scale_volume.bind("<ButtonRelease-1>", lambda event: self.volumeTo(volume=float(self.scale_volume.get())))
+        
+        self.scale_volume = tk.Scale(scale_frame, from_=self.capacity, to=0, orient=tk.VERTICAL, length=SCALE_LENGTH, width=BUTTON_WIDTH, tickinterval=self.tick_interval)
+        self.scale_volume.bind("<ButtonRelease-1>", lambda event: self.volumeTo(volume=float(self.scale_volume.get()), speed=self.entry_speed.get(), reagent=self.entry_reagent.get()))
         self.scale_volume.grid(row=2, column=0, sticky='nsew')
         
         # Buttons
@@ -192,36 +186,26 @@ class LiquidPanel(Panel):
         self.label_reagent = ttk.Label(label_frame, text="Reagent", justify=tk.RIGHT)
         self.label_volume = ttk.Label(label_frame, text="Volume", justify=tk.RIGHT)
         self.label_speed = ttk.Label(label_frame, text="Speed", justify=tk.RIGHT)
-        self.label_cycles = ttk.Label(label_frame, text="Cycles", justify=tk.RIGHT)
-        self.label_delay = ttk.Label(label_frame, text="Delay", justify=tk.RIGHT)
         self.label_reagent.grid(row=0, column=0, sticky='e')
         self.label_volume.grid(row=1, column=0, sticky='e')
         self.label_speed.grid(row=2, column=0, sticky='e')
-        self.label_cycles.grid(row=3, column=0, sticky='e')
-        self.label_delay.grid(row=4, column=0, sticky='e')
         
-        self.entry_reagent = ttk.Entry(input_frame, width=BUTTON_WIDTH, justify=tk.CENTER)#, textvariable=self.reagent_field)
-        self.entry_volume = ttk.Entry(input_frame, width=BUTTON_WIDTH, justify=tk.CENTER)#, textvariable=self.volume_field)
-        self.entry_speed = ttk.Entry(input_frame, width=BUTTON_WIDTH, justify=tk.CENTER)#, textvariable=self.speed_field)
-        self.entry_cycles = ttk.Entry(input_frame, width=BUTTON_WIDTH, justify=tk.CENTER)#, textvariable=self.cycles_field)
-        self.entry_delay = ttk.Entry(input_frame, width=BUTTON_WIDTH, justify=tk.CENTER)#, textvariable=self.delay_field)
+        self.entry_reagent = ttk.Entry(input_frame, width=2*BUTTON_WIDTH, justify=tk.CENTER)
+        self.entry_volume = ttk.Entry(input_frame, width=2*BUTTON_WIDTH, justify=tk.CENTER)
+        self.entry_speed = ttk.Entry(input_frame, width=2*BUTTON_WIDTH, justify=tk.CENTER)
         self.entry_reagent.grid(row=0, column=0, sticky='nsew')
         self.entry_volume.grid(row=1, column=0, sticky='nsew')
         self.entry_speed.grid(row=2, column=0, sticky='nsew')
-        self.entry_cycles.grid(row=3, column=0, sticky='nsew')
-        self.entry_delay.grid(row=4, column=0, sticky='nsew')
-        return super().addTo(master, (2*BUTTON_WIDTH,13*BUTTON_HEIGHT))
+        return super().addTo(master, (5*BUTTON_WIDTH,13*BUTTON_HEIGHT))
 
-    def aspirate(self, volume:float, speed:float|None = None, reagent:str|None = None):
-        try:
-            speed = float(speed)
-        except ValueError:
-            if isinstance(speed, str) and speed=='None':
-                speed = None
-            else:
-                logger.warning('Ensure input for speed is of type float')
+    def aspirate(self, volume:float, speed:float|str|None = None, reagent:str|None = None):
+        speed = float(speed) if (isinstance(speed,str) and len(speed)) else self.speed_field
+        reagent = self.reagent or reagent
         self.volume = min(self.volume + volume, self.capacity)
         self.reagent = reagent
+        
+        self.volume_field = volume
+        self.speed_field = speed
         try:
             self.execute(self.principal.aspirate, volume=volume, speed=speed, reagent=reagent)
         except AttributeError:
@@ -240,15 +224,12 @@ class LiquidPanel(Panel):
         self.refresh()
         return
     
-    def dispense(self, volume:float, speed:float|None = None):
-        try:
-            speed = float(speed)
-        except ValueError:
-            if isinstance(speed, str) and speed=='None':
-                speed = None
-            else:
-                logger.warning('Ensure input for speed is of type float')
+    def dispense(self, volume:float, speed:float|str|None = None):
+        speed = float(speed) if (isinstance(speed,str) and len(speed)) else self.speed_field
         self.volume = max(self.volume - volume, 0)
+        
+        self.volume_field = volume
+        self.speed_field = speed
         try:
             self.execute(self.principal.dispense, volume=volume, speed=speed)
         except AttributeError:
@@ -257,15 +238,10 @@ class LiquidPanel(Panel):
         self.refresh()
         return
     
-    def empty(self, speed:float|None = None):
-        try:
-            speed = float(speed)
-        except ValueError:
-            if isinstance(speed, str) and speed=='None':
-                speed = None
-            else:
-                logger.warning('Ensure input for speed is of type float')
+    def empty(self, speed:float|str|None = None):
+        speed = float(speed) if (isinstance(speed,str) and len(speed)) else self.speed_field
         self.volume = 0
+        self.speed_field = speed
         try:
             self.execute(self.principal.empty, speed=speed)
         except AttributeError:
@@ -274,16 +250,12 @@ class LiquidPanel(Panel):
         self.refresh()
         return
     
-    def fill(self, speed:float|None = None, reagent:str|None = None):
-        try:
-            speed = float(speed)
-        except ValueError:
-            if isinstance(speed, str) and speed=='None':
-                speed = None
-            else:
-                logger.warning('Ensure input for speed is of type float')
+    def fill(self, speed:float|str|None = None, reagent:str|None = None):
+        speed = float(speed) if (isinstance(speed,str) and len(speed)) else self.speed_field
+        reagent = self.reagent or reagent
         self.volume = self.capacity
         self.reagent = reagent
+        self.speed_field = speed
         try:
             self.execute(self.principal.fill, speed=speed, reagent=reagent)
         except AttributeError:
@@ -292,6 +264,18 @@ class LiquidPanel(Panel):
         self.refresh()
         return
     
-    def volumeTo(self, volume:float, speed:float|None = None):
+    def volumeTo(self, volume:float, speed:float|str|None = None, reagent:str|None = None):
         current_volume = self.volume
-        return self.aspirate(volume=volume, speed=speed) if (volume - current_volume) > 0 else self.dispense(volume=volume, speed=speed)
+        diff = volume - current_volume
+        return self.aspirate(volume=diff, speed=speed, reagent=reagent) if diff > 0 else self.dispense(volume=abs(diff), speed=speed)
+
+    def attach(self):
+        self.tip_on = True
+        self.reagent = None
+        return
+        
+    def eject(self):
+        self.tip_on = False
+        self.reagent = None
+        return
+        
