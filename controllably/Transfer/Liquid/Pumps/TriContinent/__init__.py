bd864352127e3b6defdb3800ec3dd50f2deaeb18
Chang Jie Leong <changjie9@gmail.com>
2025-02-19 12:30:10 +0800

    Update __init__.py

--- controllably/Transfer/Liquid/Pumps/TriContinent/__init__.py
+++ controllably/Transfer/Liquid/Pumps/TriContinent/__init__.py
@@ -4,7 +4,5 @@ This sub-package imports the class for syringe pumps from TriContinent.
 Classes:
     TriContinent (Pump)
 """
-from .tricontinent_utils import TriContinent
-
-from controllably import include_this_module
-include_this_module(get_local_only=False)
+from .tricontinent import TriContinent
+from .tricontinent_utils import TriContinent as TriContinentOld
