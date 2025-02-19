be997cca116ab1cdebd217e920f557ddbbc782f4
kylejeanlewis <changjie9@gmail.com>
2025-02-19 23:37:33 +0800

    Update test_init.py

--- scripts/test_init.py
+++ scripts/test_init.py
@@ -4,9 +4,9 @@ REPO = 'control-lab-le'
 ROOT = str(Path().absolute()).split(REPO)[0]
 sys.path.append(f'{ROOT}{REPO}')
 
-from controllably.core import connection, file_handler
-connection.get_ports()
+# from controllably.core import connection, file_handler
+# connection.get_ports()
 
-library = file_handler.read_config_file(f'{ROOT}{REPO}\\library\\catalogue.yaml')
-"""File reference for layout and config files"""
+# library = file_handler.read_config_file(f'{ROOT}{REPO}\\library\\catalogue.yaml')
+# """File reference for layout and config files"""
 # Helper.update_root_directory(library, REPO)
