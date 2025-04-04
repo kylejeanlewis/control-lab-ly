ad88d384ab6686798a05f6680de18775bdca6046
Chang Jie Leong <changjie9@gmail.com>
2025-04-04 13:36:01 +0800

    Update __init__.py

--- controllably/__init__.py
+++ controllably/__init__.py
@@ -3,8 +3,6 @@
 .. include:: ../docs/CHANGELOG.md
 """
 from .core import init, start_logging
-from .misc import *
-from .Control.GUI import guide_me
 
 import numpy as np
 np.set_printoptions(legacy='1.21')
