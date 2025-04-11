48057a5a64e136d01a844cc4bc9f23825ebdc87f
Chang Jie Leong <changjie9@gmail.com>
2025-04-11 13:06:27 +0800

    Update __init__.py

--- controllably/Compound/__init__.py
+++ controllably/Compound/__init__.py
@@ -4,7 +4,3 @@ This sub-package imports the base class for compound setups.
 Classes:
     CompoundSetup (ABC)
 """
-from .compound_utils import CompoundSetup
-
-from controllably import include_this_module
-include_this_module(get_local_only=False)
