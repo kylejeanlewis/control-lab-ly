454980c122c70a23bc6240e6f91ba5530c406cba
Chang Jie Leong <changjie9@gmail.com>
2025-04-25 02:01:44 +0800

    Update file reference

--- tests/core/test_factory.py
+++ tests/core/test_factory.py
@@ -1,6 +1,7 @@
 import pytest
 from dataclasses import dataclass
 import logging
+import os
 from pathlib import Path
 import sys
 from types import SimpleNamespace
@@ -18,7 +19,8 @@ from controllably.core.compound import Compound, Ensemble, Combined, Multichanne
 from controllably.core.device import SerialDevice, SocketDevice
 
 from .examples import mock_module
-import controllably
+
+HERE = str(Path(__file__).parent.parent.parent.absolute())
 
 @pytest.mark.parametrize("error, class_, kwargs", [
     (True, mock_module.TestClassError, {'a':1, 'b':2}),
@@ -168,7 +170,7 @@ def test_load_parts(monkeypatch, caplog):
     new_modules = sys.modules
     new_modules.update(dict(mock_module=mock_module))
     monkeypatch.setattr('sys.modules', new_modules)
-    config_file = Path().absolute() / 'tests/core/examples/tool.yaml'
+    config_file = os.path.join(HERE, 'tests/core/examples/tool.yaml')
     # config_file = controllably.core.file_handler.resolve_repo_filepath(config_file)
     with open(config_file, 'r') as f:
         configs = yaml.safe_load(f)
@@ -182,9 +184,9 @@ def test_load_setup_from_files(monkeypatch, caplog):
     new_modules.update(dict(mock_module=mock_module))
     monkeypatch.setattr('sys.modules', new_modules)
     monkeypatch.setattr('controllably.core.connection.get_node', lambda: '012345678901234')
-    config_file = Path().absolute() / 'tests/core/examples/tool.yaml'
+    config_file = os.path.join(HERE,'tests/core/examples/tool.yaml')
     # config_file = controllably.core.file_handler.resolve_repo_filepath(config_file)
-    registry_file = Path().absolute() / 'tests/core/examples/registry.yaml'
+    registry_file = os.path.join(HERE,'tests/core/examples/registry.yaml')
     # registry_file = controllably.core.file_handler.resolve_repo_filepath(registry_file)
     
     with caplog.at_level(logging.WARNING):
@@ -223,9 +225,9 @@ def test_load_setup_from_files(monkeypatch, caplog):
     assert device4.parts.part02.name == 'part2'
     
 def test_parse_configs():
-    config_file = Path().absolute() / 'tests/core/examples/tool.yaml'
+    config_file = os.path.join(HERE,'tests/core/examples/tool.yaml')
     # config_file = controllably.core.file_handler.resolve_repo_filepath(config_file)
-    registry_file = Path().absolute() / 'tests/core/examples/registry.yaml'
+    registry_file = os.path.join(HERE,'tests/core/examples/registry.yaml')
     # registry_file = controllably.core.file_handler.resolve_repo_filepath(registry_file)
     with open(config_file, 'r') as f:
         configs = yaml.safe_load(f)
