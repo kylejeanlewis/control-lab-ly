59697a2067673ad3a5bb6c7dff42423044914325
Chang Jie Leong <changjie9@gmail.com>
2025-05-09 18:08:17 +0800

    Create new tests for led array

---
+++ tests/Make/Light/test_ledarray.py
@@ -0,0 +1,34 @@
+# %%
+import pytest
+from controllably.Make.Light import Multi_LED, LED
+
+PORT = 'COM45'
+configs = {
+    'channels': [0,1,2,3],
+    'details': {
+        'port': PORT,
+        'baudrate': 9600,
+        'timeout': 1,
+        'verbose': True,
+        'simulation': False
+}}
+
+# %%
+@pytest.fixture
+def led_array():
+    """Fixture to create an instance of LEDArray."""
+    mled = Multi_LED(**configs)
+    mled.device.verbose = True
+    return mled
+
+# %%
+def test_set_power(led_array):
+    ...
+    
+    
+# %%
+if __name__ == "__main__":
+    mled = Multi_LED(**configs)
+    mled.device.verbose = True
+    
+# %%
