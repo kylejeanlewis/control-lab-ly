ec7b9c112379fe536f983f7e72f20ede34229e4d
Chang Jie Leong <changjie9@gmail.com>
2025-05-18 14:23:36 +0800

    Add new implementation imports

--- scripts/fastapi_client_user.py
+++ scripts/fastapi_client_user.py
@@ -67,4 +67,29 @@ queue.bindController(user)
 
 # %%
 queue.qsize()
+
+# %%
+from controllably.core.control import Controller, TwoTierQueue, Proxy
+from controllably.core.interpreter import JSONInterpreter
+from controllably.core.implementations.control.fastapi_control import FastAPIUserClient
+
+HOST = 'http://localhost:8000'
+client = FastAPIUserClient('http://localhost', 8000)
+
+# %%
+user = Controller('view', JSONInterpreter())
+user.setAddress('USER')
+
+# %%
+client.join_hub(user)
+
+# %%
+queue = Proxy(TwoTierQueue(), 'QUEUE')
+queue.bindController(user)
+
+# %%
+queue.qsize()
+# %%
+queue.get_nowait()
+
 # %%
--- scripts/fastapi_client_worker.py
+++ scripts/fastapi_client_worker.py
@@ -85,6 +85,46 @@ update_registry(worker2, workers)
 thread1 = Thread(target=create_listen_loop(worker1, sender=HOST))
 thread2 = Thread(target=create_listen_loop(worker2, sender=HOST))
 thread1.start()
-thread2.start()    
+thread2.start()
+    
+# %%
+from threading import Thread
+
+from controllably.core.control import Controller, TwoTierQueue
+from controllably.core.interpreter import JSONInterpreter
+from controllably.core.implementations.control.fastapi_control import FastAPIWorkerClient
+
+client = FastAPIWorkerClient('http://localhost', 8000)
+
+# %%
+worker1 = Controller('model', JSONInterpreter())
+worker1.setAddress('WORKER1')
+worker1.start()
+
+# %%
+worker2 = Controller('model', JSONInterpreter())
+worker2.setAddress('WORKER2')
+worker2.start()
+
+# %%
+queue = TwoTierQueue()
+queue1 = TwoTierQueue()
+worker1.register(queue, 'QUEUE')
+worker1.register(queue1, 'QUEUE1')
+client.update_registry(worker1)
+
+# %%
+queue2 = TwoTierQueue()
+worker2.register(queue, 'QUEUE2')
+client.update_registry(worker2)
+
+# %%
+thread1 = Thread(target=client.create_listen_loop(worker1, sender=client.url))
+thread2 = Thread(target=client.create_listen_loop(worker2, sender=client.url))
+thread1.start()
+thread2.start()
+
+# %%
+queue.put(12345)
 
 # %%
