69ab77121b7fa62ab37aa7308af4d337a6047a73
Chang Jie Leong <changjie9@gmail.com>
2025-05-19 00:53:02 +0800

    Update _test_worker.py

--- scripts/_test_worker.py
+++ scripts/_test_worker.py
@@ -1,46 +1,29 @@
 # %%
-import threading
-
 import test_init
-from controllably.core.connection import get_host, get_ports
-from controllably.core.control import Controller
-from controllably.core.interpreter import JSONInterpreter
-from controllably.core.implementations.control.socket_control import SocketClient, SocketServer
+from controllably.core.implementations.control import create_socket_worker
+from controllably.core.connection import get_host
 
 from controllably.core.control import TwoTierQueue
 from controllably.Move.Cartesian import Gantry
-
 from controllably.Transfer.Liquid.Pipette.Sartorius.sartorius import Sartorius
 from controllably.Transfer.Liquid.Pipette.Sartorius.sartorius_api.sartorius_api import SartoriusDevice
 from controllably.Transfer.Liquid.Pump.TriContinent.tricontinent import TriContinent
-from controllably.Transfer.Liquid.Pump.TriContinent.tricontinent_api.tricontinent_api import TriContinentDevice
 
 import logging
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
+HUB = False
+PORT = 12345
+HOST = get_host()
+
 # %%
-host = get_host()
-port = 12345
-worker = Controller('model', JSONInterpreter())
-terminate = threading.Event()
-args = [host, port, worker]
-kwargs = dict(terminate=terminate)
-worker.start()
-
-# %% Client-server version
-worker_thread = threading.Thread(target=SocketServer.start_server, args=args, kwargs=kwargs, daemon=True)
-worker_thread.start()
-
-# %% Hub-spoke version
-kwargs['relay'] = True
-worker_thread = threading.Thread(target=SocketClient.start_client, args=args, kwargs=kwargs, daemon=True)
-worker_thread.start()
+worker, worker_pack = create_socket_worker(HOST, PORT, 'WORKER', relay=HUB)
 
 # %%
 q = TwoTierQueue()
-worker.register(q)
-q.put_nowait('12345')
+worker.register(q, 'QUEUE')
+q.put_nowait('hello world')
 
 # %%
 mover = Gantry('COM0',[[100,100,100],[-100,-100,-100]], simulation=True)
