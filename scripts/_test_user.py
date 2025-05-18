f0a02ab664309b5fcff6f9929863d3e32f9a5bf7
Chang Jie Leong <changjie9@gmail.com>
2025-05-19 00:52:59 +0800

    Update _test_user.py

--- scripts/_test_user.py
+++ scripts/_test_user.py
@@ -1,94 +1,44 @@
 # %%
-import threading
-import tkinter as tk
-
 import test_init
+from controllably.core.implementations.control import create_socket_user
 from controllably.core.connection import get_host
-from controllably.core.control import Controller, Proxy, TwoTierQueue
-from controllably.core.interpreter import JSONInterpreter
-from controllably.core.implementations.control.socket_control import SocketClient
-from controllably.core.implementations.gui import MovePanel, Panel, LiquidPanel
 
+from controllably.core.control import Proxy, TwoTierQueue
 from controllably.Move.Cartesian import Gantry
 from controllably.Transfer.Liquid.Pipette.Sartorius.sartorius import Sartorius
 from controllably.Transfer.Liquid.Pump.TriContinent.tricontinent import TriContinent
+from controllably.core.implementations.gui import MovePanel, LiquidPanel
 
 import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# %%
-host = '192.109.209.100' #get_host()
-host = get_host()
-port = 12345 
-user = Controller('view', JSONInterpreter())
-terminate = threading.Event()
-args = [host, port, user]
-kwargs = dict(terminate=terminate)
-
-# %% Server-client version
-ui_thread = threading.Thread(target=SocketClient.start_client, args=args, kwargs=kwargs, daemon=True)
-ui_thread.start()
-    
-# %% Hub-spoke version
-kwargs['relay'] = True
-ui_thread = threading.Thread(target=SocketClient.start_client, args=args, kwargs=kwargs, daemon=True)
-ui_thread.start()
+HUB = False
+PORT = 12345
+HOST = get_host()
 
 # %%
-methods = user.getMethods(private=True)
-methods
+user, user_pack = create_socket_user(HOST, PORT, 'USER', relay=HUB)
 
 # %%
-p = Proxy(TwoTierQueue(),list(methods.keys())[0])
+p = Proxy(TwoTierQueue(), 'QUEUE')
 p.bindController(user)
 p.get_nowait()
 
 # %%
-# gantry = Gantry('COM0',[[100,100,100],[-100,-100,-100]], simulation=True)
-proxy = Proxy(Gantry, 'MOVER')
-gui = MovePanel()
+gantry = Gantry('COM0',[[100,100,100],[-100,-100,-100]], simulation=True)
+mover = Proxy(gantry, 'MOVER')
+mover.bindController(user)
 
 # %%
-proxy.bindController(user)
-gui.bindObject(proxy)
-gui.show()
-
-# %%
-proxy.remote = True
-position = proxy.move('x',-10)
+mover.remote = True
+position = mover.move('x',-10)
 position
 
 # %%
-proxy.remote = False
-position = proxy.move('x',10)
+mover.remote = False
+position = mover.move('x',10)
 position
-
-# %%
-gui = Panel()
-mgui = MovePanel()
-ngui = MovePanel()
-
-# %%
-gui.addPack(mgui, side=tk.LEFT)
-gui.addPack(ngui)
-gui.show()
-
-# %%
-mgui.show()
-
-# %%
-gui.addGrid(mgui, row=0, column=0)
-gui.addGrid(ngui, row=1, column=1)
-gui.show()
-
-# %%
-move_gui = MovePanel()
-move_gui.show()
-
-# %%
-liquid_gui = LiquidPanel()
-liquid_gui.show()
 
 # %%
 pipette = Proxy(Sartorius, 'PIPETTE')
@@ -99,11 +49,15 @@ pump = Proxy(TriContinent, 'PUMP')
 pump.bindController(user)
 
 # %%
+move_gui = MovePanel()
+move_gui.show()
+move_gui.bindObject(mover)
+move_gui.show()
+
+# %%
+liquid_gui = LiquidPanel()
+liquid_gui.show()
 liquid_gui.bindObject(pipette)
 liquid_gui.show()
-
-# %%
 liquid_gui.bindObject(pump)
 liquid_gui.show()
-
-# %%
