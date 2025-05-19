9eea8b02c007f90f8679ab798ac85329c6d6cb5f
Chang Jie Leong <changjie9@gmail.com>
2025-05-19 10:42:14 +0800

    Update imports

--- scripts/start_socket_hub_server.py
+++ scripts/start_socket_hub_server.py
@@ -1,5 +1,5 @@
 from controllably.core.connection import get_host
-from controllably.core.implementations.control import create_socket_hub
+from controllably.core.implementations.control.control_utils import create_socket_hub
 
 if __name__ == "__main__":
     hub, hub_pack = create_socket_hub(get_host(), 12345, 'HUB', relay=True)
