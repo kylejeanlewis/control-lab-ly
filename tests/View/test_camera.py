78586ed7058b11d5aa35d603ef1bfd37cd3dff83
Chang Jie Leong <changjie9@gmail.com>
2025-05-19 10:42:33 +0800

    Update test_camera.py
    - fix connection errors

--- tests/View/test_camera.py
+++ tests/View/test_camera.py
@@ -21,19 +21,25 @@ def camera():
     return cam
 
 def test_camera(camera):
+    camera.connect()
     assert camera.is_connected
     camera.disconnect()
     assert not camera.is_connected
-    camera.connect()
     
 def test_get_frame(camera):
+    camera.connect()
+    assert camera.is_connected
     ret, frame = camera.getFrame(True)
     assert ret
     assert ret == (frame is not None)
     assert camera.frame_size == frame.shape[-2::-1]
     camera.clear()
+    camera.disconnect()
+    assert not camera.is_connected
 
 def test_stream(camera):
+    camera.connect()
+    assert camera.is_connected
     assert len(camera.buffer) == 0
     camera.startStream()
     time.sleep(2)
@@ -42,8 +48,12 @@ def test_stream(camera):
     assert len(camera.buffer) > 0
     assert not camera.stream_event.is_set()
     camera.clear()
+    camera.disconnect()
+    assert not camera.is_connected
 
 def test_save_frame(camera, tmp_path):
+    camera.connect()
+    assert camera.is_connected
     ret, frame = camera.getFrame(True)
     assert ret
     camera.saveFrame(frame, tmp_path / "test_frame.jpg")
@@ -52,8 +62,12 @@ def test_save_frame(camera, tmp_path):
     assert saved_frame is not None
     assert saved_frame.shape == frame.shape
     camera.clear()
+    camera.disconnect()
+    assert not camera.is_connected
 
 def test_save_frames_to_video(camera, tmp_path):
+    camera.connect()
+    assert camera.is_connected
     filepath = tmp_path / "test_video.mp4"
     camera.startStream()
     assert camera.stream_event.is_set()
@@ -64,3 +78,5 @@ def test_save_frames_to_video(camera, tmp_path):
     camera.saveFramesToVideo(frames, camera.frame_rate, filepath)
     assert filepath.exists()
     camera.clear()
+    camera.disconnect()
+    assert not camera.is_connected
