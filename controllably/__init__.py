40c518674faadd0131bde9516acad9848d892c0e
Chang Jie Leong <changjie9@gmail.com>
2025-06-18 18:17:45 +0800

    Update init imports

--- controllably/__init__.py
+++ controllably/__init__.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
-from .core import init, start_logging, start_project_here, get_setup
+from .core.factory import get_setup
+from .core.file_handler import init, start_logging, start_project_here
 
+# Initialization code
+# Set numpy print options to 1.21
+import numpy as np
+np.set_printoptions(legacy='1.21')
+del np
+
+# Add the external libraries path to sys.path
 import sys
 import os
 external_libs = os.path.join(os.path.dirname(__file__), 'external')
 sys.path.insert(0, external_libs)
-
-import numpy as np
-np.set_printoptions(legacy='1.21')
+del sys, os, external_libs
--- controllably/core/__init__.py
+++ controllably/core/__init__.py
@@ -1,4 +1,2 @@
 # -*- coding: utf-8 -*-
 """Sub package for the core functionalities of control-lab-ly."""
-from .factory import get_setup
-from .file_handler import init, start_logging, start_project_here
