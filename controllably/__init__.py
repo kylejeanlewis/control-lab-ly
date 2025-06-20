16c59bc3f646d8cb44aa61101b9c6ddcbb7ac313
Chang Jie Leong <changjie9@gmail.com>
2025-06-20 12:41:58 +0800

    Update __init__.py

--- controllably/__init__.py
+++ controllably/__init__.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-from .core.factory import get_setup
-from .core.file_handler import init, start_logging, start_project_here
-
-# Initialization code
 # Set numpy print options to 1.21
 import numpy as np
 np.set_printoptions(legacy='1.21')
@@ -14,3 +10,15 @@ import os
 external_libs = os.path.join(os.path.dirname(__file__), 'external')
 sys.path.insert(0, external_libs)
 del sys, os, external_libs
+
+# Get the main logger
+import logging
+logging.getLogger('controllably').setLevel(logging.DEBUG)
+
+# Import logging filters
+from ._log_filters import CustomLevelFilter, AppFilter
+
+# Import functions
+from .core.factory import get_setup
+from .core.file_handler import init, start_project_here
+from .core.logging import start_logging
