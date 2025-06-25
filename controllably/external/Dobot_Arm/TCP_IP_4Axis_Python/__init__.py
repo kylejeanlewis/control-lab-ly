c23b0817721ad02c177fcacb8bbd3408b02c689b
Chang Jie Leong <changjie9@gmail.com>
2025-06-25 17:19:07 +0800

    Update __init__.py

--- controllably/external/Dobot_Arm/TCP_IP_4Axis_Python/__init__.py
+++ controllably/external/Dobot_Arm/TCP_IP_4Axis_Python/__init__.py
@@ -4,3 +4,4 @@ Retrieved from https://github.com/Dobot-Arm/TCP-IP-4Axis-Python/tree/main
 on 2024-11-22
 """
 from .dobot_api import DobotApiDashboard, DobotApiMove
+__all__ = ['DobotApiDashboard','DobotApiMove']
