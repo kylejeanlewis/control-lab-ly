3797fca6d5d9362c210dfda3cd365e9f568f65c4
Chang Jie Leong <changjie9@gmail.com>
2025-07-31 18:45:08 +0800

    Update position.py

--- controllably/core/position.py
+++ controllably/core/position.py
@@ -596,12 +596,17 @@ class Labware:
         self.y = dimensions.get('yDimension', 0)/2
         self.z = dimensions.get('zDimension', 0)/2
         self._dimensions = (self.x*2,self.y*2,self.z*2)
-        self._is_stackable = self._details.get('parameters',{}).get('isStackable', False)
         self.is_tiprack = self._details.get('parameters',{}).get('isTiprack', False)
         self._ordering = self._details.get('ordering', [[]])
         self._wells = {name:Well(name=name, _details=details, parent=self) for name,details in self._details.get('wells',{}).items()}
         
-        buffer = self._details.get('exclusionBuffer', ((0,0,0),(0,0,0)))
+        controllably_details = self._details.get('controllably', {})
+        is_stackable = controllably_details.get('parameters',{}).get('isStackable', None) 
+        is_stackable = self._details.get('parameters',{}).get('isStackable', False) if is_stackable is None else is_stackable
+        self._is_stackable = is_stackable
+        
+        buffer = controllably_details.get('exclusionBuffer', None)
+        buffer = self._details.get('exclusionBuffer', ((0,0,0),(0,0,0))) if buffer is None else buffer
         self.exclusion_zone = BoundingBox(
             reference=self.bottom_left_corner, 
             dimensions=self._dimensions, 
@@ -613,10 +618,12 @@ class Labware:
         return
     
     def __repr__(self) -> str:
-        return f"{self.name} ({self.__class__.__name__}:{id(self)}) -> {self.parent.name} ({self.parent.__class__.__name__}:{id(self.parent)})" 
+        parent_info = "None" if self.parent is None else f"{self.parent.name} ({self.parent.__class__.__name__}:{id(self.parent)})"
+        return f"{self.name} ({self.__class__.__name__}:{id(self)}) -> {parent_info}" 
     
     def __str__(self) -> str:
-        return f"{self.name} ({len(self._wells)}x) on {self.parent.name}" 
+        parent_info = "" if self.parent is None else f"on {self.parent.name}"
+        return f"{self.name} ({len(self._wells)}x) {parent_info}" 
     
     @classmethod
     def fromConfigs(cls, details:dict[str, Any], parent:Slot|None = None) -> Labware:
@@ -814,7 +821,8 @@ class Labware:
         Returns:
             Slot|None: Slot above
         """
-        details_above = self._details.get('slotAbove',{})
+        details_above = self._details.get('controllably',{}).get('slotAbove',None)
+        details_above = self._details.get('slotAbove',{}) if details_above is None else details_above
         assert self.is_stackable, "Labware is not stackable"
         assert len(details_above) > 0, "No details for Slot above"
         below_name = self.parent.name if isinstance(self.parent, Slot) else 'slot'
@@ -1119,6 +1127,7 @@ class Slot:
             assert self.loaded_labware.slot_above.loaded_labware is None, "Another Labware is stacked above"
             self.loaded_labware.slot_above.slot_below = None
         labware = self.loaded_labware
+        labware.parent = None
         self.loaded_labware = None
         self._delete_slot_above()
         return labware
