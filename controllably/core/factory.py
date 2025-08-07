78188ca95c83f1ca1548839c13c087cae29cf6d7
Chang Jie Leong <changjie9@gmail.com>
2025-08-07 18:23:24 +0800

    Update factory.py
    - disconnect setup parts upon unsuccessful initialization

--- controllably/core/factory.py
+++ controllably/core/factory.py
@@ -254,6 +254,11 @@ def get_setup(
     n_errors = len(errors)
     if n_errors and not silent_fail:
         logger.error(f'Errors occurred for: {", ".join(errors.keys())}')
+        for _,part in setup._asdict():
+            try:
+                part.disconnect()
+            except:
+                pass
         raise RuntimeError(f"{n_errors} error(s) during initialization", setup)
     
     if platform_type is None or len(platform_type.__annotations__) != len(setup):
