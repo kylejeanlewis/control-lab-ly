3cdb92ef47567926c1e4dee221ef01538683efd6
Chang Jie Leong <changjie9@gmail.com>
2025-09-12 14:06:52 +0800

    Update tutorial_connection.py

--- tutorials/tutorial_connection.py
+++ tutorials/tutorial_connection.py
@@ -1,20 +1,38 @@
+# %%[markdown]
+# # Connection utility functions
+# 
+# The `controllably.core.connection` module provides utility functions to manage and retrieve connection information for devices. 
+# This includes functions to get available ports, host information, node identifiers, and to match IP addresses.
+
 # %%
 from controllably.core.connection import (
     get_addresses, get_host, get_node, get_ports, match_current_ip_address
 )
 
+# %% [markdown]
+# A list if available communication ports connected to the system can be retrieved using the `get_ports()` function.
+
 # %%
 get_ports()
+
+# %% [markdown]
+# The `get_host()` function retrieves the hostname of the current machine.
 
 # %%
 host = get_host()
 host
+
+# %% [markdown]
+# The `match_current_ip_address()` function checks if the provided hostname or IP address matches the current machine's IP address.
 
 # %%
 match_current_ip_address(host)
 
 # %%
 match_current_ip_address('127.0.0.1')
+
+# %% [markdown]
+# The `get_node()` function retrieves a unique identifier for the current machine, typically the MAC address.
 
 # %%
 mac_address = get_node()
@@ -23,6 +41,10 @@ mac_address
 # %%
 node = get_node(mac_address=False)
 node
+
+# %% [markdown]
+# The `get_addresses()` function retrieves connection addresses from a provided registry dictionary, 
+# using the current machine's identifier (MAC address or node name) to look up relevant entries.
 
 # %%
 registry = {
