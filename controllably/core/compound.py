f8ff82e513131620581aceb677730754acf0bc73
Chang Jie Leong <changjie9@gmail.com>
2025-09-16 17:55:15 +0800

    Update compound.py
    - update type hinting and docstring

--- controllably/core/compound.py
+++ controllably/core/compound.py
@@ -324,7 +324,7 @@ class Ensemble(Compound):
         
         Args:
             method_name (str): method name to be executed
-            kwargs_generator (Callable[[int,int,Part], dict[str,Any]]|None): function to generate kwargs for each channel. Defaults to None.
+            kwargs_generator (Callable[[int,int,Part], dict[str,Any]]|None): function to generate kwargs for each channel using indices, channels, and Parts. Defaults to None.
             channels (Iterable[int]): channels to execute on
             max_workers (int, optional): maximum number of workers. Defaults to 4.
             timeout (int|float, optional): timeout for each worker. Defaults to 120.
@@ -371,7 +371,7 @@ class Ensemble(Compound):
         func_name = method.__name__
         if func_name.endswith("__") and not func_name.startswith("__"):
             return method
-        def func(self, *args, channel: int|Sequence[int]|None = None, **kwargs) -> list|None:
+        def func(self, *args, channel: int|Sequence[int]|None = None, **kwargs) -> dict|None:
             outs = dict()
             for chn,obj in cls._get_channel(self, channel).items():
                 obj_method: Callable = getattr(obj, func_name)
@@ -489,7 +489,7 @@ class Combined:
         return
     
     @classmethod
-    def fromConfig(cls, config:dict) -> Type[Combined]:
+    def fromConfig(cls, config:dict) -> Combined:
         """
         Factory method to create Combined from configuration dictionary
         
@@ -497,13 +497,12 @@ class Combined:
             config (dict): configuration dictionary
             
         Returns:
-            Type[Combined]: instance of Combined (or its subclasses)
+            Combined: instance of Combined (or its subclasses)
         """
         details = config.pop('details')
         device = factory.create_from_config(config)
-        # parts = {name:factory.load_parts(settings, device=device) for name,settings in details.items()}
-        for settings in details.values():
-            settings['device'] = device
+        for part_config in details.values():
+            part_config['settings']['device'] = device
         parts = factory.load_parts(details)
         return cls(device=device, parts=parts, **config)
     
@@ -728,7 +727,7 @@ class Multichannel(Combined):
         func_name = method.__name__
         if func_name.endswith("__") and not func_name.startswith("__"):
             return method
-        def func(self, *args, channel: int|Sequence[int]|None = None, **kwargs) -> list|None:
+        def func(self, *args, channel: int|Sequence[int]|None = None, **kwargs) -> dict|None:
             outs = dict()
             for chn,obj in cls._get_channel(self, channel).items():
                 cls.setActiveChannel(self, chn)
